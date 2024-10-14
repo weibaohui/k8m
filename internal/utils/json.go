5e5de2b5ae4fe7f98ac8c8373ac8c7c71c1f57c4
weibaohui <weibaohui@yeah.net>
2024-10-14 23:08:30 +0800

    新增深拷贝函数和格式化JSON转换功能
    - 在JSON工具类中添加了ToJSON函数，支持将任意结构体转换为格式化的JSON字符串
    - 引入了DeepCopy函数，可以对任意类型的参数进行深拷贝，避免了直接操作原始数据可能导致的修改问题

--- internal/utils/json.go
+++ internal/utils/json.go
@@ -5,14 +5,22 @@ import (
 	"log"
 )
 
-// JSONUtils 提供通用的 JSON 操作方法
-type JSONUtils struct{}
-
 // ToJSON 将任意结构体转换为格式化的 JSON 字符串
-func (j *JSONUtils) ToJSON(v interface{}) string {
+func ToJSON(v interface{}) string {
 	jsonData, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
 		log.Fatalf("Error converting to JSON: %v", err)
 	}
 	return string(jsonData)
+}
+
+// DeepCopy 函数接受任何类型的参数并返回其深复制的副本
+func DeepCopy[T any](src T) (T, error) {
+	var dst T
+	data, err := json.Marshal(src)
+	if err != nil {
+		return dst, err
+	}
+	err = json.Unmarshal(data, &dst)
+	return dst, err
 }
