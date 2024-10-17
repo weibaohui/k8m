4dc362487a051c16cb9b5e7766c4aea414092df8
weibaohui <weibaohui@yeah.net>
2024-10-17 12:08:28 +0800

    新增文件浏览、上传和下载功能，以及ChatGPT相关接口- 添加文件浏览、上传和下载的API接口 - 新增ChatGPT的POST和SSE接口 - 优化API路由的组织结构

--- main.go
+++ main.go
@@ -84,8 +84,14 @@ func main() {
 		api.GET("/doc/gvk/:api_version/:kind", doc.Doc)
 		api.POST("/doc/detail", doc.Detail)
 
+		// chatgpt
 		api.POST("/chat", chat.Chat)
 		api.GET("/chat/sse", chat.Sse)
+
+		// pod 文件浏览上传下载
+		api.GET("/file/list", pod.FileListHandler)
+		api.POST("/file/show", pod.ShowFileHandler)
+		api.POST("/file/save", pod.SaveFileHandler)
 
 		// k8s pod
 		// http://127.0.0.1:3618/k8s/doc/gvk/stable.example.com%2Fv1/CronTab
