79739c233c7867ef9ccb0bcb2d924b8d8a162398
weibaohui <weibaohui@yeah.net>
2024-10-18 16:04:36 +0800

    整合环境变量配置并设置默认值
    - 添加对环境变量 PORT、KUBECONFIG、OPENAI_API_KEY 和 OPENAI_API_URL 的支持- 根据环境变量 GIN_MODE 设置调试模式
    - 为各配置项添加默认值，提高灵活性和易用性

--- config/config.go
+++ config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"flag"
+	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/spf13/pflag"
 	"k8s.io/client-go/util/homedir"
@@ -34,11 +36,31 @@ func (c *Config) InitFlags() {
 	// 将Go的flag绑定到pflag
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 
-	pflag.BoolVarP(&c.Debug, "debug", "d", false, "Debug mode")
-	pflag.IntVarP(&c.Port, "port", "p", 3618, "Port for the server to listen on")
-	pflag.StringVarP(&c.APIKey, "chatgpt-key", "k", "", "API Key for ChatGPT")
-	pflag.StringVarP(&c.APIURL, "chatgpt-url", "u", "", "API URL for ChatGPT")
-	pflag.StringVarP(&c.Kubeconfig, "kubeconfig", "c", filepath.Join(homedir.HomeDir(), ".kube", "config"), "Absolute path to the kubeConfig file")
+	// 环境变量绑定
+	// GIN_MODE
+	defaultPort := 3618
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		defaultPort, _ = strconv.Atoi(envPort)
+	}
+	defaultKubeConfig := os.Getenv("KUBECONFIG")
+	if defaultKubeConfig == "" {
+		defaultKubeConfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	defaultApiKey := os.Getenv("OPENAI_API_KEY")
+	defaultApiURL := os.Getenv("OPENAI_API_URL")
+	defaultDebug := false
+	if debug := os.Getenv("GIN_MODE"); debug == "release" {
+		// GIN_MODE=release
+		// 关闭debug模式
+		defaultDebug = false
+	}
+
+	pflag.BoolVarP(&c.Debug, "debug", "d", defaultDebug, "Debug mode,same as GIN_MODE")
+	pflag.IntVarP(&c.Port, "port", "p", defaultPort, "Port for the server to listen on")
+	pflag.StringVarP(&c.APIKey, "chatgpt-key", "k", defaultApiKey, "API Key for ChatGPT")
+	pflag.StringVarP(&c.APIURL, "chatgpt-url", "u", defaultApiURL, "API URL for ChatGPT")
+	pflag.StringVarP(&c.Kubeconfig, "kubeconfig", "c", defaultKubeConfig, "Absolute path to the kubeConfig file")
 
 	// 检查是否设置了 --v 参数
 	if vFlag := pflag.Lookup("v"); vFlag == nil || vFlag.Value.String() == "0" {
