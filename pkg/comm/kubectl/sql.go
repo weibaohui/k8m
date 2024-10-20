3fe1a4c98b88d94da69266afb9bafdee4820a885
weibaohui <weibaohui@yeah.net>
2024-10-20 23:16:03 +0800

    refactor(kubectl): 重构 SQLGet 函数
    - 移除了 SQLGet 函数中的冗余代码和复杂的逻辑
    - 新增了 WithContext、Resource、Namespace 和 Name 方法，用于构建查询上下文- 重构了 Get 方法，简化了查询逻辑
    - 通过链式调用方式提高了代码可读性和易用性

--- pkg/comm/kubectl/sql.go
+++ pkg/comm/kubectl/sql.go
@@ -1,55 +1,39 @@
 package kubectl
 
 import (
-	"fmt"
+	"context"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/klog/v2"
 )
 
-func (k8s *Kubectl) SQLGet(obj runtime.Object) error {
-	// 使用 scheme.Scheme.ObjectKinds() 获取 Kind
-	gvks, _, err := scheme.Scheme.ObjectKinds(obj)
-	if err != nil {
-		return fmt.Errorf("error getting kind by scheme.Scheme.ObjectKinds : %v", err)
-	}
-
-	// 寻找kind
-	var kind string
-	typeAccessor, err := meta.TypeAccessor(obj)
-	if err != nil {
-		return fmt.Errorf("error getting typeMeta by meta.TypeAccessor(obj) : %v", err)
-	}
-	kind = typeAccessor.GetKind()
-
-	if kind == "" {
-		for _, gv := range gvks {
-			if gv.Kind != "" {
-				kind = gv.Kind
-				break
-			}
-		}
-	}
-
-	// 获取元数据（比如Name和Namespace）
-	accessor, err := meta.Accessor(obj)
-	if err != nil {
-		return err
-	}
-	name := accessor.GetName()           // 获取资源的名称
-	namespace := accessor.GetNamespace() // 获取资源的命名空间
-
+func (k8s *Kubectl) WithContext(ctx context.Context) *Kubectl {
+	tx := k8s.getInstance()
+	tx.Statement.Context = ctx
+	return tx
+}
+func (k8s *Kubectl) Resource(obj runtime.Object) *Kubectl {
+	tx := k8s.getInstance()
+	tx.Statement.ParseFromRuntimeObj(obj)
+	return tx
+}
+func (k8s *Kubectl) Namespace(ns string) *Kubectl {
+	tx := k8s.getInstance()
+	tx.Statement.Namespace = ns
+	return tx
+}
+func (k8s *Kubectl) Name(ns string) *Kubectl {
+	tx := k8s.getInstance()
+	tx.Statement.Name = ns
+	return tx
+}
+func (k8s *Kubectl) Get(obj runtime.Object) error {
 	tx := k8s.getInstance()
 
-	tx.Statement.SetGVKs(gvks).
-		SetNamespace(namespace).
-		SetName(name).
-		SetType(Query).
-		SetDest(obj)
+	tx.Statement.SetType(Query)
+	tx.Statement.Dest = obj
 	return tx.Callback().Query().Execute(tx.Statement.Context, tx)
 }
 
@@ -60,7 +44,8 @@ func (k8s *Kubectl) sqlTest() {
 			Name:      "",
 		},
 	}
-	err := k8s.SQLGet(&pod)
+	err := k8s.Resource(&pod).
+		Namespace("default").Name("").Get(&pod)
 	if err != nil {
 		klog.Errorf("k8s.First(&pod) error :%v", err)
 	}
