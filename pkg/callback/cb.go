ae84d5198b91adde18f585c14a8b244cfd39245e
weibaohui <weibaohui@yeah.net>
2024-10-21 17:01:38 +0800

    refactor(pkg/callback): 重构回调函数并更名
    - 将 QueryCallback 改为 GetCallback- 将 Query 函数改为 Get
    - 更新相关的日志信息

--- pkg/callback/cb.go
+++ pkg/callback/cb.go
@@ -8,14 +8,14 @@ import (
 )
 
 func RegisterCallback() {
-	queryCallback := kubectl.Init().Callback().Query()
-	_ = queryCallback.Register("k8m:query", Query)
+	queryCallback := kubectl.Init().Callback().Get()
+	_ = queryCallback.Register("k8m:get11", Get)
 }
 
-func Query(ctx context.Context, k8s *kubectl.Kubectl) error {
+func Get(ctx context.Context, k8s *kubectl.Kubectl) error {
 	json := k8s.Statement.String()
 	// todo 在这里可以统一进行权限认证等操作，返回error即可阻断执行
 	u := ctx.Value("user")
-	klog.V(2).Infof("%s k8s Query stmt json:\n %s\n", u, json)
+	klog.V(2).Infof("%s k8s Get stmt json:\n %s\n", u, json)
 	return nil
 }
