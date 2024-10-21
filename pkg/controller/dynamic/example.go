8a326491732f64548e193fa8042758bee1fb6c2f
weibaohui <weibaohui@yeah.net>
2024-10-21 17:24:47 +0800

    feat(controller/dynamic): 添加动态客户端示例代码
    - 新增 example.go 文件，展示如何使用动态客户端操作 Kubernetes 资源
    - 示例代码包括获取 Deployment、CRD 资源，以及列出 Deployment 和 CRD 资源列表
    - 使用 gin 框架处理 HTTP 请求，通过上下文传递请求信息
    - 引入 kubectl 和 utils 工具包，简化资源操作和日志输出

---
+++ pkg/controller/dynamic/example.go
@@ -0,0 +1,63 @@
+package dynamic
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/kubectl"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/klog/v2"
+)
+
+func Example(c *gin.Context) {
+	item := v1.Deployment{}
+	err := kubectl.Init().
+		WithContext(c.Request.Context()).
+		Resource(&item).
+		Namespace("default").
+		Name("ci-755702-codexxx").
+		Get(&item).Error
+	if err != nil {
+		klog.Errorf("k8s.First(&item) error :%v", err)
+	}
+	fmt.Printf("Get Item %s\n", item.Spec.Template.Spec.Containers[0].Image)
+	var crontab unstructured.Unstructured
+	err = kubectl.Init().
+		WithContext(c.Request.Context()).
+		CRD("stable.example.com", "v1", "CronTab").
+		Namespace("default").
+		Name("my-new-cron-object").
+		Get(&crontab).Error
+	if err != nil {
+		fmt.Printf("Fill %v\n", err)
+	}
+	json := utils.ToJSON(crontab)
+	fmt.Printf("crontab json %s\n", json)
+
+	var items []v1.Deployment
+	err = kubectl.Init().
+		WithContext(c.Request.Context()).
+		Resource(&item).
+		Namespace("default").
+		List(&items).Error
+	if err != nil {
+		fmt.Printf("List Error %v\n", err)
+	}
+	fmt.Printf("List Deployment count %d\n", len(items))
+	for _, d := range items {
+		fmt.Printf("List Deployment Items foreach %s\n", d.Spec.Template.Spec.Containers[0].Image)
+	}
+
+	var crontabList []unstructured.Unstructured
+	err = kubectl.Init().
+		WithContext(c.Request.Context()).
+		CRD("stable.example.com", "v1", "CronTab").
+		Namespace("default").
+		List(&crontabList).Error
+	fmt.Printf("List crd crontabList count %d\n", len(crontabList))
+	for _, d := range crontabList {
+		fmt.Printf("List Deployment Items foreach %s\n", d.GetName())
+	}
+}
