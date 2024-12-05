d90bd1ce56ceb57f366bbf30164244f77505bcd8
weibaohui <weibaohui@yeah.net>
2024-12-05 19:42:04 +0800

    refactor(controller): 重构节点控制器方法
    - 使用 kom 包替换 service 包以进行节点操作- 优化 Drain、Cordon 和 UnCordon 函数的实现- 移除重复的错误处理代码，使用 WriteJsonErrorOrOK 函数统一处理

--- pkg/controller/node/node.go
+++ pkg/controller/node/node.go
@@ -3,36 +3,28 @@ package node
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
-	"github.com/weibaohui/k8m/pkg/service"
+	"github.com/weibaohui/kom/kom"
+	v1 "k8s.io/api/core/v1"
 )
 
 func Drain(c *gin.Context) {
 	name := c.Param("name")
 	ctx := c.Request.Context()
-	err := service.NodeService().Drain(ctx, name)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	amis.WriteJsonOK(c)
+	err := kom.DefaultCluster().WithContext(ctx).Resource(&v1.Node{}).Name(name).
+		Ctl().Node().Drain()
+	amis.WriteJsonErrorOrOK(c, err)
 }
 func Cordon(c *gin.Context) {
 	name := c.Param("name")
 	ctx := c.Request.Context()
-	err := service.NodeService().Cordon(ctx, name)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	amis.WriteJsonOK(c)
+	err := kom.DefaultCluster().WithContext(ctx).Resource(&v1.Node{}).Name(name).
+		Ctl().Node().Cordon()
+	amis.WriteJsonErrorOrOK(c, err)
 }
 func UnCordon(c *gin.Context) {
 	name := c.Param("name")
 	ctx := c.Request.Context()
-	err := service.NodeService().UnCordon(ctx, name)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	amis.WriteJsonOK(c)
+	err := kom.DefaultCluster().WithContext(ctx).Resource(&v1.Node{}).Name(name).
+		Ctl().Node().UnCordon()
+	amis.WriteJsonErrorOrOK(c, err)
 }
