8c996b5170c024b71cb504a227f9377cdbe34537
weibaohui <weibaohui@yeah.net>
2024-12-05 19:41:56 +0800

    refactor: 移除 nodeService 中的冗余函数
    移除了 nodeService 结构体中未使用的 Drain、Cordon 和 UnCordon 函数，简化了代码结构。

--- pkg/service/node.go
+++ pkg/service/node.go
@@ -1,39 +1,4 @@
 package service
 
-import (
-	"context"
-
-	"github.com/weibaohui/kom/kom"
-	v1 "k8s.io/api/core/v1"
-)
-
 type nodeService struct {
-}
-
-func (d *nodeService) Drain(ctx context.Context, name string) error {
-	err := kom.DefaultCluster().WithContext(ctx).
-		Resource(&v1.Node{}).Name(name).
-		Ctl().Node().Drain()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-func (d *nodeService) Cordon(ctx context.Context, name string) error {
-	err := kom.DefaultCluster().WithContext(ctx).
-		Resource(&v1.Node{}).Name(name).
-		Ctl().Node().Cordon()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-func (d *nodeService) UnCordon(ctx context.Context, name string) error {
-	err := kom.DefaultCluster().WithContext(ctx).
-		Resource(&v1.Node{}).Name(name).
-		Ctl().Node().UnCordon()
-	if err != nil {
-		return err
-	}
-	return nil
 }
