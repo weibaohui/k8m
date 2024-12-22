11d7941d847d754f65d4de242663520e705a7fe4
weibaohui <weibaohui@yeah.net>
2024-12-22 19:23:08 +0800

    refactor(controller/chat): 重构 Chat 控制器并添加 SSE 相关功能
    - 移除了未使用的 import 语句，简化了代码结构
    - 删除了冗余的 Sse 函数，减少了代码重复
    - 新增了 SSEDescribe 函数，用于处理 SSE 方式的资源描述分析请求
    - 优化了现有函数，提高了代码可读性和可维护性

--- pkg/controller/chat/chat.go
+++ pkg/controller/chat/chat.go
@@ -1,18 +1,13 @@
 package chat
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday/v2"
-	"github.com/sashabaranov/go-openai"
 	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/controller/sse"
 	"github.com/weibaohui/k8m/pkg/service"
 	"k8s.io/klog/v2"
 )
@@ -84,6 +79,36 @@ func Describe(c *gin.Context) {
 		"result": result,
 	})
 }
+func SSEDescribe(c *gin.Context) {
+
+	chatService := service.ChatService()
+	if !chatService.IsEnabled() {
+		amis.WriteJsonData(c, gin.H{
+			"result": "请先配置开启ChatGPT功能",
+		})
+		return
+	}
+	var data struct {
+		Describe string `json:"describe"`
+		Kind     string `json:"kind"`
+		Group    string `json:"group"`
+	}
+
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+	}
+
+	prompt := fmt.Sprintf("请你作为kubernetes k8s 运维专家，对下面 %s %s 资源的Describe 信息 分析。请给出分析结论，如果有问题，请指出问题，并给出可能得解决方案:\n%s\n。格式要求：请使用文本格式，不要使用markdown格式。请保留换行符等保证基本的格式", data.Group, data.Kind, data.Describe)
+
+	stream, err := chatService.GetChatStream(prompt)
+	if err != nil {
+		klog.V(2).Infof("Error Ssemaking request:%v\n\n", err)
+		return
+	}
+	sse.WriteSSEChatCompletionStream(c, stream)
+}
+
 func Cron(c *gin.Context) {
 	chatService := service.ChatService()
 	if !chatService.IsEnabled() {
@@ -133,48 +158,4 @@ func Log(c *gin.Context) {
 	amis.WriteJsonData(c, gin.H{
 		"result": result,
 	})
-}
-func Sse(c *gin.Context) {
-	q := c.Query("q")
-	chatService := service.ChatService()
-	resp, err := chatService.GetChatStream(q)
-	if err != nil {
-		klog.V(2).Infof("Error Ssemaking request:%v\n\n", err)
-		return
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			klog.V(6).Infof("Body close error:%v\n", err)
-		}
-	}(resp.Body)
-
-	// 检查响应状态码
-	if resp.StatusCode == http.StatusOK {
-		scanner := bufio.NewScanner(resp.Body)
-		for scanner.Scan() {
-			line := scanner.Text()
-			line = strings.TrimPrefix(line, "data:")
-			line = strings.TrimSpace(line)
-			if line != "" && line != "[DONE]" {
-				chatResult := &openai.ChatCompletionStreamResponse{}
-				err := json.Unmarshal([]byte(line), chatResult)
-				if err != nil {
-					c.SSEvent("message", fmt.Sprintf("json error:%v\n%s", err, line))
-					continue
-				}
-				if len(chatResult.Choices) > 0 {
-					message := chatResult.Choices[0].Delta.Content
-					c.SSEvent("message", message)
-				}
-			}
-
-		}
-		if err := scanner.Err(); err != nil {
-			c.SSEvent("message", fmt.Sprintf("Error reading response:%v", err))
-		}
-	} else {
-		fmt.Println("Request failed with status code:", resp.StatusCode)
-	}
-
 }
