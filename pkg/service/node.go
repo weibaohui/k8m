2337eda33b7f90db2b00fb024410ffeb6cdfe86e
weibaohui <weibaohui@yeah.net>
2025-01-01 10:07:53 +0800

    feat(node): 添加节点 IP 使用情况注解
    - 在 nodeService 中添加 SetIPUsage 方法，用于设置节点的 IP 使用情况注解- 新增 addAnnotations 辅助函数，用于添加或更新节点的注解
    - 通过 kom.DefaultCluster().Name(nodeName).Ctl().Node().IPUsage() 获取 IP 使用情况
    - 在节点对象中添加以下注解：
      - ip.usage.total: 总 IP 数量  - ip.usage.used: 已使用的 IP 数量
      - ip.usage.available: 可用的 IP 数量

--- pkg/service/node.go
+++ pkg/service/node.go
@@ -1,4 +1,43 @@
 package service
 
+import (
+	"fmt"
+
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
 type nodeService struct {
+}
+
+func (n *nodeService) SetIPUsage(item unstructured.Unstructured) unstructured.Unstructured {
+	nodeName := item.GetName()
+	total, used, available := kom.DefaultCluster().Name(nodeName).Ctl().Node().IPUsage()
+
+	// 设置或追加 annotations
+	addAnnotations(&item, map[string]string{
+		"ip.usage.total":     fmt.Sprintf("%d", total),
+		"ip.usage.used":      fmt.Sprintf("%d", used),
+		"ip.usage.available": fmt.Sprintf("%d", available),
+	})
+
+	return item
+}
+
+// addAnnotations 添加或更新 annotations
+func addAnnotations(item *unstructured.Unstructured, newAnnotations map[string]string) {
+	// 获取现有的 annotations
+	annotations := item.GetAnnotations()
+	if annotations == nil {
+		// 如果不存在，初始化一个 map
+		annotations = make(map[string]string)
+	}
+
+	// 追加或覆盖新数据
+	for key, value := range newAnnotations {
+		annotations[key] = value
+	}
+
+	// 设置回对象
+	item.SetAnnotations(annotations)
 }
