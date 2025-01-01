90a5f75d84db380ba1abb0c691c85b6acf0d6986
weibaohui <weibaohui@yeah.net>
2025-01-01 13:09:43 +0800

    feat(pod): 添加设置 Pod 分配状态的功能
    - 在 podService 中添加 SetAllocatedStatus 方法- 该方法根据 Pod 的资源使用情况设置或更新 annotations
    - 支持设置 CPU 和内存的请求和限制及其比例

--- pkg/service/pod.go
+++ pkg/service/pod.go
@@ -2,10 +2,13 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	"github.com/weibaohui/kom/kom"
+	"github.com/weibaohui/kom/utils"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type podService struct {
@@ -26,4 +29,31 @@ func (p *podService) StreamPodLogs(ctx context.Context, ns, name string, logOpti
 	err := kom.DefaultCluster().WithContext(ctx).Namespace(ns).Name(name).ContainerName(logOptions.Container).GetLogs(&stream, logOptions).Error
 
 	return stream, err
+}
+
+// SetAllocatedStatus 设置节点的分配状态
+func (p *podService) SetAllocatedStatus(item unstructured.Unstructured) unstructured.Unstructured {
+	podName := item.GetName()
+	table := kom.DefaultCluster().Name(podName).Namespace(item.GetNamespace()).Resource(&v1.Pod{}).Ctl().Pod().ResourceUsageTable()
+	for _, row := range table {
+		if row.ResourceType == "cpu" {
+			// 设置或追加 annotations
+			utils.AddOrUpdateAnnotations(&item, map[string]string{
+				"cpu.request":         fmt.Sprintf("%s", row.Request),
+				"cpu.requestFraction": fmt.Sprintf("%s", row.RequestFraction),
+				"cpu.limit":           fmt.Sprintf("%s", row.Limit),
+				"cpu.limitFraction":   fmt.Sprintf("%s", row.LimitFraction),
+			})
+		} else if row.ResourceType == "memory" {
+			// 设置或追加 annotations
+			utils.AddOrUpdateAnnotations(&item, map[string]string{
+				"memory.request":         fmt.Sprintf("%s", row.Request),
+				"memory.requestFraction": fmt.Sprintf("%s", row.RequestFraction),
+				"memory.limit":           fmt.Sprintf("%s", row.Limit),
+				"memory.limitFraction":   fmt.Sprintf("%s", row.LimitFraction),
+			})
+		}
+	}
+
+	return item
 }
