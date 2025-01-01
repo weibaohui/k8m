5de283984a86271b70f70775201aeef3ae181679
weibaohui <weibaohui@yeah.net>
2025-01-01 10:49:42 +0800

    feat(node): 增加 CPU 和内存资源使用情况的 annotations- 在 nodeService 中添加 SetAllocatedStatus 方法，用于设置节点的分配状态 - 使用 utils.AddOrUpdateAnnotations 函数来更新或添加 annotations - 为 CPU 和内存资源添加以下 annotations：   - cpu.request   - cpu.requestFraction - cpu.limit   - cpu.limitFraction   - memory.request - memory.requestFraction   - memory.limit   - memory.limitFraction- 移除原有的 addAnnotations函数，改为使用通用的工具函数

--- pkg/service/node.go
+++ pkg/service/node.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/weibaohui/kom/kom"
+	"github.com/weibaohui/kom/utils"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -15,7 +16,7 @@ func (n *nodeService) SetIPUsage(item unstructured.Unstructured) unstructured.Un
 	total, used, available := kom.DefaultCluster().Name(nodeName).Ctl().Node().IPUsage()
 
 	// 设置或追加 annotations
-	addAnnotations(&item, map[string]string{
+	utils.AddOrUpdateAnnotations(&item, map[string]string{
 		"ip.usage.total":     fmt.Sprintf("%d", total),
 		"ip.usage.used":      fmt.Sprintf("%d", used),
 		"ip.usage.available": fmt.Sprintf("%d", available),
@@ -24,20 +25,29 @@ func (n *nodeService) SetIPUsage(item unstructured.Unstructured) unstructured.Un
 	return item
 }
 
-// addAnnotations 添加或更新 annotations
-func addAnnotations(item *unstructured.Unstructured, newAnnotations map[string]string) {
-	// 获取现有的 annotations
-	annotations := item.GetAnnotations()
-	if annotations == nil {
-		// 如果不存在，初始化一个 map
-		annotations = make(map[string]string)
+// SetAllocatedStatus 设置节点的分配状态
+func (n *nodeService) SetAllocatedStatus(item unstructured.Unstructured) unstructured.Unstructured {
+	nodeName := item.GetName()
+	table := kom.DefaultCluster().Name(nodeName).Ctl().Node().ResourceUsageTable()
+	for _, row := range table {
+		if row.ResourceType == "cpu" {
+			// 设置或追加 annotations
+			utils.AddOrUpdateAnnotations(&item, map[string]string{
+				"cpu.request":         fmt.Sprintf("%s", row.Request),
+				"cpu.requestFraction": fmt.Sprintf("%s", row.RequestFraction),
+				"cpu.limit":           fmt.Sprintf("%s", row.Limit),
+				"cpu.limitFraction":   fmt.Sprintf("%s", row.LimitFraction),
+			})
+		} else if row.ResourceType == "memory" {
+			// 设置或追加 annotations
+			utils.AddOrUpdateAnnotations(&item, map[string]string{
+				"memory.request":         fmt.Sprintf("%s", row.Request),
+				"memory.requestFraction": fmt.Sprintf("%s", row.RequestFraction),
+				"memory.limit":           fmt.Sprintf("%s", row.Limit),
+				"memory.limitFraction":   fmt.Sprintf("%s", row.LimitFraction),
+			})
+		}
 	}
 
-	// 追加或覆盖新数据
-	for key, value := range newAnnotations {
-		annotations[key] = value
-	}
-
-	// 设置回对象
-	item.SetAnnotations(annotations)
+	return item
 }
