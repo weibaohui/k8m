dccbaaac660a433ec64389202010118b3a239bff
weibaohui <weibaohui@yeah.net>
2025-01-01 23:08:14 +0800

    perf(cache): 调整缓存配置以提高性能
    -将 Get 缓冲区的键数量从 64 增加到 256
    - 此修改旨在通过减少缓存查找次数来提高整体性能

--- pkg/service/pod.go
+++ pkg/service/pod.go
@@ -26,7 +26,7 @@ func newPodService() *podService {
 		cache, err := ristretto.NewCache(&ristretto.Config[string, any]{
 			NumCounters: 1e7,     // number of keys to track frequency of (10M).
 			MaxCost:     1 << 30, // maximum cost of cache (1GB).
-			BufferItems: 64,      // number of keys per Get buffer.
+			BufferItems: 256,     // number of keys per Get buffer.
 		})
 		if err != nil {
 			klog.Errorf("Failed to create cache: %v", err)
