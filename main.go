2b15851cf44c01c6094f0850b901934facfc1fd0
weibaohui <weibaohui@yeah.net>
2025-01-02 23:06:48 +0800

    feat(api): 为 CRD列表操作添加 POST 接口
    - 在 existingConnection 路由中添加了两个新的 POST接口：
      - /:kind/group/:group/version/:version/list/ns/:ns
      - /:kind/group/:group/version/:version/list- 这些新增的 POST接口与现有的 GET 接口使用相同的处理函数 dynamic.List
    - 添加这些接口是为了支持 CRD（自定义资源定义）的列表操作

--- main.go
+++ main.go
@@ -120,7 +120,9 @@ func main() {
 		api.POST("/:kind/group/:group/version/:version/update/ns/:ns/name/:name", dynamic.Save)       // CRD
 		api.POST("/:kind/group/:group/version/:version/describe/ns/:ns/name/:name", dynamic.Describe) // CRD
 		api.GET("/:kind/group/:group/version/:version/list/ns/:ns", dynamic.List)                     // CRD
+		api.POST("/:kind/group/:group/version/:version/list/ns/:ns", dynamic.List)                    // CRD
 		api.GET("/:kind/group/:group/version/:version/list", dynamic.List)                            // CRD
+		api.POST("/:kind/group/:group/version/:version/list", dynamic.List)                           // CRD
 		// k8s pod
 		api.GET("/pod/logs/sse/ns/:ns/pod_name/:pod_name/container/:container_name", pod.StreamLogs)
 		api.GET("/pod/logs/download/ns/:ns/pod_name/:pod_name/container/:container_name", pod.DownloadLogs)
