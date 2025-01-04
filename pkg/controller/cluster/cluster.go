ce6a8aace49d7ef58bd774ec864325710af1dd73
weibaohui <weibaohui@yeah.net>
2025-01-04 17:18:35 +0800

    feat(controller): 添加集群管理相关 API 接口
    - 实现了集群列表展示、集群扫描、集群重新连接等功能
    - 添加了设置默认集群的接口，并使用 cookie进行存储
    - 通过 gin 框架来处理 HTTP 请求和响应

---
+++ pkg/controller/cluster/cluster.go
@@ -0,0 +1,39 @@
+package cluster
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/service"
+)
+
+func List(c *gin.Context) {
+	clusters := service.ClusterService().AllClusters()
+	amis.WriteJsonData(c, clusters)
+}
+
+func Scan(c *gin.Context) {
+	service.ClusterService().Scan()
+	amis.WriteJsonData(c, "ok")
+}
+
+func Reconnect(c *gin.Context) {
+	fileName := c.Param("fileName")
+	contextName := c.Param("contextName")
+	service.ClusterService().Reconnect(fileName, contextName)
+	amis.WriteJsonOKMsg(c, "已执行，请查看最新状态")
+}
+
+func SetDefault(c *gin.Context) {
+	fileName := c.Param("fileName")
+	contextName := c.Param("contextName")
+	cookieValue := fileName + "/" + contextName
+	c.SetCookie(
+		"defaultCluster",
+		cookieValue,
+		3600,
+		"/",
+		"",
+		false,
+		false,
+	)
+}
