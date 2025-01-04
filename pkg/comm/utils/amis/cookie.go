d93adae7ac6d7c3e6be9c782dbe6a8cc349cd57a
weibaohui <weibaohui@yeah.net>
2025-01-04 18:27:05 +0800

    fix(comm): 优化选中集群的获取逻辑
    - 移除了错误处理，改为检查 cookie 是否为空
    - 如果没有选中的集群 cookie，尝试从已连接的集群中获取第一个集群
    - 如果存在集群，构造选中集群的值并返回
    - 如果没有连接的集群，返回空字符串

--- pkg/comm/utils/amis/cookie.go
+++ pkg/comm/utils/amis/cookie.go
@@ -1,13 +1,21 @@
 package amis
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/service"
 )
 
 func GetSelectedCluster(c *gin.Context) string {
-	selectedCluster, err := c.Cookie("selectedCluster")
-	if err != nil {
-		return "InCluster"
+	selectedCluster, _ := c.Cookie("selectedCluster")
+	if selectedCluster == "" {
+		clusters := service.ClusterService().ConnectedClusters()
+		if len(clusters) > 0 {
+			c := clusters[0]
+			selectedCluster = fmt.Sprintf("%s/%s", c.FileName, c.ContextName)
+			return selectedCluster
+		}
 	}
 	return selectedCluster
 }
