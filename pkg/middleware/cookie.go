7bf6840e880233f619bc75165eba33b9218a83ba
weibaohui <weibaohui@yeah.net>
2025-01-05 19:15:06 +0800

    feat(middleware): 增加 InCluster 集群判断逻辑
    - 引入 flag 包，用于获取配置信息
    - 在 Cookie 不存在时，增加对 InCluster 配置的判断
    - 如果 InCluster 为 true，则将 clusterID 设置为 "InCluster"- 否则，从集群中选择一个默认集群

--- pkg/middleware/cookie.go
+++ pkg/middleware/cookie.go
@@ -4,29 +4,35 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/flag"
 	"github.com/weibaohui/k8m/pkg/service"
 )
 
 func EnsureSelectedClusterMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		// 检查是否存在名为 "selectedCluster" 的 Cookie
 		_, err := c.Cookie("selectedCluster")
 		if err != nil {
-			clusterID := service.ClusterService().FirstClusterID()
-			if clusterID == "" {
-				c.Next()
+			var clusterID string
+			cfg := flag.Init()
+			if cfg.InCluster {
+				clusterID = "InCluster"
 			} else {
-				// 如果 Cookie 不存在，写入一个默认值
-				c.SetCookie(
-					"selectedCluster",           // Cookie 名称
-					clusterID,                   // Cookie 默认值
-					int(24*time.Hour.Seconds()), // 有效期（秒），这里是 1 天
-					"/",                         // Cookie 路径
-					"",                          // 域名，默认当前域
-					false,                       // 是否仅 HTTPS
-					false,                       // 是否 HttpOnly
-				)
+				// 从集群中选择一个
+				clusterID = service.ClusterService().FirstClusterID()
 			}
+
+			// 如果 Cookie 不存在，写入一个默认值
+			c.SetCookie(
+				"selectedCluster",           // Cookie 名称
+				clusterID,                   // Cookie 默认值
+				int(24*time.Hour.Seconds()), // 有效期（秒），这里是 1 天
+				"/",                         // Cookie 路径
+				"",                          // 域名，默认当前域
+				false,                       // 是否仅 HTTPS
+				false,                       // 是否 HttpOnly
+			)
 
 		}
 
