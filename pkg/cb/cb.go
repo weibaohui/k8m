992a8856cfef47f841f4baa321f87ea8f5aaf91d
weibaohui <weibaohui@yeah.net>
2025-01-06 15:40:33 +0800

    feat(cb): 更新回调注册和日志记录功能
    - 注册 get、exec 和 streamExec 回调
    - 优化 Get 函数，添加日志记录
    - 新增 Audit 函数，用于 exec 和 streamExec 回调
    - 重构代码，提高可读性和可维护性

--- pkg/cb/cb.go
+++ pkg/cb/cb.go
@@ -1,6 +1,9 @@
 package cb
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/weibaohui/k8m/pkg/service"
 	"github.com/weibaohui/kom/kom"
 	"k8s.io/klog/v2"
@@ -12,14 +15,25 @@ func RegisterCallback() {
 		selectedCluster := service.ClusterService().ClusterID(cluster)
 		klog.V(6).Infof("注册回调%s", selectedCluster)
 		queryCallback := kom.Cluster(selectedCluster).Callback().Get()
-		_ = queryCallback.Register("k8m:get11", Get)
+		_ = queryCallback.Register("k8m:get", Get)
+		execCallback := kom.Cluster(selectedCluster).Callback().Exec()
+		_ = execCallback.Register("k8m:exec", Audit)
+		streamExecCallback := kom.Cluster(selectedCluster).Callback().StreamExec()
+		_ = streamExecCallback.Register("k8m:streamExec", Audit)
 	}
 
 }
 
 func Get(k8s *kom.Kubectl) error {
-	// // todo 在这里可以统一进行权限认证等操作，返回error即可阻断执行
-	// u := k8s.Statement.Context.Value("user")
-	// klog.V(2).Infof("%s k8s Get \n", u)
+	stmt := k8s.Statement
+	cluster := k8s.ID
+	klog.V(2).Infof("k8s [%s] Get %s %s/%s \n", cluster, stmt.GVR.Resource, stmt.Namespace, stmt.Name)
+	return nil
+}
+func Audit(k8s *kom.Kubectl) error {
+	stmt := k8s.Statement
+	cluster := k8s.ID
+	cmd := fmt.Sprintf("%s %s", stmt.Command, strings.Join(stmt.Args, " "))
+	klog.V(2).Infof("k8s [%s] Exec cmd in %s %s/%s : %s \n", cluster, stmt.GVR.Resource, stmt.Namespace, stmt.Name, cmd)
 	return nil
 }
