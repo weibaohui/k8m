6e829b8bcb1f337931f92a49a907f51071f1f8e2
weibaohui <weibaohui@yeah.net>
2025-01-06 11:26:51 +0800

    feat(cluster): 增加集群状态聚合功能
    - 在 ClusterConfig 中添加 NodeStatusAggregated 和 PodStatusAggregated 字段
    - 实现 SetNodeStatusAggregated、SetPodStatusAggregated、GetNodeStatusAggregated 和 GetPodStatusAggregated 方法
    - 添加 DelayStartFunc 方法，用于延迟启动聚合任务
    - 优化 Scan 方法，改名为 RegisterClustersInPath

--- pkg/service/clusters.go
+++ pkg/service/clusters.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 
 	"github.com/duke-git/lancet/v2/slice"
+	"github.com/robfig/cron/v3"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/flag"
 	"github.com/weibaohui/kom/kom"
 	"k8s.io/client-go/kubernetes"
@@ -16,9 +18,69 @@ import (
 )
 
 type clusterService struct {
-	clusterConfigs []*ClusterConfig // 文件名+context名称 -> 集群配置
+	clusterConfigs        []*ClusterConfig // 文件名+context名称 -> 集群配置
+	AggregateDelaySeconds int              // 聚合延迟时间
 }
 
+func (c *clusterService) SetNodeStatusAggregated(selectedCluster string, true bool) {
+	clusterConfig := c.GetClusterByID(selectedCluster)
+	if clusterConfig == nil {
+		return
+	}
+	clusterConfig.NodeStatusAggregated = true
+}
+
+// SetPodStatusAggregated 设置Pod状态聚合
+func (c *clusterService) SetPodStatusAggregated(selectedCluster string, true bool) {
+	clusterConfig := c.GetClusterByID(selectedCluster)
+	if clusterConfig == nil {
+		return
+	}
+	clusterConfig.PodStatusAggregated = true
+}
+
+// GetPodStatusAggregated 获取指定集群的Pod聚合状态
+func (c *clusterService) GetPodStatusAggregated(selectedCluster string) bool {
+	clusterConfig := c.GetClusterByID(selectedCluster)
+	if clusterConfig == nil {
+		return false
+	}
+	return clusterConfig.PodStatusAggregated
+}
+
+// GetNodeStatusAggregated 获取指定集群的Node聚合状态
+func (c *clusterService) GetNodeStatusAggregated(selectedCluster string) bool {
+	clusterConfig := c.GetClusterByID(selectedCluster)
+	if clusterConfig == nil {
+		return false
+	}
+	return clusterConfig.NodeStatusAggregated
+}
+
+// GetClusterByID 获取ClusterConfig
+func (c *clusterService) GetClusterByID(selectedCluster string) *ClusterConfig {
+	if selectedCluster == "" {
+		return nil
+	}
+	// 解析selectedCluster
+	clusterID := strings.Split(selectedCluster, "/")
+	fileName := clusterID[0]
+	contextName := clusterID[1]
+	for _, clusterConfig := range c.clusterConfigs {
+		if clusterConfig.FileName == fileName && clusterConfig.ContextName == contextName {
+			return clusterConfig
+		}
+	}
+	return nil
+}
+
+func (c *clusterService) DelayStartFunc(f func()) {
+	// 延迟启动cron
+	// 设置一次性任务的执行时间，例如 5 秒后执行
+	schedule := utils.DelayStartSchedule(c.AggregateDelaySeconds)
+	_, _ = cron.New().AddFunc(schedule, f)
+	klog.V(6).Infof("延迟启动cron %ds: %s", c.AggregateDelaySeconds, schedule)
+}
 func (c *clusterService) Reconnect(fileName string, contextName string) {
 	// 先清除原来的状态
 
@@ -35,7 +97,7 @@ func (c *clusterService) Reconnect(fileName string, contextName string) {
 func (c *clusterService) Scan() {
 	c.clusterConfigs = []*ClusterConfig{}
 	cfg := flag.Init()
-	c.ListClustersInPath(cfg.KubeConfig)
+	c.RegisterClustersInPath(cfg.KubeConfig)
 }
 
 func (c *clusterService) AllClusters() []*ClusterConfig {
@@ -64,18 +126,20 @@ func (c *clusterService) FirstClusterID() string {
 }
 
 type ClusterConfig struct {
-	FileName      string       `json:"fileName,omitempty"`      // kubeconfig 文件名称
-	ContextName   string       `json:"contextName,omitempty"`   // context名称
-	ClusterName   string       `json:"clusterName,omitempty"`   // 集群名称
-	Server        string       `json:"server,omitempty"`        // 集群地址
-	ServerVersion string       `json:"serverVersion,omitempty"` // 通过这个值来判断集群是否可用
-	UserName      string       `json:"userName,omitempty"`      // 用户名
-	restConfig    *rest.Config // 直连rest.Config
-	kubeConfig    []byte       // 集群配置.kubeconfig原始文件内容
-	Err           string       `json:"err,omitempty"` // 连接错误信息
+	FileName             string       `json:"fileName,omitempty"`             // kubeconfig 文件名称
+	ContextName          string       `json:"contextName,omitempty"`          // context名称
+	ClusterName          string       `json:"clusterName,omitempty"`          // 集群名称
+	Server               string       `json:"server,omitempty"`               // 集群地址
+	ServerVersion        string       `json:"serverVersion,omitempty"`        // 通过这个值来判断集群是否可用
+	UserName             string       `json:"userName,omitempty"`             // 用户名
+	Err                  string       `json:"err,omitempty"`                  // 连接错误信息
+	NodeStatusAggregated bool         `json:"nodeStatusAggregated,omitempty"` // 是否已聚合节点状态
+	PodStatusAggregated  bool         `json:"podStatusAggregated,omitempty"`  // 是否已聚合容器组状态
+	restConfig           *rest.Config // 直连rest.Config
+	kubeConfig           []byte       // 集群配置.kubeconfig原始文件内容
 }
 
-func (c *clusterService) ListClustersInPath(path string) {
+func (c *clusterService) RegisterClustersInPath(path string) {
 	// 1. 通过kubeconfig文件，找到所在目录
 	dir := filepath.Dir(path)
 
