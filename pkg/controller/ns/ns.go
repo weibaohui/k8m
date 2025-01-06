e4f79e8aad261d48e180766c8a9eec3fa4bc1b1d
weibaohui <weibaohui@yeah.net>
2025-01-06 14:36:34 +0800

    feat(ns): 优化命名空间获取逻辑
    - 先判断 kubeconfig 中是否限制了命名空间
    - 如果限制了，从 cluster 实例中获取命名空间
    - 如果没有限制，从集群中获取所有命名空间
    - 优化了命名空间列表的构建过程

--- pkg/controller/ns/ns.go
+++ pkg/controller/ns/ns.go
@@ -3,6 +3,7 @@ package ns
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/service"
 	"github.com/weibaohui/kom/kom"
 	v1 "k8s.io/api/core/v1"
 )
@@ -11,13 +12,30 @@ func OptionList(c *gin.Context) {
 	ctx := c.Request.Context()
 	selectedCluster := amis.GetSelectedCluster(c)
 
+	var list []map[string]string
+	// 先判断kubeconfig中是否限制了namespace
+	// 1、如果限制了，那么从cluster 实例中取
+	// 2、如果没有限制，那么从集群中取
+	cluster := service.ClusterService().GetClusterByID(selectedCluster)
+	if cluster != nil && cluster.Namespace != "" {
+		list = append(list, map[string]string{
+			"label": cluster.Namespace,
+			"value": cluster.Namespace,
+		})
+		amis.WriteJsonData(c, gin.H{
+			"options": list,
+		})
+		return
+	}
+
+	// 没有指定的情况
 	var ns []v1.Namespace
 	err := kom.Cluster(selectedCluster).WithContext(ctx).Resource(&v1.Namespace{}).List(&ns).Error
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
 	}
-	var list []map[string]string
+
 	for _, n := range ns {
 		list = append(list, map[string]string{
 			"label": n.Name,
