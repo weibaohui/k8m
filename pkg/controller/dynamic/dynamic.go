5ec56a092a526008a8655695a0b3fc50570044f6
weibaohui <weibaohui@yeah.net>
2025-01-17 16:10:45 +0800

    refactor(controller/dynamic): 重构 CRD 相关接口并新增树形结构数据处理
    -重构原有的 CRD 列表获取逻辑，提取为独立函数 getCrdList
    - 新增 getCrdGroupList 函数用于获取 CRD组列表
    - 新增 getCrdKindListByGroup 函数用于获取指定组的 CRD 类型列表
    - 新增 CrdGuidTree 和 CrdGuidTreeThree 函数，用于生成 CRD 树形结构数据
    - 优化数据处理逻辑，提高代码复用性和可维护性

--- pkg/controller/dynamic/dynamic.go
+++ pkg/controller/dynamic/dynamic.go
@@ -559,31 +559,7 @@ func GroupOptionList(c *gin.Context) {
 	ctx := c.Request.Context()
 	selectedCluster := amis.GetSelectedCluster(c)
 
-	var list []unstructured.Unstructured
-	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
-		"apiextensions.k8s.io",
-		"v1",
-		"CustomResourceDefinition").
-		WithCache(time.Second * 30).
-		List(&list).Error
-	if err != nil {
-		amis.WriteJsonData(c, gin.H{
-			"options": make([]map[string]string, 0),
-		})
-		return
-	}
-
-	var groups []string
-	for _, item := range list {
-		group, found, err := unstructured.NestedString(item.Object, "spec", "group")
-		if err != nil || !found {
-			continue
-		}
-
-		groups = append(groups, group)
-	}
-	groups = slice.Unique(groups)
-	slice.Sort(groups, "asc")
+	groups := getCrdGroupList(ctx, selectedCluster)
 	var options []map[string]string
 	for _, n := range groups {
 		options = append(options, map[string]string{
@@ -608,30 +584,8 @@ func KindOptionList(c *gin.Context) {
 		})
 		return
 	}
-	var list []unstructured.Unstructured
-	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
-		"apiextensions.k8s.io",
-		"v1",
-		"CustomResourceDefinition").
-		Where("`spec.group`=?", g).
-		WithCache(time.Second * 30).
-		List(&list).Error
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
 
-	var kinds []string
-	for _, item := range list {
-		kind, found, err := unstructured.NestedString(item.Object, "spec", "names", "kind")
-		if err != nil || !found {
-			continue
-		}
-
-		kinds = append(kinds, kind)
-	}
-	kinds = slice.Unique(kinds)
-	slice.Sort(kinds, "asc")
+	kinds := getCrdKindListByGroup(ctx, selectedCluster, g)
 
 	var options []map[string]string
 	for _, n := range kinds {
@@ -644,4 +598,176 @@ func KindOptionList(c *gin.Context) {
 	amis.WriteJsonData(c, gin.H{
 		"options": options,
 	})
+}
+
+type CrdTree struct {
+	Label    string     `json:"label,omitempty"`
+	Value    string     `json:"value,omitempty"`
+	Group    string     `json:"group,omitempty"`
+	Kind     string     `json:"kind,omitempty"`
+	Version  string     `json:"version,omitempty"`
+	Children []*CrdTree `json:"children"`
+}
+
+func CrdGuidTreeThree(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	list, err := getCrdList(ctx, selectedCluster)
+
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	//
+	var crdTreeList []*CrdTree
+
+	for _, item := range list {
+		group, found, err := unstructured.NestedString(item.Object, "spec", "group")
+		if err != nil || !found {
+			continue
+		}
+		groupNode := &CrdTree{
+			Label:    group,
+			Value:    group,
+			Group:    group,
+			Children: make([]*CrdTree, 0),
+		}
+		crdTreeList = append(crdTreeList, groupNode)
+
+		kind, found, err := unstructured.NestedString(item.Object, "spec", "names", "kind")
+		if err != nil || !found {
+			continue
+		}
+		kindNode := &CrdTree{
+			Label:    kind,
+			Value:    kind,
+			Group:    group,
+			Kind:     kind,
+			Children: make([]*CrdTree, 0),
+		}
+		groupNode.Children = append(
+			groupNode.Children, kindNode)
+
+		// 获取 spec.versions 数组
+		versions, found, err := unstructured.NestedSlice(item.Object, "spec", "versions")
+		if err != nil || !found {
+			continue
+		}
+
+		// 提取每个版本的 name
+		for _, version := range versions {
+			versionMap, ok := version.(map[string]interface{})
+			if !ok {
+				continue
+			}
+			version, found, err := unstructured.NestedString(versionMap, "name")
+			if err != nil || !found {
+				continue
+			}
+
+			versionNode := &CrdTree{
+				Label:   version,
+				Value:   version,
+				Group:   group,
+				Kind:    kind,
+				Version: version,
+			}
+
+			kindNode.Children = append(kindNode.Children, versionNode)
+
+		}
+
+	}
+
+	amis.WriteJsonData(c, gin.H{
+		"options": crdTreeList,
+	})
+}
+func CrdGuidTree(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	groups := getCrdGroupList(ctx, selectedCluster)
+	var crdTreeList []*CrdTree
+	for _, group := range groups {
+		node := &CrdTree{
+			Label:    group,
+			Value:    group,
+			Group:    group,
+			Children: make([]*CrdTree, 0),
+		}
+		crdTreeList = append(crdTreeList, node)
+		kinds := getCrdKindListByGroup(ctx, selectedCluster, group)
+		for _, kind := range kinds {
+			kindNode := &CrdTree{
+				Label:    kind,
+				Value:    kind,
+				Group:    group,
+				Kind:     kind,
+				Children: make([]*CrdTree, 0),
+			}
+			node.Children = append(node.Children, kindNode)
+		}
+	}
+	amis.WriteJsonData(c, gin.H{
+		"options": crdTreeList,
+	})
+}
+
+func getCrdGroupList(ctx context.Context, selectedCluster string) []string {
+	list, err := getCrdList(ctx, selectedCluster)
+	if err != nil {
+		return make([]string, 0)
+	}
+
+	var groups []string
+	for _, item := range list {
+		group, found, err := unstructured.NestedString(item.Object, "spec", "group")
+		if err != nil || !found {
+			continue
+		}
+
+		groups = append(groups, group)
+	}
+	groups = slice.Unique(groups)
+	slice.Sort(groups, "asc")
+	return groups
+}
+
+func getCrdList(ctx context.Context, selectedCluster string) ([]unstructured.Unstructured, error) {
+	var list []unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
+		"apiextensions.k8s.io",
+		"v1",
+		"CustomResourceDefinition").
+		WithCache(time.Second * 30).
+		List(&list).Error
+	return list, err
+}
+func getCrdKindListByGroup(ctx context.Context, selectedCluster string, group string) []string {
+	var list []unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
+		"apiextensions.k8s.io",
+		"v1",
+		"CustomResourceDefinition").
+		Where("`spec.group`=?", group).
+		WithCache(time.Second * 30).
+		List(&list).Error
+	if err != nil {
+		return make([]string, 0)
+	}
+
+	var kinds []string
+	for _, item := range list {
+		kind, found, err := unstructured.NestedString(item.Object, "spec", "names", "kind")
+		if err != nil || !found {
+			continue
+		}
+		kinds = append(kinds, kind)
+	}
+	kinds = slice.Unique(kinds)
+	slice.Sort(kinds, "asc")
+	return kinds
 }
