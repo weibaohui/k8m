f1a7ddfb8092adfe97dd4ec8558181a4613a76fb
weibaohui <weibaohui@yeah.net>
2025-01-17 16:30:53 +0800

    refactor(controller): 重构 Dynamic API
    - 将 UpdateLabels 和 UpdateAnnotations 函数移至新文件- 新增 annotation.go 和 label.go 文件，分别处理注解和标签更新
    - 将 CRD相关函数移至 crd_option.go 文件
    - 优化了代码结构，提高了可维护性

---
+++ pkg/controller/dynamic/annotation.go
@@ -0,0 +1,94 @@
+package dynamic
+
+import (
+	"fmt"
+
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func UpdateAnnotations(c *gin.Context) {
+	ns := c.Param("ns")
+	name := c.Param("name")
+	kind := c.Param("kind")
+	group := c.Param("group")
+	version := c.Param("version")
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	var req struct {
+		Annotations map[string]interface{} `json:"annotations"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	// 部分key为k8m增加的指标数据，不是资源自身的注解，因此过滤掉。
+	// last-applied-configuration是k8s管理的，不允许修改。
+	var immutableKeys = []string{
+		"cpu.request",
+		"cpu.requestFraction",
+		"cpu.limit",
+		"cpu.limitFraction",
+		"cpu.total",
+		"memory.request",
+		"memory.requestFraction",
+		"memory.limit",
+		"memory.limitFraction",
+		"memory.total",
+		"ip.usage.total",
+		"ip.usage.used",
+		"ip.usage.available",
+		"pod.count.total",
+		"pod.count.used",
+		"pod.count.available",
+		"kubectl.kubernetes.io/last-applied-configuration",
+	}
+
+	// 判断下前台传来的annotations是否是immutableKeys中的key，如果是则不允许修改
+	// 创建一个新的map，用于存储过滤后的annotations
+	filteredAnnotations := make(map[string]string)
+
+	for k, v := range req.Annotations {
+		if !slice.Contain(immutableKeys, k) {
+			filteredAnnotations[k] = fmt.Sprintf("%s", v)
+		}
+	}
+
+	// 判断是否还有值，有值再更新
+	if len(filteredAnnotations) == 0 {
+		amis.WriteJsonOK(c)
+		return
+	}
+	var obj *unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).WithContext(ctx).
+		Name(name).Namespace(ns).
+		CRD(group, version, kind).
+		Get(&obj).Error
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	// 单独处理kubectl.kubernetes.io/last-applied-configuration
+	// 这个要用原来的覆盖
+	if obj.GetAnnotations()["kubectl.kubernetes.io/last-applied-configuration"] != "" {
+		filteredAnnotations["kubectl.kubernetes.io/last-applied-configuration"] = obj.GetAnnotations()["kubectl.kubernetes.io/last-applied-configuration"]
+	}
+	obj.SetAnnotations(filteredAnnotations)
+
+	err = kom.Cluster(selectedCluster).WithContext(ctx).
+		Name(name).Namespace(ns).
+		CRD(group, version, kind).
+		Update(obj).Error
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	amis.WriteJsonOK(c)
+}
---
+++ pkg/controller/dynamic/crd_option.go
@@ -0,0 +1,233 @@
+package dynamic
+
+import (
+	"context"
+	"time"
+
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func GroupOptionList(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	groups := getCrdGroupList(ctx, selectedCluster)
+	var options []map[string]string
+	for _, n := range groups {
+		options = append(options, map[string]string{
+			"label": n,
+			"value": n,
+		})
+	}
+
+	amis.WriteJsonData(c, gin.H{
+		"options": options,
+	})
+}
+
+func KindOptionList(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+	g := c.Query("spec[group]")
+	if g == "" {
+		// 还没选group
+		amis.WriteJsonData(c, gin.H{
+			"options": make([]map[string]string, 0),
+		})
+		return
+	}
+
+	kinds := getCrdKindListByGroup(ctx, selectedCluster, g)
+
+	var options []map[string]string
+	for _, n := range kinds {
+		options = append(options, map[string]string{
+			"label": n,
+			"value": n,
+		})
+	}
+
+	amis.WriteJsonData(c, gin.H{
+		"options": options,
+	})
+}
+
+// Deprecated: 废弃，请不要使用
+type CrdTree struct {
+	Label    string     `json:"label,omitempty"`
+	Value    string     `json:"value,omitempty"`
+	Group    string     `json:"group,omitempty"`
+	Kind     string     `json:"kind,omitempty"`
+	Version  string     `json:"version,omitempty"`
+	Children []*CrdTree `json:"children"`
+}
+
+// Deprecated: 废弃，请不要使用
+func CrdGuidTreeThree(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	list, err := getCrdList(ctx, selectedCluster)
+
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	//
+	var crdTreeList []*CrdTree
+
+	for _, item := range list {
+		group, found, err := unstructured.NestedString(item.Object, "spec", "group")
+		if err != nil || !found {
+			continue
+		}
+		groupNode := &CrdTree{
+			Label:    group,
+			Value:    group,
+			Group:    group,
+			Children: make([]*CrdTree, 0),
+		}
+		crdTreeList = append(crdTreeList, groupNode)
+
+		kind, found, err := unstructured.NestedString(item.Object, "spec", "names", "kind")
+		if err != nil || !found {
+			continue
+		}
+		kindNode := &CrdTree{
+			Label:    kind,
+			Value:    kind,
+			Group:    group,
+			Kind:     kind,
+			Children: make([]*CrdTree, 0),
+		}
+		groupNode.Children = append(
+			groupNode.Children, kindNode)
+
+		// 获取 spec.versions 数组
+		versions, found, err := unstructured.NestedSlice(item.Object, "spec", "versions")
+		if err != nil || !found {
+			continue
+		}
+
+		// 提取每个版本的 name
+		for _, version := range versions {
+			versionMap, ok := version.(map[string]interface{})
+			if !ok {
+				continue
+			}
+			version, found, err := unstructured.NestedString(versionMap, "name")
+			if err != nil || !found {
+				continue
+			}
+
+			versionNode := &CrdTree{
+				Label:   version,
+				Value:   version,
+				Group:   group,
+				Kind:    kind,
+				Version: version,
+			}
+
+			kindNode.Children = append(kindNode.Children, versionNode)
+
+		}
+
+	}
+
+	amis.WriteJsonData(c, gin.H{
+		"options": crdTreeList,
+	})
+}
+
+// Deprecated: 废弃，请不要使用
+func CrdGuidTree(c *gin.Context) {
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	groups := getCrdGroupList(ctx, selectedCluster)
+	var crdTreeList []*CrdTree
+	for _, group := range groups {
+		node := &CrdTree{
+			Label:    group,
+			Value:    group,
+			Group:    group,
+			Children: make([]*CrdTree, 0),
+		}
+		crdTreeList = append(crdTreeList, node)
+		kinds := getCrdKindListByGroup(ctx, selectedCluster, group)
+		for _, kind := range kinds {
+			kindNode := &CrdTree{
+				Label:    kind,
+				Value:    kind,
+				Group:    group,
+				Kind:     kind,
+				Children: make([]*CrdTree, 0),
+			}
+			node.Children = append(node.Children, kindNode)
+		}
+	}
+	amis.WriteJsonData(c, gin.H{
+		"options": crdTreeList,
+	})
+}
+
+func getCrdGroupList(ctx context.Context, selectedCluster string) []string {
+	list, err := getCrdList(ctx, selectedCluster)
+	if err != nil {
+		return make([]string, 0)
+	}
+
+	var groups []string
+	for _, item := range list {
+		group, found, err := unstructured.NestedString(item.Object, "spec", "group")
+		if err != nil || !found {
+			continue
+		}
+
+		groups = append(groups, group)
+	}
+	groups = slice.Unique(groups)
+	slice.Sort(groups, "asc")
+	return groups
+}
+
+func getCrdList(ctx context.Context, selectedCluster string) ([]unstructured.Unstructured, error) {
+	var list []unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
+		"apiextensions.k8s.io",
+		"v1",
+		"CustomResourceDefinition").
+		WithCache(time.Second * 30).
+		List(&list).Error
+	return list, err
+}
+func getCrdKindListByGroup(ctx context.Context, selectedCluster string, group string) []string {
+	var list []unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).WithContext(ctx).GVK(
+		"apiextensions.k8s.io",
+		"v1",
+		"CustomResourceDefinition").
+		Where("`spec.group`=?", group).
+		WithCache(time.Second * 30).
+		List(&list).Error
+	if err != nil {
+		return make([]string, 0)
+	}
+
+	var kinds []string
+	for _, item := range list {
+		kind, found, err := unstructured.NestedString(item.Object, "spec", "names", "kind")
+		if err != nil || !found {
+			continue
+		}
+		kinds = append(kinds, kind)
+	}
+	kinds = slice.Unique(kinds)
+	slice.Sort(kinds, "asc")
+	return kinds
+}
