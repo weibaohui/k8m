3f0d8f61593f67bd47e4377dbd6268e53a4b5738
weibaohui <weibaohui@yeah.net>
2025-01-26 23:50:34 +0800

    feat(controller): 添加 Pod 反亲和性操作支持
    - 实现了列出、添加、更新和删除 Pod 反亲和性规则的功能
    - 支持动态 CRD 资源的 Pod 反亲和性配置
    - 优化了 Pod 反亲和性规则的解析和处理逻辑

---
+++ pkg/controller/dynamic/pod_anti_affinity.go
@@ -0,0 +1,246 @@
+package dynamic
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+)
+
+// ListPodAntiAffinity 获取 Pod 亲和性列表
+func ListPodAntiAffinity(c *gin.Context) {
+	name := c.Param("name")
+	ns := c.Param("ns")
+	group := c.Param("group")
+	kind := c.Param("kind")
+	version := c.Param("version")
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	// 先获取资源中的定义
+	var item unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).
+		WithContext(ctx).
+		CRD(group, version, kind).
+		Namespace(ns).Name(name).
+		Get(&item).Error
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	// 获取资源路径
+	paths, err := getResourcePaths(kind)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	requiredAntiAffinityPath := append(paths, "affinity", "podAntiAffinity", "requiredDuringSchedulingIgnoredDuringExecution")
+	// 获取 Affinity 配置并解析
+	affinity, found, err := unstructured.NestedFieldNoCopy(item.Object, requiredAntiAffinityPath...)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	if !found {
+		// 没有的话，返回一个空列表
+		amis.WriteJsonList(c, []interface{}{})
+		return
+	}
+
+	// 强制转换为数组
+	list, ok := affinity.([]interface{})
+	if !ok {
+		amis.WriteJsonError(c, fmt.Errorf("list is not an array"))
+		return
+	}
+	amis.WriteJsonList(c, list)
+}
+
+func AddPodAntiAffinity(c *gin.Context) {
+	processPodAntiAffinity(c, "add")
+}
+func UpdatePodAntiAffinity(c *gin.Context) {
+	processPodAffinity(c, "modify")
+}
+func DeletePodAntiAffinity(c *gin.Context) {
+	processPodAntiAffinity(c, "delete")
+}
+func processPodAntiAffinity(c *gin.Context, action string) {
+	name := c.Param("name")
+	ns := c.Param("ns")
+	group := c.Param("group")
+	kind := c.Param("kind")
+	version := c.Param("version")
+	ctx := c.Request.Context()
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	var info podAffinity
+
+	if err := c.ShouldBindJSON(&info); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	// 先获取资源中的定义
+	var item unstructured.Unstructured
+	err := kom.Cluster(selectedCluster).
+		WithContext(ctx).
+		CRD(group, version, kind).
+		Namespace(ns).Name(name).
+		Get(&item).Error
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	originalPodAffinity, err := getPodAntiAffinityTerms(kind, &item, action, info)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	patchData, err := generateRequiredPodAntiAffinityDynamicPatch(kind, originalPodAffinity)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	patchJSON := utils.ToJSON(patchData)
+	klog.V(6).Infof("UpdatePodAffinity Patch JSON :\n%s\n", patchJSON)
+	var obj interface{}
+	err = kom.Cluster(selectedCluster).
+		WithContext(ctx).
+		CRD(group, version, kind).
+		Namespace(ns).Name(name).
+		Patch(&obj, types.MergePatchType, patchJSON).Error
+	amis.WriteJsonErrorOrOK(c, err)
+}
+
+// getPodAffinityTerms 获取 PodAffinity 的 Affinity Terms
+// action : modify\update\add
+func getPodAntiAffinityTerms(kind string, item *unstructured.Unstructured, action string, rule podAffinity) ([]interface{}, error) {
+
+	// 获取资源路径
+	paths, err := getResourcePaths(kind)
+	if err != nil {
+		return nil, err
+	}
+	requiredAntiAffinityPath := append(paths, "affinity", "podAntiAffinity", "requiredDuringSchedulingIgnoredDuringExecution")
+
+	// 获取 Affinity 配置并解析
+	affinity, found, err := unstructured.NestedFieldNoCopy(item.Object, requiredAntiAffinityPath...)
+	if err != nil {
+		return nil, err
+	}
+	if !found {
+		// 如果没有，那么如果是 add 操作，那么创建一个空的 podAffinityTerms
+		affinity = make([]interface{}, 0)
+	}
+
+	// 强制转换为数组
+	podAffinityTerms, ok := affinity.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("podAffinityTerms is not an array")
+	}
+
+	x := utils.ToJSON(affinity)
+	// 如果nodeSelectorTerms被设置为-{},那么json输出为
+	// [
+	//  {}
+	// ]
+	// 其长度为8
+	if (len(podAffinityTerms) == 0 || len(x) == 8) && action == "add" {
+		podAffinityTerms = []interface{}{
+			map[string]interface{}{
+				"labelSelector": map[string]interface{}{
+					"matchLabels": rule.LabelSelector.MatchLabels,
+				},
+				"topologyKey": rule.TopologyKey,
+			},
+		}
+		return podAffinityTerms, nil
+	}
+
+	// 进行操作：删除或新增
+	var newPodAffinityTerms []interface{}
+	for _, term := range podAffinityTerms {
+		termMap, ok := term.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		// 获取 topologyKey
+		topologyKey, found, err := unstructured.NestedFieldNoCopy(termMap, "topologyKey")
+		if err != nil {
+			return nil, err
+		}
+		if !found {
+			continue
+		}
+
+		// 处理每一个 matchLabels
+		if action == "delete" {
+			if topologyKey == rule.TopologyKey {
+				// 如果是删除，并且 matchLabels 和 topologyKey 匹配，跳过这个 term
+				continue
+			}
+		}
+
+		// 如果是修改操作，并且 matchLabels 和 topologyKey 匹配，修改该 term 的 matchLabels
+		if action == "modify" && topologyKey == rule.TopologyKey {
+			// 修改新值，不修改原值，直接添加到新列表
+			newPodAffinityTerms = append(newPodAffinityTerms, map[string]interface{}{
+				"labelSelector": map[string]interface{}{
+					"matchLabels": rule.LabelSelector.MatchLabels,
+				},
+				"topologyKey": rule.TopologyKey,
+			})
+			continue
+		}
+
+		// 否则，保留原有的 term
+		newPodAffinityTerms = append(newPodAffinityTerms, termMap)
+	}
+
+	// 如果是新增操作，增加新的 podAffinityTerm
+	if action == "add" {
+		newPodAffinityTerms = append(newPodAffinityTerms, map[string]interface{}{
+			"labelSelector": map[string]interface{}{
+				"matchLabels": rule.LabelSelector.MatchLabels,
+			},
+			"topologyKey": rule.TopologyKey,
+		})
+	}
+
+	return newPodAffinityTerms, nil
+}
+
+// 生成动态的 patch 数据
+func generateRequiredPodAntiAffinityDynamicPatch(kind string, terms []interface{}) (map[string]interface{}, error) {
+	// 获取资源路径
+	paths, err := getResourcePaths(kind)
+	if err != nil {
+		return nil, err
+	}
+	requiredAntiAffinityPath := append(paths, "affinity", "podAntiAffinity")
+
+	// 动态构造 patch 数据
+	patch := make(map[string]interface{})
+	current := patch
+
+	// 按层级动态生成嵌套结构
+	for _, path := range requiredAntiAffinityPath {
+		if _, exists := current[path]; !exists {
+			current[path] = make(map[string]interface{})
+		}
+		current = current[path].(map[string]interface{})
+	}
+
+	current["requiredDuringSchedulingIgnoredDuringExecution"] = terms
+
+	return patch, nil
+}
