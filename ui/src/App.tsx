354713968388bb854a2a566a2a684ba95e2047da
weibaohui <weibaohui@yeah.net>
2025-02-01 15:49:17 +0800

    refactor(ui): 优化 App组件中的路由选择
    - 移除了开发环境和生产环境的路由判断逻辑
    - 保留了 HashRouter 作为主要路由方式
    - 注释掉了 BrowserRouter 选项，便于未来可能的切换

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,8 @@
-import {BrowserRouter, HashRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import routes from '@/routes'
-import {isDev} from './utils/utils'
 
 const App = () => {
-    if (!isDev) {
-        return <HashRouter>{routes()}</HashRouter>
-    }
-    return <BrowserRouter basename={'/'}>{routes()}</BrowserRouter>
+    return <HashRouter>{routes()}</HashRouter>
+    // return <BrowserRouter basename={'/'}>{routes()}</BrowserRouter>
 }
 export default App
