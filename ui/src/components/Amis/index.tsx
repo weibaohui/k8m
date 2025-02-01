fe03f5891947b2bf39276923acd4502afbf96a01
weibaohui <weibaohui@yeah.net>
2025-02-01 23:21:49 +0800

    feat(ui): 添加新的自定义组件并优化现有组件
    - 新增 SimpleImageName、FormatBytes 和 FormatLsShortDate 组件
    - 重命名 WebSocketMarkdownViewer 为 WebSocketMarkdownViewerComponent
    - 添加 SSELogDownloadComponent、SSELogDisplayComponent 和 WebSocketViewerComponent
    - 更新组件注册逻辑，支持新组件

--- ui/src/components/Amis/index.tsx
+++ ui/src/components/Amis/index.tsx
@@ -6,13 +6,16 @@ import NodeRolesComponent from '@/components/Amis/custom/NodeRoles.tsx';
 import AutoConvertMemory from "@/components/Amis/custom/AutoConvertMemory.ts";
 import FilterAnnotations from "@/components/Amis/custom/FilterAnnotations.ts";
 import ShowAnnotationIcon from "@/components/Amis/custom/ShowAnnotationIcon.ts";
-import K8sAgeComponent from "@/components/Amis/custom/K8sAge.tsx";
-import K8sPodReadyComponent from "@/components/Amis/custom/K8sPodReady.tsx";
-import HighlightHtmlComponent from "@/components/Amis/custom/HighlightHtml.tsx";
-import WebSocketMarkdownViewer from "@/components/Amis/custom/WebSocketMarkdownViewer.tsx";
 import simpleImageName from "@/components/Amis/custom/SimpleImageName.ts";
 import FormatBytes from "@/components/Amis/custom/FormatBytes.ts";
 import FormatLsShortDate from "@/components/Amis/custom/FormatLsShortDate.ts";
+import K8sAgeComponent from "@/components/Amis/custom/K8sAge.tsx";
+import K8sPodReadyComponent from "@/components/Amis/custom/K8sPodReady.tsx";
+import HighlightHtmlComponent from "@/components/Amis/custom/HighlightHtml.tsx";
+import WebSocketMarkdownViewerComponent from "@/components/Amis/custom/WebSocketMarkdownViewer.tsx";
+import SSELogDownloadComponent from "@/components/Amis/custom/SSELogDownload.tsx";
+import SSELogDisplayComponent from "@/components/Amis/custom/SSELogDisplay.tsx";
+import WebSocketViewerComponent from "@/components/Amis/custom/WebSocketViewer.tsx";
 // 注册自定义组件
 registerRenderer({type: 'k8sTextConditions', component: k8sTextConditionsComponent})
 registerRenderer({type: 'nodeRoles', component: NodeRolesComponent})
@@ -22,7 +25,14 @@ registerRenderer({type: 'k8sPodReady', component: K8sPodReadyComponent})
 // @ts-ignore
 registerRenderer({type: 'highlightHtml', component: HighlightHtmlComponent})
 // @ts-ignore
-registerRenderer({type: 'webSocketMarkdownViewer', component: WebSocketMarkdownViewer})
+registerRenderer({type: 'webSocketMarkdownViewer', component: WebSocketMarkdownViewerComponent})
+// @ts-ignore
+registerRenderer({type: 'log-download', component: SSELogDownloadComponent})
+// @ts-ignore
+registerRenderer({type: 'log-display', component: SSELogDisplayComponent})
+// @ts-ignore
+registerRenderer({type: 'websocketViewer', component: WebSocketViewerComponent})
+
 // 注册过滤器
 registerFilter("autoConvertMemory", AutoConvertMemory)
 registerFilter("filterAnnotations", FilterAnnotations)
