f515dea0bd71656b0817072d04afe70639e493cf
weibaohui <weibaohui@yeah.net>
2025-02-07 08:40:04 +0800

    feat(service): 新增 PVC 统计服务
    - 实现了 PVC 统计服务的基本功能，包括增加、减少和获取 PVC 统计数据
    - 添加了 PVC 的 watch 功能，实时监控集群中的 PVC 变化
    - 通过定时任务定期检查并启动未开启 PVC watch 的集群

---
+++ pkg/service/pvc.go
@@ -0,0 +1,144 @@
+package service
+
+import (
+	"sync"
+
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/robfig/cron/v3"
+	"github.com/weibaohui/kom/kom"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/klog/v2"
+)
+
+type pvcService struct {
+	CountList []*pvcCount
+	lock      sync.RWMutex
+}
+
+// 定义结构体，为按spec.storageclass.name统计数量，包括集群、name、数量
+type pvcCount struct {
+	ClusterName string // 集群名称
+	Name        string // storageClassName
+	Count       int    // 数量
+}
+
+// AddPVCCount 增加pvc统计数据
+func (p *pvcService) AddPVCCount(selectedCluster string, pvc *corev1.PersistentVolumeClaim) {
+	// 从CountList中看是否有该集群、该storageClassName的项，有则加1，无则创建为1
+	// 为避免并发操作统计错误，加一个锁
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	h := slice.Filter(p.CountList, func(index int, item *pvcCount) bool {
+		return item.ClusterName == selectedCluster && item.Name == *pvc.Spec.StorageClassName
+	})
+	if len(h) == 0 {
+		// 还没有该集群、该storageClassName的项，创建
+		p.CountList = append(p.CountList, &pvcCount{
+			ClusterName: selectedCluster,
+			Name:        *pvc.Spec.StorageClassName,
+			Count:       1,
+		})
+		return
+	}
+	if len(h) == 1 {
+		h[0].Count = h[0].Count + 1
+		return
+	}
+
+}
+
+// RemovePVCCount 减少pvc统计数据
+func (p *pvcService) RemovePVCCount(selectedCluster string, pvc *corev1.PersistentVolumeClaim) {
+	// 从CountList中看是否有该集群、该storageClassName的项，有则减1，无则不操作
+	// 为避免并发操作统计错误，加一个锁
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	h := slice.Filter(p.CountList, func(index int, item *pvcCount) bool {
+		return item.ClusterName == selectedCluster && item.Name == *pvc.Spec.StorageClassName
+	})
+	if len(h) == 0 {
+		return
+	}
+	if len(h) == 1 {
+		h[0].Count = h[0].Count - 1
+		if h[0].Count < 0 {
+			h[0].Count = 0
+		}
+	}
+}
+
+// GetPVCCount 按StorageClassName获取pvc统计数据
+func (p *pvcService) GetPVCCount(selectedCluster string, name string) int {
+	// 从CountList中看是否有该集群的项，有则返回，无则返回0
+	// 为避免并发操作统计错误，加一个锁
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+	for _, item := range p.CountList {
+		if item.ClusterName == selectedCluster && item.Name == name {
+			return item.Count
+		}
+	}
+	return 0
+}
+
+func (p *pvcService) Watch() {
+	// 设置一个定时器，不断查看是否有集群未开启watch，未开启的话，开启watch
+	inst := cron.New()
+	_, err := inst.AddFunc("@every 1m", func() {
+		// 延迟启动cron
+		clusters := ClusterService().ConnectedClusters()
+		for _, cluster := range clusters {
+			if !cluster.GetClusterWatchStatus("pvc") {
+				selectedCluster := ClusterService().ClusterID(cluster)
+				p.watchSingleCluster(selectedCluster)
+				cluster.SetClusterWatchStarted("pvc")
+			}
+		}
+	})
+	if err != nil {
+		klog.Errorf("Error add cron job for PVC: %v\n", err)
+	}
+	inst.Start()
+	klog.V(6).Infof("新增PVC状态定时更新任务【@every 1m】\n")
+}
+
+func (p *pvcService) watchSingleCluster(selectedCluster string) {
+	// watch  命名空间下 pvc 的变更
+	var watcher watch.Interface
+	var pvc corev1.PersistentVolumeClaim
+	err := kom.Cluster(selectedCluster).Resource(&pvc).AllNamespace().Watch(&watcher).Error
+	if err != nil {
+		klog.Errorf("%s 创建pvc监听器失败 %v", selectedCluster, err)
+		return
+	}
+	go func() {
+		klog.V(6).Infof("%s start watch pvc", selectedCluster)
+		defer watcher.Stop()
+		for event := range watcher.ResultChan() {
+			err = kom.Cluster(selectedCluster).Tools().ConvertRuntimeObjectToTypedObject(event.Object, &pvc)
+			if err != nil {
+				klog.V(6).Infof("%s 无法将对象转换为 *v1.PersistentVolumeClaim 类型: %v", selectedCluster, err)
+				return
+			}
+			// 处理事件
+			switch event.Type {
+			case watch.Added:
+				p.AddPVCCount(selectedCluster, &pvc)
+				klog.V(6).Infof("%s 添加PVC [ %s/%s ]\n", selectedCluster, pvc.Namespace, pvc.Name)
+			case watch.Modified:
+				// 统计数量，修改跳过
+				klog.V(6).Infof("%s 修改PVC [ %s/%s ]\n", selectedCluster, pvc.Namespace, pvc.Name)
+			case watch.Deleted:
+				p.RemovePVCCount(selectedCluster, &pvc)
+				klog.V(6).Infof("%s 删除PVC [ %s/%s ]\n", selectedCluster, pvc.Namespace, pvc.Name)
+			}
+		}
+	}()
+
+	// 延迟设置完成状态，等待 ListWatch完成
+	ClusterService().DelayStartFunc(func() {
+		ClusterService().SetStorageClassStatusAggregated(selectedCluster, true)
+	})
+}
