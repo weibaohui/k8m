ecc8e077009f4391bd7318c5f966211f0af102ab
weibaohui <weibaohui@yeah.net>
2025-02-12 15:51:44 +0800

    refactor(ui): 移除未使用的 uuid 工具函数
    - 删除了 utils.ts 文件中的 uuid导出
    - 移除了 nanoid 的引用

--- ui/src/utils/utils.ts
+++ ui/src/utils/utils.ts
@@ -1,7 +1,3 @@
-import {nanoid} from 'nanoid'
-
-export const uuid = nanoid
-
 export const sleep = (time = 10, fn?: () => void) => {
     return new Promise((resolve) => {
         const timer = setTimeout(() => {
