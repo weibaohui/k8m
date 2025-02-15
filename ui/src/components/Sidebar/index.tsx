3a5a6c2dabe74fb0d0c67f7a5801fb4c50b9e52f
weibaohui <weibaohui@yeah.net>
2025-02-15 21:55:49 +0800

    style(ui): 优化侧边栏样式
    - 添加外层 div 容器，设置高度和 overflow 属性
    - 在 Menu 组件中添加 borderRight 属性

--- ui/src/components/Sidebar/index.tsx
+++ ui/src/components/Sidebar/index.tsx
@@ -4,14 +4,19 @@ import menu from '@/components/Sidebar/menu';
 const Sidebar = () => {
     const { collapse } = useStore(state => state)
 
-    return <Menu style={{ minWidth: 0, flex: "auto" }}
-        mode="inline"
-        inlineCollapsed={collapse}
-        defaultOpenKeys={['home']}
-        defaultSelectedKeys={[]}
-        items={menu()}
-    >
-    </Menu>
+    return (
+        <div style={{ height: 'calc(100vh - 64px)', minWidth: 0, flex: "auto", overflow: 'auto' }}>
+            <Menu
+                mode="inline"
+                inlineCollapsed={collapse}
+                defaultOpenKeys={['home']}
+                defaultSelectedKeys={[]}
+                items={menu()}
+                style={{ height: '100%', borderRight: 0 }}
+            >
+            </Menu>
+        </div>
+    )
 }
 
 export default Sidebar
