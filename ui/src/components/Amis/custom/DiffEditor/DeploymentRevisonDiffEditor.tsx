67f409b979e1fd311142e5daa7180505f92f406c
weibaohui <weibaohui@yeah.net>
2025-02-18 23:03:25 +0800

    feat(ui): 添加 Deployment 版本差异编辑器组件
    - 新增 DeploymentRevisonDiffEditor 组件，用于展示 Deployment 版本差异
    - 组件通过 API 获取当前运行版本和指定版本的差异数据
    - 使用 DiffEditorComponent 来显示差异内容
    - 组件支持根据数据动态替换 URL 和版本号

---
+++ ui/src/components/Amis/custom/DiffEditor/DeploymentRevisonDiffEditor.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import DiffEditorComponent from '.';
+import { fetcher } from "@/components/Amis/fetcher";
+import { replacePlaceholders } from '@/utils/utils';
+
+interface DeploymentRevisionDiffEditorProps {
+    data: Record<string, any>;
+    url: string;
+    revision: string;
+}
+
+// 用于展示Deployment版本差异的组件
+const DeploymentRevisionDiffEditor: React.FC<DeploymentRevisionDiffEditorProps> = ({
+    url, data, revision
+}) => {
+    const [original, setOriginal] = useState<string>();
+    const [modified, setModified] = useState<string>();
+    url = replacePlaceholders(url, data);
+    revision = replacePlaceholders(revision, data);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetcher({
+                    url: url,
+                    method: 'get',
+                });
+                //@ts-ignore
+                setOriginal(response.data?.data.current);
+                //@ts-ignore
+                setModified(response.data?.data.latest);
+            } catch (error) {
+                console.error('Failed to fetch deployment revision data:', error);
+            }
+        };
+        fetchData();
+    }, [url]);
+    return (
+        <DiffEditorComponent
+            originalValue={original}  // 左侧显示指定版本
+            modifiedValue={modified}   // 右侧显示最新版本
+            originalLabel="当前运行版本"
+            modifiedLabel={`#${revision}版本`}
+            height="calc(100vh - 200px)"  // 设置合适的高度
+            width="100%"
+            readOnly={true}               // 设置为只读模式
+        />
+    );
+};
+
+export default DeploymentRevisionDiffEditor;
