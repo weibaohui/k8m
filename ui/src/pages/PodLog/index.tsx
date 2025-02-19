707a10de067bd1764dde8d848236a131fb96cfd2
weibaohui <weibaohui@yeah.net>
2025-02-19 14:28:50 +0800

    refactor(ui): 简化 PodLog 组件结构
    - 移除了未使用的 import 语句
    - 删除了不必要的接口定义

--- ui/src/pages/PodLog/index.tsx
+++ ui/src/pages/PodLog/index.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Select, Card } from 'antd';
-import { useSearchParams } from 'react-router-dom';
-import { fetcher } from '../../components/Amis/fetcher';
-import SSELogDisplayComponent from '../../components/Amis/custom/LogView/SSELogDisplay';
-import SSELogDownloadComponent from '../../components/Amis/custom/LogView/SSELogDownload';
-import LogOptionsComponent from '../../components/Amis/custom/LogView/LogOptions';
+import React from 'react';
+import {useSearchParams} from 'react-router-dom';
 import PodLogViewer from '../../components/Amis/custom/LogView/PodLogViewer';
 
-interface PodSpec {
-    containers: Container[];
-}
 
-interface PodData {
-    spec: PodSpec;
-}
-interface Container {
-    name: string;
-}
 const PodLog: React.FC = () => {
 
     const [searchParams] = useSearchParams();
@@ -28,8 +14,8 @@ const PodLog: React.FC = () => {
     }
 
     return (
-        <div style={{ padding: '6px' }}>
-            <PodLogViewer namespace={namespace} name={name} data={{}} showTitle={true} />
+        <div style={{padding: '6px'}}>
+            <PodLogViewer namespace={namespace} name={name} data={{}} showTitle={true}/>
         </div>
     );
 };
