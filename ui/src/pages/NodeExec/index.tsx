91cdb519eee941fae16cce1eac2810b603a2b9d5
weibaohui <weibaohui@yeah.net>
2025-02-19 17:37:17 +0800

    feat(ui): 添加节点执行命令功能
    - 新增 NodeExec 组件，实现节点命令行执行功能- 使用 React、Ant Design 和自定义组件 XTermComponent
    -通过后端 API 获取节点信息并创建节点 shell
    - 使用 WebSocket 实

---
+++ ui/src/pages/NodeExec/index.tsx
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react';
+import {Card} from 'antd';
+import {useSearchParams} from 'react-router-dom';
+import XTermComponent from '@/components/Amis/custom/XTerm';
+import {fetcher} from '@/components/Amis/fetcher.ts';
+
+interface PodData {
+    podName: string;
+    ns: string;
+    containerName: string;
+}
+
+const NodeExec: React.FC = () => {
+    const [searchParams] = useSearchParams();
+    const nodeName = searchParams.get('name') || '';
+    const [podShell, setPodSell] = useState<PodData>();
+
+    useEffect(() => {
+        if (!nodeName) return;
+
+        // 获取Pod详情以获取容器列表
+        fetcher({
+            url: `/k8s/node/name/${nodeName}/create_node_shell`,
+            method: 'post'
+        })
+            .then(response => {
+                const data = response.data?.data as unknown as PodData;
+                console.log(response.data?.data)
+                console.log(data)
+                setPodSell(data)
+            })
+            .catch(error => console.error('Error fetching pod details:', error));
+    }, [nodeName]);
+
+    if (!nodeName) {
+        return <div>请在URL中提供节点名称参数</div>;
+    }
+
+    return (
+        <div style={{padding: '6px'}}>
+            <Card
+                variant="outlined"
+                style={{width: '100%', height: 'calc(100vh - 12px)'}}
+            >
+                {podShell && (
+                    <div style={{background: '#f5f5f5', padding: '4px', borderRadius: '4px'}}>
+                        <XTermComponent
+                            url={`${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/k8s/node/xterm/name/${nodeName}/pod/${podShell.podName}`}
+                            // url={`${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/k8s/pod/ns/${podShell.ns}/pod_name/${podShell.podName}`}
+                            params={{}}
+                            data={{}}
+                            height="calc(100vh - 120px)"
+                        />
+                    </div>
+                )}
+            </Card>
+        </div>
+    );
+};
+
+export default NodeExec;
