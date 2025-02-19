8a909acb0823fd80986844bc470cfc4dcaa45ce7
weibaohui <weibaohui@yeah.net>
2025-02-19 17:36:31 +0800

    refactor(ui): 调整 WebSocket 代理路径
    - 将 '/k8s/pod/wsexec' 路径更改为 '/k8s/pod/xterm'
    - 将 '/k8s/pod/xterm' 路径更改为 '/k8s/node/xterm'

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -44,12 +44,13 @@ export default defineConfig(({mode}) => {
                     ws: true, // 开启 WebSocket 代理
                     changeOrigin: true,
                 },
-                '/k8s/pod/wsexec': {
+
+                '/k8s/pod/xterm': {
                     target: 'ws://127.0.0.1:3618', // 替换为实际的目标地址
                     ws: true, // 开启 WebSocket 代理
                     changeOrigin: true,
                 },
-                '/k8s/pod/xterm': {
+                '/k8s/node/xterm': {
                     target: 'ws://127.0.0.1:3618', // 替换为实际的目标地址
                     ws: true, // 开启 WebSocket 代理
                     changeOrigin: true,
