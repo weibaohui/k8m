97970996d9c366d65ff1db2b6c7b9f30b84957c7
weibaohui <weibaohui@yeah.net>
2025-02-19 17:37:10 +0800

    feat(ui): 添加 NodeExec 组件并更新路由
    - 在路由中添加 NodeExec 组件
    -优化代码格式，调整缩进和空格

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,26 +1,28 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import Loading from '@/components/Loading'
-import { lazy, Suspense } from 'react'
+import {lazy, Suspense} from 'react'
 import Layout from '@/layout'
 import Login from '@/pages/Login/index.tsx'
 import PodExec from '@/pages/PodExec'
 import PodLog from '@/pages/PodLog'
+import NodeExec from '@/pages/NodeExec'
 
 const lazyLoad = (Component: React.LazyExoticComponent<() => JSX.Element>) => {
     return (
-        <Suspense fallback={<Loading />}>
-            <Component />
+        <Suspense fallback={<Loading/>}>
+            <Component/>
         </Suspense>
     )
 }
 const Router = () => {
     return (
         <Routes>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/PodExec' element={<PodExec />}></Route>
-            <Route path='/PodLog' element={<PodLog />}></Route>
-            <Route path='/' element={<Layout />}>
-                <Route path='/' element={<Navigate to="/cluster/node" />}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/NodeExec' element={<NodeExec/>}></Route>
+            <Route path='/PodExec' element={<PodExec/>}></Route>
+            <Route path='/PodLog' element={<PodLog/>}></Route>
+            <Route path='/' element={<Layout/>}>
+                <Route path='/' element={<Navigate to="/cluster/node"/>}></Route>
                 <Route path='/*' element={
                     lazyLoad(
                         lazy(
