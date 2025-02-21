9b0b62df12795d83bd8682e6720cb38816730025
weibaohui <weibaohui@yeah.net>
2025-02-21 21:30:57 +0800

    feat(ui): 添加中文语言支持
    - 引入 antd 中文语言包
    - 引入 dayjs 中文语言包
    - 设置 dayjs 为中文
    - 使用 ConfigProvider 包裹 App 组件以应用中文语言

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -5,5 +5,15 @@ import 'antd/dist/reset.css'
 import '@/styles/global.scss'
 import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App.tsx'
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+dayjs.locale('zh-cn');
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ConfigProvider locale={zhCN}>
+        <App />
+    </ConfigProvider>
+)
