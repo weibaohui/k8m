0c63c112b38287a558d6b0654f1ecee2ca81ff8f
weibaohui <weibaohui@yeah.net>
2025-02-22 19:25:21 +0800

    fix(controller): 修复模板列表分页查询逻辑
    - 修改了 ListKind 函数中的查询逻辑
    - 使用了 gorm 的 Scope 函数对查询进行定制
    - 优化了查询结果，仅返回 Kind 字段并去重

--- pkg/controller/template/kind.go
+++ pkg/controller/template/kind.go
@@ -5,13 +5,16 @@ import (
 	"github.com/weibaohui/k8m/internal/dao"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/models"
+	"gorm.io/gorm"
 )
 
 func ListKind(c *gin.Context) {
 	params := dao.BuildParams(c)
-	m := &models.CustomTemplateKind{}
+	m := &models.CustomTemplate{}
 	params.PerPage = 1000
-	items, total, err := m.List(params)
+	items, total, err := m.List(params, func(db *gorm.DB) *gorm.DB {
+		return db.Select("Kind").Distinct("Kind")
+	})
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
