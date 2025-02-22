80b7609c5793f439a8f66c5bc5175c69a3bee793
weibaohui <weibaohui@yeah.net>
2025-02-22 10:05:59 +0800

    feat(models): 添加数据库自动迁移功能
    - 新增 migrate.go 文件，实现数据库自动迁移功能
    - 在 init 函数中调用 AutoMigrate 函数进行迁移
    - 使用 klog 记录迁移过程的日志信息

---
+++ pkg/models/migrate.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/weibaohui/k8m/internal/dao"
+	"k8s.io/klog/v2"
+)
+
+func init() {
+
+	err := AutoMigrate()
+	if err != nil {
+		klog.Errorf("数据库迁移失败: %v", err.Error())
+	}
+	klog.V(4).Info("数据库自动迁移完成")
+}
+func AutoMigrate() error {
+
+	// 添加需要迁移的所有模型
+	return dao.DB().AutoMigrate(&CustomTemplate{})
+}
