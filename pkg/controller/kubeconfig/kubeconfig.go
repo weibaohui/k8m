2a62d710102ef5e4e8e7cd70a4ded68d3df52559
weibaohui <weibaohui@yeah.net>
2025-02-23 00:06:16 +0800

    feat(kubeconfig): 优化 kubeconfig 保存和删除逻辑
    - 重构 Save 函数，支持解析 kubeconfig 内容并保存多个集群配置
    - 新增 Remove函数，实现批量删除 kubeconfig功能
    - 使用 k8s客户端库解析 kubeconfig，提高兼容性和准确性
    -增加日志记录，方便调试和监控

--- pkg/controller/kubeconfig/kubeconfig.go
+++ pkg/controller/kubeconfig/kubeconfig.go
@@ -7,7 +7,8 @@ import (
 	"github.com/weibaohui/k8m/internal/dao"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/models"
-	"gorm.io/gorm"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
 func Save(c *gin.Context) {
@@ -22,30 +23,53 @@ func Save(c *gin.Context) {
 	// 因先删除再创建
 	// 可能会更新地址的kubeconfig
 
-	kc := &models.KubeConfig{}
-	kc.Cluster = m.Cluster
-	kc.Server = m.Server
-	kc.User = m.User
-	list, _, err := kc.List(params, func(d *gorm.DB) *gorm.DB {
-		return d.Where("cluster = ?", m.Cluster).
-			Where("server = ?", m.Server).
-			Where("user = ?", m.User)
-	})
+	config, err := clientcmd.Load([]byte(m.Content))
+	if err != nil {
+		klog.V(6).Infof("解析 集群 [%s]失败: %v", m.Server, err)
+		return
+	}
+	for contextName, _ := range config.Contexts {
+		context := config.Contexts[contextName]
+		cluster := config.Clusters[context.Cluster]
+
+		kc := &models.KubeConfig{
+			Cluster:   context.Cluster,
+			Server:    cluster.Server,
+			User:      context.AuthInfo,
+			Namespace: context.Namespace,
+			Content:   m.Content,
+		}
+		if list, _, err := kc.List(params); err == nil && list != nil {
+			for _, item := range list {
+				_ = kc.Delete(params, fmt.Sprintf("%d", item.ID))
+			}
+		}
+
+		err = kc.Save(params)
+		if err != nil {
+			klog.V(6).Infof("保存 集群 [%s]失败: %v", m.Server, err)
+			amis.WriteJsonError(c, err)
+			return
+		}
+
+	}
+
+	amis.WriteJsonOK(c)
+}
+func Remove(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.KubeConfig{}
+	err := c.ShouldBindJSON(&m)
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
 	}
 
-	for _, config := range list {
-		_ = kc.Delete(params, fmt.Sprintf("%d", config.ID))
+	if list, _, err := m.List(params); err == nil && list != nil {
+		for _, item := range list {
+			_ = m.Delete(params, fmt.Sprintf("%d", item.ID))
+		}
 	}
 
-	err = m.Save(params)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	amis.WriteJsonData(c, gin.H{
-		"id": m.ID,
-	})
+	amis.WriteJsonOK(c)
 }
