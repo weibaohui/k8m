95e82728f67b3a0ffcce6901830b310463a126b7
weibaohui <weibaohui@yeah.net>
2025-02-22 16:58:38 +0800

    feat(controller): 添加模板列表和添加模板功能
    - 新增 ListTemplate 函数，用于获取模板列表和总数
    - 新增 AddTemplate函数，用于添加新模板
    - 引入必要的包和模块- 使用 amis 工具库处理 JSON 响应

---
+++ pkg/controller/template/template.go
@@ -0,0 +1,35 @@
+package template
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
+)
+
+func ListTemplate(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.CustomTemplate{}
+
+	items, total, err := m.List(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonListWithTotal(c, total, items)
+}
+func AddTemplate(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.CustomTemplate{}
+	err := c.ShouldBindJSON(&m)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	err = m.Save(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
