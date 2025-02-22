95c8c9ff1f0e42df19cab84b9c32e4eb0dd6248b
weibaohui <weibaohui@yeah.net>
2025-02-22 20:35:41 +0800

    feat(models): 添加 KubeConfig模型
    - 新增 KubeConfig 结构体，用于用户导入 kubeconfig
    - 实现 KubeConfig 模型的通用查询、保存、删除和获取单个记录的方法
    - 为 KubeConfig 表创建必要的索引

---
+++ pkg/models/kube_config.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"gorm.io/gorm"
+)
+
+// KubeConfig 用户导入kubeconfig
+type KubeConfig struct {
+	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id,omitempty"` // 模板 ID，主键，自增
+	Content   string    `gorm:"type:text" json:"content,omitempty"`           // 模板内容，支持大文本存储
+	Server    string    `gorm:"index" json:"server,omitempty"`
+	User      string    `gorm:"index" json:"user,omitempty"`
+	Cluster   string    `gorm:"index" json:"cluster,omitempty"`    // 模板类型，最大长度 100
+	CreatedBy string    `gorm:"index" json:"created_by,omitempty"` // 创建者
+	CreatedAt time.Time `json:"created_at,omitempty"`              // Automatically managed by GORM for creation time
+	UpdatedAt time.Time `json:"updated_at,omitempty"`              // Automatically managed by GORM for update time
+}
+
+func (c *KubeConfig) List(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) ([]*KubeConfig, int64, error) {
+	return dao.GenericQuery(params, c, queryFuncs...)
+}
+
+func (c *KubeConfig) Save(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericSave(params, c, queryFuncs...)
+}
+
+func (c *KubeConfig) Delete(params *dao.Params, ids string, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericDelete(params, c, utils.ToInt64Slice(ids), queryFuncs...)
+}
+
+func (c *KubeConfig) GetOne(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) (*KubeConfig, error) {
+	return dao.GenericGetOne(params, c, queryFuncs...)
+}
