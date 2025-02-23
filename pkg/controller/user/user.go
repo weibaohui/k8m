b3ce3854acd3838c751139ff1240a9b6540a9ade
weibaohui <weibaohui@yeah.net>
2025-02-23 10:35:42 +0800

    feat(user): 添加用户密码更新功能并优化用户列表和创建逻辑- 新增 UpdatePsw 函数，用于更新用户密码 -优化 List 函数，添加管理员权限判断 - 在 Create 函数中增加用户名不能为 admin 的限制

--- pkg/controller/user/user.go
+++ pkg/controller/user/user.go
@@ -1,17 +1,24 @@
 package user
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/models"
+	"gorm.io/gorm"
 )
 
 func List(c *gin.Context) {
 	params := dao.BuildParams(c)
 	m := &models.User{}
 
-	items, total, err := m.List(params)
+	// todo 管理页面，判断是否管理员，看到所有的用户，
+	items, total, err := m.List(params, func(db *gorm.DB) *gorm.DB {
+		return db
+	})
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
@@ -24,6 +31,12 @@ func Save(c *gin.Context) {
 	err := c.ShouldBindJSON(&m)
 	if err != nil {
 		amis.WriteJsonError(c, err)
+		return
+	}
+	// 用户名不能为admin
+
+	if m.Username == "admin" {
+		amis.WriteJsonError(c, fmt.Errorf("用户名不能为admin"))
 		return
 	}
 
@@ -41,6 +54,26 @@ func Delete(c *gin.Context) {
 	params := dao.BuildParams(c)
 	m := &models.User{}
 	err := m.Delete(params, ids)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
+func UpdatePsw(c *gin.Context) {
+	// todo 校验管理员权限，不是管理员不可以改
+	id := c.Param("id")
+	params := dao.BuildParams(c)
+	m := &models.User{}
+	err := c.ShouldBindJSON(m)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	m.ID = uint(utils.ToInt64(id))
+	err = m.Save(params, func(db *gorm.DB) *gorm.DB {
+		return db.Select("password").Updates(m)
+	})
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
