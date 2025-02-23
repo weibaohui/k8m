b07752b26dee36cf0c09581cb6f90e21d812b04c
weibaohui <weibaohui@yeah.net>
2025-02-23 20:29:46 +0800

    feat(models): 添加操作日志模型
    - 新增 OperationLog 结构体，用于记录操作日志信息
    - 实现了列表、保存、删除和获取单个操作日志的通用方法
    - 为操作日志模型添加了必要的字段，包括集群、命名空间、用户名等信息

---
+++ pkg/models/operation_log.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"time"
+
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"gorm.io/gorm"
+)
+
+// OperationLog 用户导入OperationLog
+type OperationLog struct {
+	ID            uint      `gorm:"primaryKey;autoIncrement" json:"id,omitempty"` // 模板 ID，主键，自增
+	Cluster       string    `gorm:"index" json:"cluster,omitempty"`
+	Namespace     string    `json:"namespace,omitempty"`
+	Name          string    `json:"name,omitempty"`
+	UserName      string    `json:"username,omitempty"`
+	Group         string    `json:"group,omitempty"`
+	Kind          string    `json:"kind,omitempty"`
+	Command       string    `json:"command,omitempty"` // shell 执行命令
+	Action        string    `json:"action,omitempty"`  // 操作类型
+	Type          string    `json:"type,omitempty"`    // 操作类型 shell
+	PodName       string    `json:"pod_name,omitempty"`
+	ContainerName string    `json:"container_name,omitempty"`
+	CreatedAt     time.Time `json:"created_at,omitempty"` // Automatically managed by GORM for creation time
+	UpdatedAt     time.Time `json:"updated_at,omitempty"` // Automatically managed by GORM for update time
+
+}
+
+func (c *OperationLog) List(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) ([]*OperationLog, int64, error) {
+	return dao.GenericQuery(params, c, queryFuncs...)
+}
+
+func (c *OperationLog) Save(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericSave(params, c, queryFuncs...)
+}
+
+func (c *OperationLog) Delete(params *dao.Params, ids string, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericDelete(params, c, utils.ToInt64Slice(ids), queryFuncs...)
+}
+
+func (c *OperationLog) GetOne(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) (*OperationLog, error) {
+	return dao.GenericGetOne(params, c, queryFuncs...)
+}
