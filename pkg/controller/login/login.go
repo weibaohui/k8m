7f731a4bcdeafd1087efb9ec0a93db41bf662fb9
weibaohui <weibaohui@yeah.net>
2025-02-23 11:40:30 +0800

    refactor(login): 重构登录逻辑并优化密码加密解密流程- 移除登录逻辑中的 AES 加密解密代码 - 使用 utils 包中的 AesDecrypt 方法解密密码 - 新增 AesEncrypt 方法用于重新加密密码- 优化密码验证流程，增加盐值处理 - 删除未使用的 pkcs7Padding、pkcs7UnPadding、AesEncrypt 和 AesDecrypt 函数

--- pkg/controller/login/login.go
+++ pkg/controller/login/login.go
@@ -2,23 +2,20 @@ package login
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/base64"
-	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/k8m/pkg/flag"
 	"github.com/weibaohui/k8m/pkg/models"
 	"github.com/weibaohui/k8m/pkg/service"
 )
-
-var secretKey = "secret-key-16-ok"
 
 // LoginRequest 用户结构体
 type LoginRequest struct {
@@ -49,7 +46,7 @@ func LoginByPassword(c *gin.Context) {
 	cfg := flag.Init()
 
 	// 对密码进行解密
-	decrypt, err := AesDecrypt(req.Password)
+	decrypt, err := utils.AesDecrypt(req.Password)
 	if err != nil {
 		amis.WriteJsonError(c, err)
 		return
@@ -78,16 +75,24 @@ func LoginByPassword(c *gin.Context) {
 		}
 		for _, v := range list {
 			if v.Username == req.Username {
-				decryptDBPsw, err := AesDecrypt(v.Password)
+				// password base64解密
+				// 前端密码解密的值，加上盐值，重新计算
+				decryptPsw, err := utils.AesEncrypt([]byte(fmt.Sprintf("%s%s", string(decrypt), v.Salt)))
 				if err != nil {
 					amis.WriteJsonError(c, err)
 					return
 				}
-				if string(decrypt) != string(decryptDBPsw) {
+				dbPsw, err := base64.StdEncoding.DecodeString(v.Password)
+				if err != nil {
+					amis.WriteJsonError(c, err)
+					return
+				}
+				if !bytes.Equal(dbPsw, decryptPsw) {
 					// 前端处理登录状态码，不要修改
 					c.JSON(http.StatusUnauthorized, gin.H{"message": "用户名或密码错误"})
 					return
 				}
+
 				token, _ := generateToken(v.Username, v.Role)
 				c.JSON(http.StatusOK, gin.H{"token": token})
 				return
@@ -96,76 +101,4 @@ func LoginByPassword(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusUnauthorized, gin.H{"message": "用户名或密码错误"})
-}
-
-// pkcs7Padding 填充
-func pkcs7Padding(data []byte, blockSize int) []byte {
-	// 判断缺少几位长度。最少1，最多 blockSize
-	padding := blockSize - len(data)%blockSize
-	// 补足位数。把切片[]byte{byte(padding)}复制padding个
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(data, padText...)
-}
-
-// pkcs7UnPadding 填充的反向操作
-func pkcs7UnPadding(data []byte) ([]byte, error) {
-	length := len(data)
-	if length == 0 {
-		return nil, errors.New("加密字符串错误！")
-	}
-	// 获取填充的个数
-	unPadding := int(data[length-1])
-	return data[:(length - unPadding)], nil
-}
-
-// AesEncrypt 加密
-func AesEncrypt(data []byte) ([]byte, error) {
-	key := []byte(secretKey)
-	// 创建加密实例
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	// 判断加密块的大小
-	blockSize := block.BlockSize()
-	// 填充
-	encryptBytes := pkcs7Padding(data, blockSize)
-	// 初始化加密数据接收切片
-	crypted := make([]byte, len(encryptBytes))
-	// 使用cbc加密模式
-	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
-	// 执行加密
-	blockMode.CryptBlocks(crypted, encryptBytes)
-	return crypted, nil
-}
-
-// AesDecrypt 解密
-func AesDecrypt(ciphertextBase64 string) ([]byte, error) {
-
-	// 解码 Base64 密文
-	data, err := base64.StdEncoding.DecodeString(ciphertextBase64)
-	if err != nil {
-		return nil, err
-	}
-
-	key := []byte(secretKey)
-	// 创建实例
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	// 获取块的大小
-	blockSize := block.BlockSize()
-	// 使用cbc
-	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
-	// 初始化解密数据接收切片
-	crypted := make([]byte, len(data))
-	// 执行解密
-	blockMode.CryptBlocks(crypted, data)
-	// 去除填充
-	crypted, err = pkcs7UnPadding(crypted)
-	if err != nil {
-		return nil, err
-	}
-	return crypted, nil
 }
