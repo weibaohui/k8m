b7d7ed15338cb5dfa92637d2e377f05a20123631
weibaohui <weibaohui@yeah.net>
2025-02-23 10:35:33 +0800

    feat(ui): 新增 PasswordEditorWithForm 组件
    - 添加 PasswordEditorWithForm 组件，用于编辑和保存密码
    - 实现密码输入、加密和保存功能
    -集成 antd 组件库，提供友好的用户界面
    - 优化 API 请求处理，支持动态替换参数

---
+++ ui/src/components/Amis/custom/PasswordEditorWithForm/PasswordEditorWithForm.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+
+import {replacePlaceholders} from "@/utils/utils.ts";
+import {fetcher} from "@/components/Amis/fetcher.ts";
+import {Button, Input, message} from 'antd';
+import {encrypt} from "@/utils/crypto.ts";
+
+
+interface PasswordEditorWithFormProps {
+    api: string;
+    data: Record<string, any>
+}
+
+const PasswordEditorWithForm: React.FC<PasswordEditorWithFormProps> = ({
+                                                                           api,
+                                                                           data,
+                                                                       }) => {
+    const [loading, setLoading] = useState(false);
+    const [password, setPassword] = useState('');
+
+    if (api) {
+        api = replacePlaceholders(api, data)
+    }
+
+    const handleSave = async () => {
+        if (!api) return;
+        if (!password) {
+            message.error('请输入密码');
+            return;
+        }
+        setLoading(true);
+
+        const encryptedPassword = encrypt(password);
+
+        const response = await fetcher({
+            url: api,
+            method: 'post',
+            data: {
+                password: encryptedPassword
+            }
+        });
+
+        if (response.data?.status !== 0) {
+            message.error(`保存失败:请尝试刷新后重试。 ${response.data?.msg}`);
+        } else {
+            message.info('保存成功！');
+            setPassword('');
+        }
+        setLoading(false);
+    };
+
+    return (
+        <>
+            <div style={{width: '100%', height: 'calc(100vh - 200px)', display: 'flex', flexDirection: 'column'}}>
+                <div style={{padding: '10px', display: 'flex', justifyContent: 'flex-end'}}>
+                    <Input.Password
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="请输入密码"
+                    />
+                    {api && <Button type="primary" onClick={handleSave} loading={loading}>保存</Button>}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default PasswordEditorWithForm;
