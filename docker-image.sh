371d82394361cd3842d7709ee85b7aeb75570de8
weibaohui <weibaohui@yeah.net>
2025-02-24 17:01:53 +0800

    docker 构建镜像
    - 将 Dockerfile 中的基础镜像替换为阿里云镜像
    - 修改 Makefile 中的构建命令，使用 podman 替代 docker
    - 更新 docker-image.sh 脚

--- docker-image.sh
+++ docker-image.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 #
 set -e 
-docker rmi golang:alpine
-docker rmi registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-amd64
-docker rmi registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-arm64
+podman rmi golang:alpine
+podman rmi registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-amd64
+podman rmi registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-arm64
 
 # 拉取 amd64 架构的 Golang 镜像
-docker pull --platform=linux/amd64 golang:alpine
-docker tag golang:alpine registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-amd64
-docker rmi golang:alpine
+podman pull --platform=linux/amd64 golang:alpine
+podman tag golang:alpine registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-amd64
+podman rmi golang:alpine
 # 拉取 arm64 架构的 Golang 镜像
-docker pull --platform=linux/arm64 golang:alpine
-docker tag golang:alpine registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-arm64
-docker rmi golang:alpine
+podman pull --platform=linux/arm64 golang:alpine
+podman tag golang:alpine registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-arm64
+podman rmi golang:alpine
 
 # 创建多架构 Manifest List
-docker manifest create registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine \
+podman manifest create registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine \
   registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-amd64 \
   registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine-arm64
 
 # 推送 Manifest List 到镜像仓库
-docker push registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine
+podman push registry.cn-hangzhou.aliyuncs.com/minik8m/golang:alpine
 
 
 # 拉起alpine镜像
-docker pull --platform=linux/amd64 alpine:latest
-docker tag alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-amd64
-docker pull --platform=linux/arm64 alpine:latest
-docker tag alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-arm64
-docker manifest create registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-arm64 registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-amd64 
-docker push registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest
+podman pull --platform=linux/amd64 alpine:latest
+podman tag alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-amd64
+podman pull --platform=linux/arm64 alpine:latest
+podman tag alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-arm64
+podman manifest create registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-arm64 registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest-amd64 
+podman push registry.cn-hangzhou.aliyuncs.com/minik8m/alpine:latest
+
+# 拉起bitnami/kubectl:latest镜像
+podman pull --platform=linux/amd64 bitnami/kubectl:latest
+podman tag bitnami/kubectl:latest registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest-amd64
+podman pull --platform=linux/arm64 bitnami/kubectl:latest
+podman tag bitnami/kubectl:latest registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest-arm64
+podman manifest create registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest-arm64 registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest-amd64 
+podman push registry.cn-hangzhou.aliyuncs.com/minik8m/kubectl:latest
