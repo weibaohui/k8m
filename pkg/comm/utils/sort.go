e4d7a90a5d924026c04a9b2ccdee27e6ff2fde45
weibaohui <weibaohui@yeah.net>
2025-03-01 23:55:38 +0800

    feat(utils): 添加版本号比较功能
    - 新增 ParseVersion 函数，用于将版本号字符串解析为数字切片
    - 新增 CompareVersions 函数，用于比较两个版本号的大小
    - 这些函数可以用于对版本号进行排序

--- pkg/comm/utils/sort.go
+++ pkg/comm/utils/sort.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -22,4 +24,34 @@ func SortByLastTimestamp(items []unstructured.Unstructured) []unstructured.Unstr
 		return foundI // 如果 I 有，J 没有，I 排在前面
 	})
 	return items
+}
+
+// ParseVersion 函数用于将版本号字符串解析为数字切片
+func ParseVersion(version string) []int {
+	parts := strings.Split(version, ".")
+	result := make([]int, len(parts))
+	for i, part := range parts {
+		num, _ := strconv.Atoi(part)
+		result[i] = num
+	}
+	return result
+}
+
+// CompareVersions 函数用于比较两个版本号的大小
+// Example:
+//
+//	sort.Slice(versions, func(i, j int) bool {
+//			return utils.CompareVersions(versions[i], versions[j])
+//		})
+func CompareVersions(v1, v2 string) bool {
+	parts1 := ParseVersion(v1)
+	parts2 := ParseVersion(v2)
+	for i := 0; i < len(parts1) && i < len(parts2); i++ {
+		if parts1[i] > parts2[i] {
+			return true
+		} else if parts1[i] < parts2[i] {
+			return false
+		}
+	}
+	return len(parts1) > len(parts2)
 }
