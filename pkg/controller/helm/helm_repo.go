05e96c127f6592572f436fc5bb318665e3aaf3c1
weibaohui <weibaohui@yeah.net>
2025-03-01 23:56:43 +0800

    feat(pkg/controller/helm): 添加 Helm 仓库管理相关接口
    - 实现了 Helm 仓库列表查询、添加/更新仓库、仓库选项列表、删除仓库和更新仓库索引等功能
    - 新增了与数据库交互的方法，用于获取仓库列表和删除仓库
    - 添加了错误处理和 JSON 响应封装，以提高代码可读性和可维护性

---
+++ pkg/controller/helm/helm_repo.go
@@ -0,0 +1,98 @@
+package helm
+
+import (
+	"strings"
+
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
+	"gorm.io/gorm"
+	"helm.sh/helm/v3/pkg/repo"
+)
+
+func ListRepo(c *gin.Context) {
+	// 从数据库查询列表
+	params := dao.BuildParams(c)
+	m := &models.HelmRepository{}
+	items, total, err := m.List(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonListWithTotal(c, total, items)
+}
+
+// AddOrUpdateRepo 添加或更新Helm仓库
+func AddOrUpdateRepo(c *gin.Context) {
+	var repoEntry repo.Entry
+	if err := c.ShouldBindJSON(&repoEntry); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	h, err := getHelm(c)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	if err := h.AddOrUpdateRepo(&repoEntry); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
+
+func RepoOptionList(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.HelmRepository{}
+	items, _, err := m.List(params, func(db *gorm.DB) *gorm.DB {
+		return db.Distinct("name")
+	})
+	if err != nil {
+		amis.WriteJsonData(c, gin.H{
+			"options": make([]map[string]string, 0),
+		})
+		return
+	}
+	var names []map[string]string
+	for _, n := range items {
+		names = append(names, map[string]string{
+			"label": n.Name,
+			"value": n.Name,
+		})
+	}
+	slice.SortBy(names, func(a, b map[string]string) bool {
+		return a["label"] < b["label"]
+	})
+	amis.WriteJsonData(c, gin.H{
+		"options": names,
+	})
+}
+
+func DeleteRepo(c *gin.Context) {
+	ids := c.Param("ids")
+	// 删除
+	dao.DB().Where("id in ?", strings.Split(ids, ",")).Delete(&models.HelmRepository{})
+	dao.DB().Where("repository_id in ?", strings.Split(ids, ",")).Delete(&models.HelmChart{})
+
+	amis.WriteJsonOK(c)
+}
+func UpdateReposIndex(c *gin.Context) {
+	var req struct {
+		IDs string `json:"ids"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	h, err := getHelm(c)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	go h.UpdateReposIndex(req.IDs)
+	amis.WriteJsonOK(c)
+}
