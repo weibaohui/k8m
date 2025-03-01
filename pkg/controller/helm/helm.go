a6457444370d173244d3942a0dc460901ae3f736
weibaohui <weibaohui@yeah.net>
2025-03-02 01:35:42 +0800

    fix(controller): 优化 Helm Release安装逻辑
    - 添加处理 values 中的 # 注释功能
    - 删除 values 中的空行
    - 使用 c.Param 获取参数替代 JSON 解析
    - 优化错误处理和日志记录

--- pkg/controller/helm/helm.go
+++ pkg/controller/helm/helm.go
@@ -1,10 +1,14 @@
 package helm
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/helm"
 	"github.com/weibaohui/k8m/pkg/service"
+	"k8s.io/klog/v2"
 )
 
 // ListReleaseHistory 获取Release的历史版本
@@ -32,12 +36,13 @@ func getHelm(c *gin.Context) (helm.Helm, error) {
 
 // InstallRelease 安装Helm Release
 func InstallRelease(c *gin.Context) {
+	releaseName := c.Param("release")
+	repoName := c.Param("repo")
+	chartName := c.Param("chart")
+	version := c.Param("version")
+
 	var req struct {
-		ReleaseName string   `json:"release_name"`
-		RepoName    string   `json:"repo_name"`
-		ChartName   string   `json:"chart_name"`
-		Version     string   `json:"version"`
-		Values      []string `json:"values,omitempty"`
+		Values string `json:"values,omitempty"`
 	}
 
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -51,7 +56,20 @@ func InstallRelease(c *gin.Context) {
 		return
 	}
 
-	if err := h.InstallRelease(req.ReleaseName, req.RepoName, req.ChartName, req.Version, req.Values...); err != nil {
+	// values 中有#注释
+	// 逐行对values 进行处理，去掉#注释
+	values := strings.Split(req.Values, "\n")
+	for i, v := range values {
+		if strings.HasPrefix(strings.TrimSpace(v), "#") {
+			values[i] = ""
+		}
+	}
+	// 删除空行
+	values = utils.RemoveEmptyLines(values)
+
+	req.Values = strings.Join(values, "\n")
+	klog.V(0).Infof("values: \n%s", req.Values)
+	if err = h.InstallRelease(releaseName, repoName, chartName, version, req.Values); err != nil {
 		amis.WriteJsonError(c, err)
 		return
 	}
