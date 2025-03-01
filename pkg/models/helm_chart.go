0ea8441ec00b894a1255304becd12f8e94cf7839
weibaohui <weibaohui@yeah.net>
2025-03-01 19:20:26 +0800

    feat(models): 优化 HelmChart 结构体
    - 添加 RepositoryName 字段，用于冗余仓库名称信息
    - 新增 KubeVersion、AppVersion 和 Deprecated 字段，丰富 Chart 信息
    - 调整字段顺序，提高代码可读性

--- pkg/models/helm_chart.go
+++ pkg/models/helm_chart.go
@@ -10,16 +10,20 @@ import (
 )
 
 type HelmChart struct {
-	ID            uint           `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
-	RepositoryID  uint           `gorm:"index;not null" json:"repository_id,omitempty"` // 关联仓库ID
-	Name          string         `gorm:"index;not null" json:"name,omitempty"`          // Chart名称
-	LatestVersion string         `json:"latest_version,omitempty"`                      // 最新版本（冗余字段，优化查询）
-	Description   string         `json:"description,omitempty"`                         // Chart描述
-	Home          string         `json:"home,omitempty"`                                // 项目主页URL
-	Icon          string         `json:"icon,omitempty"`                                // Chart图标链接
-	Keywords      pq.StringArray `gorm:"type:text[]" json:"keywords,omitempty"`         // 关键词（PostgreSQL数组类型）
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
+	ID             uint           `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
+	RepositoryID   uint           `gorm:"index;not null" json:"repository_id,omitempty"` // 关联仓库ID
+	RepositoryName string         `json:"repository_name,omitempty"`                     // 关联仓库ID
+	Name           string         `gorm:"index;not null" json:"name,omitempty"`          // Chart名称
+	LatestVersion  string         `json:"latest_version,omitempty"`                      // 最新版本（冗余字段，优化查询）
+	Description    string         `json:"description,omitempty"`                         // Chart描述
+	Home           string         `json:"home,omitempty"`                                // 项目主页URL
+	Icon           string         `json:"icon,omitempty"`                                // Chart图标链接
+	Keywords       pq.StringArray `gorm:"type:text[]" json:"keywords,omitempty"`         // 关键词（PostgreSQL数组类型）
+	KubeVersion    string         `json:"kubeVersion,omitempty"`
+	AppVersion     string         `json:"appVersion,omitempty"`
+	Deprecated     bool           `json:"deprecated,omitempty"` // Whether or not this chart is deprecated
+	CreatedAt      time.Time      `json:"created_at"`
+	UpdatedAt      time.Time      `json:"updated_at"`
 }
 
 func (c *HelmChart) List(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) ([]*HelmChart, int64, error) {
