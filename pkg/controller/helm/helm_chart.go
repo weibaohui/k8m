385b66fae2b65c1367286fa26716627c0a0372f7
weibaohui <weibaohui@yeah.net>
2025-03-01 23:56:59 +0800

    feat(pkg/controller/helm): 添加 Helm Chart 相关接口
    - 实现了列出 Chart、获取 Chart值和获取 Chart 版本列表的功能
    - 新增 ListChart、GetChartValue 和 ChartVersionOptionList 函数
    - 集成了数据库查询、集群管理和版本排序等功能

---
+++ pkg/controller/helm/helm_chart.go
@@ -0,0 +1,75 @@
+package helm
+
+import (
+	"sort"
+
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/helm"
+	"github.com/weibaohui/k8m/pkg/models"
+	"github.com/weibaohui/k8m/pkg/service"
+)
+
+func ListChart(c *gin.Context) {
+	// 从数据库查询列表
+	params := dao.BuildParams(c)
+	m := &models.HelmChart{}
+	items, total, err := m.List(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonListWithTotal(c, total, items)
+}
+
+// GetChartValue 获取Chart的值
+func GetChartValue(c *gin.Context) {
+	chartName := c.Param("chart")
+	version := c.Param("version")
+
+	selectedCluster := amis.GetSelectedCluster(c)
+	restConfig := service.ClusterService().GetClusterByID(selectedCluster).GetRestConfig()
+	h, err := helm.New(restConfig)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	value, err := h.GetChartValue(chartName, version)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonData(c, value)
+}
+
+// ChartVersionOptionList 获取Chart的版本列表
+func ChartVersionOptionList(c *gin.Context) {
+	repoName := c.Param("repo")
+	chartName := c.Param("chart")
+
+	h, err := getHelm(c)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	versions, err := h.GetChartVersions(repoName, chartName)
+	if err != nil {
+		amis.WriteJsonData(c, gin.H{
+			"options": make([]map[string]string, 0),
+		})
+		return
+	}
+
+	sort.Slice(versions, func(i, j int) bool {
+		return utils.CompareVersions(versions[i], versions[j])
+	})
+
+	amis.WriteJsonData(c, gin.H{
+		"options": versions,
+	})
+
+}
