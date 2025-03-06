87d987abe7cd2eed3947efe8eec0946dd5077025
weibaohui <weibaohui@yeah.net>
2025-03-06 22:25:26 +0800

    feat(models): 添加用户组模型
    - 新增 UserGroup 结构体，包含用户组相关字段
    - 实现用户组的列表、保存、删除和获取单个记录的通用方法
    - 使用 gorm 和内部 DAO 进行数据库操作

---
+++ pkg/models/user_group.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"time"
+
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"gorm.io/gorm"
+)
+
+type UserGroup struct {
+	ID          uint      `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
+	GroupName   string    `gorm:"uniqueIndex;not null" json:"group_name,omitempty"`
+	Description string    `json:"description,omitempty"`
+	CreatedBy   string    `gorm:"index" json:"created_by,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
+
+func (c *UserGroup) List(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) ([]*UserGroup, int64, error) {
+	return dao.GenericQuery(params, c, queryFuncs...)
+}
+
+func (c *UserGroup) Save(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericSave(params, c, queryFuncs...)
+}
+
+func (c *UserGroup) Delete(params *dao.Params, ids string, queryFuncs ...func(*gorm.DB) *gorm.DB) error {
+	return dao.GenericDelete(params, c, utils.ToInt64Slice(ids), queryFuncs...)
+}
+
+func (c *UserGroup) GetOne(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) (*UserGroup, error) {
+	return dao.GenericGetOne(params, c, queryFuncs...)
+}
