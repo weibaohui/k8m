3e16b9267d65cd22f2b8c3f4887018a257167700
weibaohui <weibaohui@yeah.net>
2025-03-08 23:27:22 +0800

    feat(k8sgpt): 添加 API 字段获取功能并优化分析功能
    - 新增 GetFields 函数，用于获取 Kubernetes API 字段信息
    - 重构 RunAnalysis 函数，支持根据请求参数分析指定的资源类型
    - 优化分析结果的输出格式，使用 markdown 格式
    - 移除不必要的错误处理和资源关闭逻辑

--- pkg/controller/k8sgpt/k8sgpt.go
+++ pkg/controller/k8sgpt/k8sgpt.go
@@ -1,51 +1,74 @@
 package k8sgpt
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/k8sgpt/analysis"
+	"github.com/weibaohui/k8m/pkg/k8sgpt/kubernetes"
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/klog/v2"
 )
 
+func GetFields(c *gin.Context) {
+	selectedCluster := amis.GetSelectedCluster(c)
+
+	kind := "Deployment"
+	apiDoc := kubernetes.K8sApiReference{
+		Kind: kind,
+		ApiVersion: schema.GroupVersion{
+			Group:   "apps",
+			Version: "v1",
+		},
+		OpenapiSchema: kom.Cluster(selectedCluster).Status().OpenAPISchema(),
+	}
+
+	v2 := apiDoc.GetApiDocV2("spec.template.spec.containers.imagePullPolicy")
+	amis.WriteJsonData(c, v2)
+}
 func RunAnalysis(c *gin.Context) {
 	ctx := amis.GetContextWithUser(c)
+	kind := c.Param("kind")
 	selectedCluster := amis.GetSelectedCluster(c)
 
 	config, err := analysis.NewAnalysis(ctx,
 		selectedCluster,
 		// []string{"Pod", "Service", "Deployment", "ReplicaSet", "PersistentVolumeClaim", "Service", "Ingress", "StatefulSet", "CronJob", "Node", "ValidatingWebhookConfiguration", "MutatingWebhookConfiguration", "HorizontalPodAutoScaler", "PodDisruptionBudget", "NetworkPolicy", "Log"}, // Filter for these analyzers (e.g. Pod, PersistentVolumeClaim, Service, ReplicaSet)
-		[]string{"Deployment"}, // Filter for these analyzers (e.g. Pod, PersistentVolumeClaim, Service, ReplicaSet)
+		[]string{kind}, // Filter for these analyzers (e.g. Pod, PersistentVolumeClaim, Service, ReplicaSet)
 		"*",
 		"",
 		true,
 		1,
 		true,
-		true,
+		false,
 	)
 
 	if err != nil {
 		klog.Errorf("Error: %v", err)
+		return
 	}
 	defer config.Close()
 
 	config.RunAnalysis()
-	var output = "text"
-	if err := config.GetAIResults(output, true); err != nil {
+	var output = "markdown"
+	if err := config.GetAIResults(true); err != nil {
 		color.Red("Error: %v", err)
-		os.Exit(1)
+		return
+
 	}
 	// print results
 	output_data, err := config.PrintOutput(output)
 	if err != nil {
 		color.Red("Error: %v", err)
-	}
-	statsData := config.PrintStats()
-	fmt.Println(string(statsData))
+		return
 
-	fmt.Println(string(output_data))
+	}
+	// statsData := config.PrintStats()
+	// fmt.Println(string(statsData))
+
+	amis.WriteJsonData(c, gin.H{
+		"result": string(output_data),
+	})
 
 }
