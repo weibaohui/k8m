03df04524e3bda66c273ab4f6cc37ec081f684ab
weibaohui <weibaohui@yeah.net>
2025-03-08 21:19:59 +0800

    refactor(service): 重构 chatService 以简化和提高可维护性
    - 移除了未使用的变量和函数
    - 简化了 GetChatStream 和 Chat 函数的逻辑
    - 删除了冗余的环境变量读取和配置设置
    - 优化了代码结构，提高了代码的可读性和可维护性

--- pkg/service/chat.go
+++ pkg/service/chat.go
@@ -1,101 +1,27 @@
 package service
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
 	"regexp"
 	"strings"
 
 	"github.com/sashabaranov/go-openai"
-	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"k8s.io/klog/v2"
 )
 
 type chatService struct {
-	model  string
-	apiKey string
-	apiUrl string
 }
 
-func (c *chatService) SetVars(apikey, apiUrl, model string) {
-	c.model = model
-	c.apiUrl = apiUrl
-	c.apiKey = apikey
-}
-
-// Deprecated 获取stream
-func (c *chatService) GetChatStream1(chat string) (*http.Response, error) {
-	key, apiURL, model, enable := c.getChatGPTAuth()
-	if !enable {
-		return nil, fmt.Errorf("ChatGPT功能未开启")
-	}
-
-	// url := "https://api.siliconflow.cn/v1/chat/completions"
-	url := fmt.Sprintf("%s/chat/completions", apiURL)
-
-	// 构建请求体
-	payload := map[string]interface{}{
-		"model": model,
-		"messages": []map[string]string{
-			{
-				"role":    "user",
-				"content": chat,
-			},
-		},
-		"stream": true,
-	}
-
-	// 将请求体编码为JSON
-	jsonPayload, err := json.Marshal(payload)
-	if err != nil {
-		klog.V(2).Infof("JSON编码失败:%v\n", err)
-		return nil, err
-	}
-
-	// 设置请求头
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		klog.V(2).Infof("创建请求失败:%v\n", err)
-		return nil, err
-	}
-
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", key))
-
-	// 执行请求
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		klog.V(2).Infof("请求失败:%v\n\n", err)
-		return nil, err
-	}
-	return resp, err
-}
 func (c *chatService) GetChatStream(chat string) (*openai.ChatCompletionStream, error) {
-	apiKey, apiURL, model, enable := c.getChatGPTAuth()
-	if !enable {
-		return nil, fmt.Errorf("ChatGPT功能未开启")
-	}
-	// 初始化OpenAI客户端
-	cfg := openai.DefaultConfig(apiKey)
-	cfg.BaseURL = apiURL
-	openaiClient := openai.NewClientWithConfig(cfg)
 
-	stream, err := openaiClient.CreateChatCompletionStream(context.Background(), openai.ChatCompletionRequest{
-		Model: model,
-		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: chat,
-			},
-		},
-		Stream: true,
-	})
+	client, err := AIService().DefaultClient()
+
+	if err != nil {
+		klog.V(2).Infof("获取AI服务错误 : %v\n", err)
+		return nil, fmt.Errorf("获取AI服务错误 : %v", err)
+	}
+	stream, err := client.GetStreamCompletion(context.Background(), chat)
 
 	if err != nil {
 		klog.V(2).Infof("ChatCompletion error: %v\n", err)
@@ -106,74 +32,18 @@ func (c *chatService) GetChatStream(chat string) (*openai.ChatCompletionStream, 
 
 }
 func (c *chatService) Chat(chat string) string {
-	apiKey, apiURL, model, enable := c.getChatGPTAuth()
-	if !enable {
+	client, err := AIService().DefaultClient()
+
+	if err != nil {
+		klog.V(2).Infof("获取AI服务错误 : %v\n", err)
 		return ""
 	}
-	// 初始化OpenAI客户端
-	cfg := openai.DefaultConfig(apiKey)
-	cfg.BaseURL = apiURL
-	openaiClient := openai.NewClientWithConfig(cfg)
-
-	resp, err := openaiClient.CreateChatCompletion(
-		context.TODO(),
-		openai.ChatCompletionRequest{
-			Model: model,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: chat,
-				},
-			},
-		},
-	)
-
+	result, err := client.GetCompletion(context.TODO(), chat)
 	if err != nil {
 		klog.V(2).Infof("ChatCompletion error: %v\n", err)
 		return ""
 	}
-
-	result := resp.Choices[0].Message.Content
 	return result
-}
-
-func (c *chatService) getChatGPTAuth() (apiKey string, apiURL string, model string, enable bool) {
-	// 从环境变量读取OpenAI API Key和API URL
-	// 环境变量优先
-	apiKey = os.Getenv("OPENAI_API_KEY")
-	apiURL = os.Getenv("OPENAI_API_URL")
-	model = os.Getenv("OPENAI_MODEL")
-	if apiKey == "" && apiURL == "" {
-		apiKey = c.apiKey
-		apiURL = c.apiUrl
-		klog.V(4).Infof("ChatGPT 环境变量没有设置 , 尝试使用默认配置 key:%s,url:%s\n", utils.MaskString(apiKey, 5), apiURL)
-	} else {
-		klog.V(4).Infof("ChatGPT 环境变量已设置, key:%s,url:%s\n", utils.MaskString(apiKey, 5), apiURL)
-	}
-	if apiKey != "" && apiURL != "" {
-		enable = true
-		klog.V(4).Infof("ChatGPT 启用 key:%s,url:%s\n", utils.MaskString(apiKey, 5), apiURL)
-	}
-	if model == "" {
-		// 如果环境变量没设置，保底使用内置的
-		model = c.model
-		klog.V(4).Infof("ChatGPT 默认模型:%s\n", model)
-	}
-
-	if model != "" {
-		// model 确实有值，且到这里应该为ENV环境变量的值
-		// 那么model优先使用环境变量的值
-		klog.V(4).Infof("ChatGPT 使用环境变量中设置的模型:%s\n", model)
-		c.model = model
-	}
-	c.apiKey = apiKey
-	c.apiUrl = apiURL
-	return
-}
-func (c *chatService) IsEnabled() bool {
-	_, _, _, enable := c.getChatGPTAuth()
-	klog.V(4).Infof("ChatGPT 状态:%v\n", enable)
-	return enable
 }
 
 // CleanCmd 提取Markdown包裹的命令正文
