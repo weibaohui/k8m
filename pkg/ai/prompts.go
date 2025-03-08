d1d0d0bfad71d25c45656df2a71935de04279a1b
weibaohui <weibaohui@yeah.net>
2025-03-08 21:28:16 +0800

    feat(ai): 添加 AI 生成错误处理提示的功能
    - 新增多个错误处理提示模板，包括默认模板、Prometheus 配置验证、Prometheus relabel 配置解析和 Kyverno 策略报告
    - 创建 PromptMap 以根据错误类型选择相应的提示模板- 为后续的 AI 错误处理功能做准备

---
+++ pkg/ai/prompts.go
@@ -0,0 +1,70 @@
+package ai
+
+const (
+	default_prompt = `简化以下由三个破折号分隔的Kubernetes错误信息，
+	原语言为--- %s ---；
+	错误内容：--- %s ---。
+	请以分步形式提供最可能的解决方案，字符数不超过280。
+	输出格式：
+	错误信息: {此处解释错误}
+	解决方案: {此处分步说明解决方案}
+	`
+
+	prom_conf_prompt = `Simplify the following Prometheus error message delimited by triple dashes written in --- %s --- language; --- %s ---.
+	This error came when validating the Prometheus configuration file.
+	Provide step by step instructions to fix, with suggestions, referencing Prometheus documentation if relevant.
+	Write the output in the following format in no more than 300 characters:
+	Error: {Explain error here}
+	Solution: {Step by step solution here}
+	`
+
+	prom_relabel_prompt = `
+	Return your prompt in this language: %s, beginning with
+	The following is a list of the form:
+	job_name:
+	{Prometheus job_name}
+	relabel_configs:
+	{Prometheus relabel_configs}
+	kubernetes_sd_configs:
+	{Prometheus service discovery config}
+	---
+	%s
+	---
+	For each job_name, describe the Kubernetes service and pod labels,
+	namespaces, ports, and containers they match.
+	Return the message:
+	Discovered and parsed Prometheus scrape configurations.
+	For targets to be scraped by Prometheus, ensure they are running with
+	at least one of the following label sets:
+	Then for each job, write this format:
+	- Job: {job_name}
+	  - Service Labels:
+	    - {list of service labels}
+	  - Pod Labels:
+	    - {list of pod labels}
+	  - Namespaces:
+	    - {list of namespaces}
+	  - Ports:
+	    - {list of ports}
+	  - Containers:
+	    - {list of container names}
+	`
+
+	kyverno_prompt = `Simplify the following Kyverno warnings message delimited by triple dashes written in --- %s --- language; --- %s ---.
+	Provide the most probable solution as a kubectl command. 
+
+	Write the output in the following format, for the solution, only show the kubectl command:
+	
+	Error: {Explain error here}
+
+	Solution: {kubectl command}
+	`
+)
+
+var PromptMap = map[string]string{
+	"default":                       default_prompt,
+	"PrometheusConfigValidate":      prom_conf_prompt,
+	"PrometheusConfigRelabelReport": prom_relabel_prompt,
+	"PolicyReport":                  kyverno_prompt,
+	"ClusterPolicyReport":           kyverno_prompt,
+}
