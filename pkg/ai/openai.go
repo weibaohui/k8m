694958bce281bd56bc2a96833e2fcc3e88ff79b7
weibaohui <weibaohui@yeah.net>
2025-03-08 21:28:04 +0800

    feat(ai): 添加 OpenAI 客户端支持
    - 实现了一个新的 OpenAIClient 结构体，用于与 OpenAI API 进行交互
    - 添加了 Configure 方法来配置 OpenAI 客户端- 实现了 GetCompletion 和 GetStreamCompletion 方法来获取 AI 生成的文本
    - 添加了自定义 HTTP 头部传输逻辑，以支持特殊的请求头

---
+++ pkg/ai/openai.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2023 The K8sGPT Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ai
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/url"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+const openAIClientName = "openai"
+
+type OpenAIClient struct {
+	nopCloser
+	client      *openai.Client
+	model       string
+	temperature float32
+	topP        float32
+	// organizationId string
+}
+
+func (c *OpenAIClient) Configure(config IAIConfig) error {
+	token := config.GetPassword()
+	cfg := openai.DefaultConfig(token)
+	orgId := config.GetOrganizationId()
+	proxyEndpoint := config.GetProxyEndpoint()
+
+	baseURL := config.GetBaseURL()
+	if baseURL != "" {
+		cfg.BaseURL = baseURL
+	}
+
+	transport := &http.Transport{}
+	if proxyEndpoint != "" {
+		proxyUrl, err := url.Parse(proxyEndpoint)
+		if err != nil {
+			return err
+		}
+		transport.Proxy = http.ProxyURL(proxyUrl)
+	}
+
+	if orgId != "" {
+		cfg.OrgID = orgId
+	}
+
+	customHeaders := config.GetCustomHeaders()
+	cfg.HTTPClient = &http.Client{
+		Transport: &OpenAIHeaderTransport{
+			Origin:  transport,
+			Headers: customHeaders,
+		},
+	}
+
+	client := openai.NewClientWithConfig(cfg)
+	if client == nil {
+		return errors.New("error creating OpenAI client")
+	}
+	c.client = client
+	c.model = config.GetModel()
+	c.temperature = config.GetTemperature()
+	c.topP = config.GetTopP()
+	return nil
+}
+
+func (c *OpenAIClient) GetCompletion(ctx context.Context, prompt string) (string, error) {
+
+	// Create a completion request
+	resp, err := c.client.CreateChatCompletion(ctx,
+		openai.ChatCompletionRequest{
+			Model: c.model,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		})
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
+func (c *OpenAIClient) GetStreamCompletion(ctx context.Context, prompt string) (*openai.ChatCompletionStream, error) {
+	stream, err := c.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
+		Model: c.model,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: prompt,
+			},
+		},
+		Stream: true,
+	})
+	return stream, err
+}
+
+func (c *OpenAIClient) GetName() string {
+	return openAIClientName
+}
+
+// OpenAIHeaderTransport is an http.RoundTripper that adds the given headers to each request.
+type OpenAIHeaderTransport struct {
+	Origin  http.RoundTripper
+	Headers []http.Header
+}
+
+// RoundTrip implements the http.RoundTripper interface.
+func (t *OpenAIHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Clone the request to avoid modifying the original request
+	clonedReq := req.Clone(req.Context())
+	for _, header := range t.Headers {
+		for key, values := range header {
+			// Possible values per header:  RFC 2616
+			for _, value := range values {
+				clonedReq.Header.Add(key, value)
+			}
+		}
+	}
+
+	return t.Origin.RoundTrip(clonedReq)
+}
