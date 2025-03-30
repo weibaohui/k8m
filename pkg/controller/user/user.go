c3800d8b6393cafac72ce40855e97e3c407bd233
weibaohui <weibaohui@yeah.net>
2025-03-30 18:36:57 +0800

    feat(controller): 添加用户选项列表功能
    新增 UserOptionList 函数，用于获取并返回按用户名排序的用户选项列表。该功能通过查询数据库中的用户名，并将其格式化为选项列表，以便在前端展示

--- pkg/controller/user/user.go
+++ pkg/controller/user/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 
+	"github.com/duke-git/lancet/v2/slice"
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/internal/dao"
 	"github.com/weibaohui/k8m/pkg/comm/utils"
@@ -175,4 +176,31 @@ func genQueryFuncs(c *gin.Context, params *dao.Params) []func(*gorm.DB) *gorm.DB
 
 	}
 	return queryFuncs
+}
+
+func UserOptionList(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.User{}
+	items, _, err := m.List(params, func(db *gorm.DB) *gorm.DB {
+		return db.Distinct("username")
+	})
+	if err != nil {
+		amis.WriteJsonData(c, gin.H{
+			"options": make([]map[string]string, 0),
+		})
+		return
+	}
+	var names []map[string]string
+	for _, n := range items {
+		names = append(names, map[string]string{
+			"label": n.Username,
+			"value": n.Username,
+		})
+	}
+	slice.SortBy(names, func(a, b map[string]string) bool {
+		return a["label"] < b["label"]
+	})
+	amis.WriteJsonData(c, gin.H{
+		"options": names,
+	})
 }
