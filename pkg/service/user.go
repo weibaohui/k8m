fb3cf1adb3aab4ea2cea708942ebc8cb1ea889ba
weibaohui <weibaohui@yeah.net>
2025-03-30 19:06:10 +0800

    feat(user): 添加获取用户集群角色权限的功能
    新增 `GetClusterRole` 方法，用于查询用户在指定集群中的角色权限。该方法通过 GORM 查询数据库，返回用户在该集群中的角色信息。

--- pkg/service/user.go
+++ pkg/service/user.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/weibaohui/k8m/internal/dao"
 	"github.com/weibaohui/k8m/pkg/models"
+	"gorm.io/gorm"
 )
 
 type userService struct {
@@ -18,4 +19,19 @@ func (u *userService) List() ([]*models.User, error) {
 		return nil, err
 	}
 	return list, nil
+}
+
+// GetClusterRole 获取用户在指定集群中的角色权限
+func (u *userService) GetClusterRole(cluster string, username string) (string, error) {
+	params := &dao.Params{}
+	clusterRole := &models.ClusterUserRole{}
+	queryFunc := func(db *gorm.DB) *gorm.DB {
+		return db.Where("cluster = ? AND username = ?", cluster, username)
+	}
+	role, err := clusterRole.GetOne(params, queryFunc)
+
+	if err != nil {
+		return "", err
+	}
+	return role.Role, nil
 }
