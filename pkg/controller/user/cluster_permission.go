65f91878a82816590d3c48d25b7e4652a37db83d
weibaohui <weibaohui@yeah.net>
2025-03-30 18:36:49 +0800

    refactor(user): 重构保存集群权限逻辑以支持多用户
    重构 `SaveClusterPermission` 函数，使其能够处理多个用户的权限保存。新增了对用户列表的解析和处理逻辑，并优化了错误处理和日志记录。

--- pkg/controller/user/cluster_permission.go
+++ pkg/controller/user/cluster_permission.go
@@ -2,14 +2,18 @@ package user
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
 	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/k8m/pkg/models"
 	"github.com/weibaohui/k8m/pkg/service"
+	"gorm.io/gorm"
+	"k8s.io/klog/v2"
 )
 
 func ListClusterPermissions(c *gin.Context) {
@@ -25,26 +29,59 @@ func ListClusterPermissions(c *gin.Context) {
 }
 
 func SaveClusterPermission(c *gin.Context) {
+	clusterBase64 := c.Param("cluster")
+	role := c.Param("role")
+	cluster, err := utils.DecodeBase64(clusterBase64)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	// {"users":"lisi,no2fa,test"}
+	type requestBody struct {
+		Users string `json:"users"`
+	}
+	var userList requestBody
+
+	err = c.ShouldBindJSON(&userList)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	if userList.Users == "" {
+		amis.WriteJsonError(c, fmt.Errorf("用户列表不能为空"))
+		return
+	}
+
+	_, _, err = handlePermissionCommonLogic(c, "保存", fmt.Sprintf("cluster %s save permission %v", cluster, userList.Users))
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
 	params := dao.BuildParams(c)
-	m := &models.ClusterUserRole{}
-	err := c.ShouldBindJSON(&m)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
+
+	for _, username := range strings.Split(userList.Users, ",") {
+		var m models.ClusterUserRole
+		m.Cluster = cluster
+		m.Role = role
+		m.Username = username
+		one, err := m.GetOne(params, func(db *gorm.DB) *gorm.DB {
+			return db.Where(m)
+		})
+
+		if err != nil || one == nil {
+			// 不在用户权限条目，则添加
+			err := m.Save(params)
+			if err != nil {
+				klog.V(6).Infof("新增用户权限失败: %s", err.Error())
+				continue
+			}
+		}
+
+		// 如果存在该集群下的用户条目，则跳过，不做处理
 	}
 
-	_, _, err = handlePermissionCommonLogic(c, "保存", m.Cluster)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-
-	err = m.Save(params)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	amis.WriteJsonData(c, gin.H{"id": m.ID})
+	amis.WriteJsonOK(c)
 }
 
 func DeleteClusterPermission(c *gin.Context) {
