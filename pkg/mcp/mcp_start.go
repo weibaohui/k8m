599735896cf148f7df94c15f6e6567f641edb6b4
weibaohui <weibaohui@yeah.net>
2025-04-03 00:28:04 +0800

    feat(mcp): 添加启动MCP服务器的功能
    在pkg/mcp/mcp_start.go中实现了Start函数，用于启动MCP服务器。该函数接收版本号和端口号作为参数，并配置服务器的上下文、资源能力、日志记录等选项。通过从请求头中提取用户名和角色信息，并将其添加到上下文中，确保服务器在处理请求时能够正确识别用户身份。

--- pkg/mcp/mcp_start.go
+++ pkg/mcp/mcp_start.go
@@ -1,1 +1,40 @@
 package mcp
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/mark3labs/mcp-go/server"
+	"github.com/weibaohui/k8m/pkg/constants"
+	"github.com/weibaohui/kom/mcp"
+	"github.com/weibaohui/kom/mcp/metadata"
+	"k8s.io/klog/v2"
+)
+
+func Start(version string, port int) {
+	var ctxFn = func(ctx context.Context, r *http.Request) context.Context {
+		username := r.Header.Get(constants.JwtUserName)
+		role := r.Header.Get(constants.JwtUserRole)
+		klog.Infof("%s: %s", constants.JwtUserName, username)
+		klog.Infof("%s: %s", constants.JwtUserRole, role)
+		ctx = context.WithValue(ctx, constants.JwtUserName, username)
+		ctx = context.WithValue(ctx, constants.JwtUserRole, role)
+		return ctx
+	}
+	cfg := metadata.ServerConfig{
+		Name:    "k8m mcp server",
+		Version: version,
+		Port:    port,
+		ServerOptions: []server.ServerOption{
+			server.WithResourceCapabilities(false, false),
+			server.WithPromptCapabilities(false),
+			server.WithLogging(),
+		},
+		SSEOption: []server.SSEOption{
+			server.WithSSEContextFunc(ctxFn),
+		},
+		AuthKey:     constants.JwtUserName,
+		AuthRoleKey: constants.JwtUserRole,
+	}
+	mcp.RunMCPServerWithOption(&cfg)
+}
