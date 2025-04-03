0dd99eae8a0e4d4fc33d33648321063429bebbde
weibaohui <weibaohui@yeah.net>
2025-04-03 20:50:35 +0800

    refactor(middleware): 重构认证和集群选择中间件逻辑
    将JWT认证逻辑提取到utils包中，简化auth.go代码。在cookie.go中，根据用户角色和集群权限优化集群选择逻辑，确保用户只能访问有权限的集群。

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,58 +1,27 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v4"
-	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/k8m/pkg/flag"
-	"k8s.io/klog/v2"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
-
-	// 定义 JWT 密钥
-	cfg := flag.Init()
-	var jwtSecret = []byte(cfg.JwtTokenSecret)
-
 	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
-		if tokenString == "" {
-			// 尝试从query中获取
-			tokenString = c.Query("token")
-			klog.V(6).Infof("从query中获取token为%v", tokenString)
-			if tokenString == "" {
-				c.JSON(http.StatusUnauthorized, gin.H{"message": "未提供 Token"})
-				c.Abort()
-				return
-			}
-		}
-
-		if strings.HasPrefix(tokenString, "Bearer ") {
-			// token Bear xxxx
-			tokenString = tokenString[7:]
-		}
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return jwtSecret, nil
-		})
-		if err != nil || !token.Valid {
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "Token 无效"})
+		cfg := flag.Init()
+		claims, err := utils.GetJWTClaims(c, cfg.JwtTokenSecret)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
 			c.Abort()
 			return
 		}
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			amis.WriteJsonError(c, fmt.Errorf("invalid JWT claims"))
-			c.Abort()
-			return
-		}
+
 		c.Set(constants.JwtUserName, claims[constants.JwtUserName])
 		c.Set(constants.JwtUserRole, claims[constants.JwtUserRole])
+		c.Set(constants.JwtClusters, claims[constants.JwtClusters])
 		c.Next()
 	}
 }
--- pkg/middleware/cookie.go
+++ pkg/middleware/cookie.go
@@ -1,20 +1,24 @@
 package middleware
 
 import (
+	"net/http"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/k8m/pkg/flag"
+	"github.com/weibaohui/k8m/pkg/models"
 	"github.com/weibaohui/k8m/pkg/service"
-	"k8s.io/klog/v2"
-	"slices"
 )
 
 func EnsureSelectedClusterMiddleware() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+
 		// 获取请求路径
 		path := c.Request.URL.Path
 
@@ -30,15 +34,64 @@ func EnsureSelectedClusterMiddleware() gin.HandlerFunc {
 			}
 		}
 
+		if path == "/" ||
+			path == "/favicon.ico" ||
+			path == "/auth/login" ||
+			path == "/mgm/user/list" ||
+			path == "/mgm/config/AnySelect" ||
+			path == "/mgm/user_group/option_list" ||
+			strings.HasPrefix(path, "/assets/") ||
+			strings.HasPrefix(path, "/public/") ||
+			strings.Contains(path, "/cluster/file/option_list") ||
+			strings.Contains(path, "/cluster/scan") ||
+			strings.Contains(path, "/cluster/all") ||
+			strings.Contains(path, "/cluster/kubeconfig/save") ||
+			strings.Contains(path, "/cluster/kubeconfig/remove") ||
+			strings.Contains(path, "/cluster/reconnect") ||
+			strings.Contains(path, "/cluster/setDefault") {
+			c.Next()
+			return
+
+		}
+
 		cfg := flag.Init()
+		claims, err := utils.GetJWTClaims(c, cfg.JwtTokenSecret)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			c.Abort()
+			return
+		}
+		role := claims[constants.JwtUserRole].(string)
+		cst := claims[constants.JwtClusters].(string)
+		csts := strings.Split(cst, ",")
+		// klog.V(6).Infof("username=%s", username)
+
 		var clusterID string
 		allClusters := service.ClusterService().AllClusters()
 		// 检查是否存在名为 "selectedCluster" 的 Cookie
 		sc, err := c.Cookie("selectedCluster")
 		if err != nil {
 			// 不存在cookie
-			clusterID = service.ClusterService().FirstClusterID()
 
+			if role == models.RolePlatformAdmin {
+				// 平台管理员，选一个
+				clusterID = service.ClusterService().FirstClusterID()
+			} else {
+				// 其他的只能选有权限中的某一个
+				if len(csts) > 0 {
+					clusterID = csts[0]
+				}
+
+			}
+
+			if clusterID == "" {
+				// 没有集群，说明不是管理员，也没有权限，那么就不要访问了
+				c.JSON(512, gin.H{
+					"msg": "no cluster",
+				})
+				c.Abort()
+				return
+			}
 			// 如果 Cookie 不存在，写入一个默认值
 			c.SetCookie(
 				"selectedCluster",           // Cookie 名称
@@ -64,45 +117,20 @@ func EnsureSelectedClusterMiddleware() gin.HandlerFunc {
 				false,                       // 是否 HttpOnly
 			)
 		}
-		// 非集群内模式下，但是用了InCluster，肯定不对，需要纠正过来
-		if !cfg.InCluster && sc == "InCluster" {
-			// 非集群内模式,但是当前cookie是InCluster,那么给他纠正过来
-			clusterID = service.ClusterService().FirstClusterID()
-			c.SetCookie(
-				"selectedCluster",           // Cookie 名称
-				clusterID,                   // Cookie 默认值
-				int(24*time.Hour.Seconds()), // 有效期（秒），这里是 1 天
-				"/",                         // Cookie 路径
-				"",                          // 域名默认当前域
-				false,                       // 是否仅 HTTPS
-				false,                       // 是否 HttpOnly
-			)
-		}
 
+		// 如果sc为空，说明没有选择，不能直接使用
+		// 或者sc不在权限列表中
 		// 如果设置了sc，但是不能用
-		if sc != "" && !service.ClusterService().IsConnected(sc) {
-			// 前端跳转到集群选择页面
-			// 所以要排除集群页面的路径
-			path := c.Request.URL.Path
-			klog.V(6).Infof("c.Request.URL.Path=%s", path)
-			if !(path == "/" ||
-				path == "/favicon.ico" ||
-				path == "/auth/login" ||
-				strings.HasPrefix(path, "/assets/") ||
-				strings.HasPrefix(path, "/public/") ||
-				strings.Contains(path, "/cluster/file/option_list") ||
-				strings.Contains(path, "/cluster/scan") ||
-				strings.Contains(path, "/cluster/all") ||
-				strings.Contains(path, "/cluster/kubeconfig/save") ||
-				strings.Contains(path, "/cluster/kubeconfig/remove") ||
-				strings.Contains(path, "/cluster/reconnect") ||
-				strings.Contains(path, "/cluster/setDefault")) {
-				c.JSON(512, gin.H{
-					"msg": sc,
-				})
-				c.Abort()
-			}
+		// 或者设置了sc，但是不在权限列表中（平台管理员为空，要排除）
+		// klog.V(6).Infof("csts=%v", csts)
+		// klog.V(6).Infof("sc=%v", sc)
+		if (sc != "" && !service.ClusterService().IsConnected(sc)) || (sc == "") || (sc != "" && role != models.RolePlatformAdmin && !slices.Contains(csts, sc)) {
 
+			//512 集群选择的跳转
+			c.JSON(512, gin.H{
+				"msg": sc,
+			})
+			c.Abort()
 		}
 		// 继续处理下一个中间件或最终路由
 		c.Next()
