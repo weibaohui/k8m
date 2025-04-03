8da8476fc0faae39d05da6b98b99144d9eff7b26
weibaohui <weibaohui@yeah.net>
2025-04-03 21:27:32 +0800

    feat(api): 添加容器健康检查的GET和POST接口
    添加了`/container_health_checks`的GET接口用于获取容器健康检查信息，以及`/update_health_checks`的POST接口用于更新容器健康检查配置。这些接口扩展了API的功能，支持对容器健康检查的管理。

--- main.go
+++ main.go
@@ -188,8 +188,10 @@ func main() {
 		api.GET("/:kind/group/:group/version/:version/container_info/ns/:ns/name/:name/container/:container_name", dynamic.ContainerInfo)
 		api.GET("/:kind/group/:group/version/:version/container_resources_info/ns/:ns/name/:name/container/:container_name", dynamic.ContainerResourcesInfo)
 		api.GET("/:kind/group/:group/version/:version/image_pull_secrets/ns/:ns/name/:name", dynamic.ImagePullSecretOptionList)
+		api.GET("/:kind/group/:group/version/:version/container_health_checks/ns/:ns/name/:name/container/:container_name", dynamic.ContainerHealthChecksInfo)
 		api.POST("/:kind/group/:group/version/:version/update_image/ns/:ns/name/:name", dynamic.UpdateImageTag)
 		api.POST("/:kind/group/:group/version/:version/update_resources/ns/:ns/name/:name", dynamic.UpdateResources)
+		api.POST("/:kind/group/:group/version/:version/update_health_checks/ns/:ns/name/:name", dynamic.UpdateHealthChecks)
 
 		// 节点亲和性
 		api.POST("/:kind/group/:group/version/:version/update_node_affinity/ns/:ns/name/:name", dynamic.UpdateNodeAffinity)
