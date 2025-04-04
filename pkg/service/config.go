5db4481b5cca591bf3f5d9de723071906b281c8e
weibaohui <weibaohui@yeah.net>
2025-04-04 23:25:28 +0800

    refactor(config): 移除context参数并添加配置更新逻辑
    移除`GetConfig`、`UpdateConfig`和`InitDefaultConfig`方法中的`context.Context`参数，简化代码结构。新增`UpdateFlagFromDBConfig`方法，用于从数据库加载配置并更新Flag中的值，确保配置更改后立即生效。

--- pkg/service/config.go
+++ pkg/service/config.go
@@ -1,9 +1,8 @@
 package service
 
 import (
-	"context"
-
 	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/flag"
 	"github.com/weibaohui/k8m/pkg/models"
 	"gorm.io/gorm"
 )
@@ -16,25 +15,78 @@ func NewConfigService() *configService {
 	return &configService{db: dao.DB()}
 }
 
-func (s *configService) GetConfig(ctx context.Context) (*models.Config, error) {
+func (s *configService) GetConfig() (*models.Config, error) {
 	var config models.Config
-	if err := s.db.WithContext(ctx).First(&config).Error; err != nil {
+	if err := s.db.First(&config).Error; err != nil {
 		return nil, err
 	}
 	return &config, nil
 }
 
-func (s *configService) UpdateConfig(ctx context.Context, config *models.Config) error {
-	return s.db.WithContext(ctx).Save(config).Error
-}
+func (s *configService) UpdateConfig(config *models.Config) error {
 
-func (s *configService) InitDefaultConfig(ctx context.Context) error {
-	var count int64
-	if err := s.db.WithContext(ctx).Model(&models.Config{}).Count(&count).Error; err != nil {
+	err := s.db.Save(config).Error
+	if err != nil {
 		return err
 	}
-	if count == 0 {
-		return s.db.WithContext(ctx).Create(&models.Config{}).Error
+	// 保存后，让其生效
+	err = s.UpdateFlagFromDBConfig()
+	if err != nil {
+		return err
 	}
+	return nil
+}
+
+// UpdateFlagFromDBConfig 从数据库中加载配置，更新Flag方法中的值
+func (s *configService) UpdateFlagFromDBConfig() error {
+	cfg := flag.Init()
+	m, err := s.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	cfg.AnySelect = m.AnySelect
+	cfg.Debug = m.Debug
+	cfg.InCluster = m.InCluster
+
+	if m.ApiKey != "" {
+		cfg.ApiKey = m.ApiKey
+	}
+	if m.ApiModel != "" {
+		cfg.ApiModel = m.ApiModel
+	}
+	if m.ApiURL != "" {
+		cfg.ApiURL = m.ApiURL
+	}
+
+	if m.KubeConfig != "" {
+		cfg.KubeConfig = m.KubeConfig
+	}
+	if m.KubectlShellImage != "" {
+		cfg.KubectlShellImage = m.KubectlShellImage
+	}
+	if m.NodeShellImage != "" {
+		cfg.NodeShellImage = m.NodeShellImage
+	}
+	if m.Port > 0 {
+		cfg.Port = m.Port
+	}
+	if m.SqlitePath != "" {
+		cfg.SqlitePath = m.SqlitePath
+	}
+	if m.MCPServerPort > 0 {
+		cfg.MCPServerPort = m.MCPServerPort
+	}
+	if m.LogV > 0 {
+		cfg.LogV = m.LogV
+	}
+
+	cfg.PrintConfig = m.PrintConfig
+
+	// JwtTokenSecret 暂不启用，因为前端也要处理
+	// cfg.JwtTokenSecret = m.JwtTokenSecret
+	// LoginType 暂不启用，因为就一种password
+	// cfg.LoginType = m.LoginType
+
 	return nil
 }
