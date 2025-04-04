25efbb35b74da0b41b34b4c1033ca47a263a4705
weibaohui <weibaohui@yeah.net>
2025-04-04 21:15:13 +0800

    refactor(ui): 调整工具栏用户信息显示布局
    将用户名从下拉菜单中移至头像右侧，并更改用户角色显示为“角色”，以提升界面布局的清晰度和用户体验

--- ui/src/components/ToolBar/index.tsx
+++ ui/src/components/ToolBar/index.tsx
@@ -43,7 +43,7 @@ const Toolbar = () => {
             key: 'username',
             label: (
                 <div style={{ padding: '4px 0', display: 'flex', alignItems: 'center', gap: '8px' }}>
-                    <span>{userInfo.username}</span>
+                    <span>角色</span>
                     <span style={{
                         backgroundColor: '#f0f0f0',
                         padding: '2px 8px',
@@ -87,13 +87,16 @@ const Toolbar = () => {
             <Space>
                 <li>
                     <Dropdown menu={{ items: menuItems }} placement='bottomRight'>
-                        <Avatar size="small" style={{ backgroundColor: '#1677ff', cursor: 'pointer' }}>
-                            <UserOutlined style={{ fontSize: 14 }} />
-                        </Avatar>
+                        <span style={{ cursor: 'pointer' }}>
+                            <Avatar size="small" style={{ backgroundColor: '#1677ff' }} >
+                                <UserOutlined style={{ fontSize: 14 }} />
+                            </Avatar>
+                            <span className='ml-1'>{userInfo.username}</span>
+                        </span>
                     </Dropdown>
                 </li>
             </Space>
-        </div>
+        </div >
     );
 };
 
