0d7109290831de52f17e9a9ab3fc7c8d1a9b6bc2
weibaohui <weibaohui@yeah.net>
2025-04-05 12:31:07 +0800

    feat(controller): 添加获取参数配置的功能
    新增了Config函数，用于根据传入的key获取相应的参数配置，并将其以JSON格式返回。该功能为控制器提供了统一的参数配置获取接口。

---
+++ pkg/controller/param/config.go
@@ -0,0 +1,21 @@
+package param
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/flag"
+)
+
+// Config 获取某一个参数配置
+func Config(c *gin.Context) {
+	key := c.Param("key")
+	cfg := flag.Init()
+	s := ""
+	switch key {
+	case "AnySelect":
+		s = fmt.Sprintf("%v", cfg.AnySelect)
+	}
+	amis.WriteJsonData(c, s)
+}
