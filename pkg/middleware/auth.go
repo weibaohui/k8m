5c8cf1576ff8e7b8eabd52bf0b9ad7a28f5df709
weibaohui <weibaohui@yeah.net>
2025-04-05 12:05:36 +0800

    feat(middleware): 添加平台管理员角色校验中间件
    新增 `PlatformAuthMiddleware` 中间件，用于校验用户是否具有平台管理员角色。通过解析 JWT 中的角色信息并进行权限检查，确保只有具备平台管理员角色的用户才能访问相关资源。

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -2,13 +2,17 @@ package middleware
 
 import (
 	"net/http"
+	"slices"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/k8m/pkg/flag"
+	"github.com/weibaohui/k8m/pkg/models"
 )
 
+// AuthMiddleware 登录校验
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		cfg := flag.Init()
@@ -22,6 +26,35 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Set(constants.JwtUserName, claims[constants.JwtUserName])
 		c.Set(constants.JwtUserRole, claims[constants.JwtUserRole])
 		c.Set(constants.JwtClusters, claims[constants.JwtClusters])
+		c.Next()
+	}
+}
+
+// PlatformAuthMiddleware 平台管理员角色校验
+func PlatformAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cfg := flag.Init()
+		claims, err := utils.GetJWTClaims(c, cfg.JwtTokenSecret)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			c.Abort()
+			return
+		}
+		username := claims[constants.JwtUserName].(string)
+		role := claims[constants.JwtUserRole].(string)
+		cst := claims[constants.JwtClusters].(string)
+
+		// 权限检查
+		roles := strings.Split(role, ",")
+		if !slices.Contains(roles, models.RolePlatformAdmin) {
+			c.JSON(http.StatusForbidden, gin.H{"error": "平台管理员权限校验失败"})
+			c.Abort()
+			return
+		}
+
+		c.Set(constants.JwtUserName, username)
+		c.Set(constants.JwtUserRole, role)
+		c.Set(constants.JwtClusters, cst)
 		c.Next()
 	}
 }
