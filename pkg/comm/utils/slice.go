25f1ecc5722af3b5e13c3ccc550cf52ca197e8ff
weibaohui <weibaohui@yeah.net>
2025-04-05 21:37:11 +0800

    feat(utils): 添加 AllIn 函数用于判断数组包含关系
    AllIn 函数用于判断 A 数组中的元素是否都存在于 B 数组中。
    此函数通过将 B 数组转换为 map 结构来提高查找效率，然后遍历 A 数组进行比较。

--- pkg/comm/utils/slice.go
+++ pkg/comm/utils/slice.go
@@ -10,3 +10,25 @@ func RemoveEmptyLines(lines []string) []string {
 	}
 	return result
 }
+
+// AllIn 判断A数组中的元素，是否都在B数组中
+// a := []string{"apple", "banana"}
+// b := []string{"apple", "banana", "cherry"}
+//
+// fmt.Println(AllIn(a, b)) // true
+//
+// a2 := []string{"apple", "kiwi"}
+// fmt.Println(AllIn(a2, b)) // false
+func AllIn(a, b []string) bool {
+	bSet := make(map[string]struct{}, len(b))
+	for _, item := range b {
+		bSet[item] = struct{}{}
+	}
+
+	for _, item := range a {
+		if _, ok := bSet[item]; !ok {
+			return false
+		}
+	}
+	return true
+}
