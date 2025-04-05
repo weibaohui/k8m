49c5b856e80de73d32f553c7d3e5002abdb90dcb
weibaohui <weibaohui@yeah.net>
2025-04-05 21:37:03 +0800

    feat(ns): 根据用户权限筛选命名空间
    - 新增逻辑判断用户是否具有限制的命名空间权限
    - 如果用户具有限制的命名空间权限，则只显示这些命名空间
    - 否则，显示集群中的所有命名空间
    - 对显示的命名空间列表进行排序

--- pkg/controller/ns/ns.go
+++ pkg/controller/ns/ns.go
@@ -1,9 +1,12 @@
 package ns
 
 import (
+	"strings"
+
 	"github.com/duke-git/lancet/v2/slice"
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
 	"github.com/weibaohui/k8m/pkg/service"
 	"github.com/weibaohui/kom/kom"
 	v1 "k8s.io/api/core/v1"
@@ -30,24 +33,48 @@ func OptionList(c *gin.Context) {
 	}
 
 	// 没有指定的情况
-	var ns []v1.Namespace
-	err := kom.Cluster(selectedCluster).WithContext(ctx).Resource(&v1.Namespace{}).List(&ns).Error
-	if err != nil {
-		amis.WriteJsonData(c, gin.H{
-			"options": make([]map[string]string, 0),
+	// 先看jwt登录用户中，是否有限制的ns
+	_, _, clusterUserRoles := amis.GetLoginUserWithClusterRoles(c)
+	if clusterUserRoles != nil {
+		// 先筛选带有ns的授权列表
+		clusterUserRoles = slice.Filter(clusterUserRoles, func(index int, item *models.ClusterUserRole) bool {
+			return item.Namespaces != "" && item.Cluster == selectedCluster
 		})
-		return
+		if len(clusterUserRoles) > 0 {
+			// 具有授权列表，摘取其中的ns
+			for _, item := range clusterUserRoles {
+				if item.Namespaces != "" {
+					ns := strings.Split(item.Namespaces, ",")
+					for _, n := range ns {
+						list = append(list, map[string]string{
+							"label": n,
+							"value": n,
+						})
+					}
+				}
+			}
+		} else {
+			// 授权列表中，都没有指定ns，说明不需要按ns进行过滤
+			// 那么读取集群中的ns
+			var ns []v1.Namespace
+			err := kom.Cluster(selectedCluster).WithContext(ctx).Resource(&v1.Namespace{}).List(&ns).Error
+			if err != nil {
+				amis.WriteJsonData(c, gin.H{
+					"options": make([]map[string]string, 0),
+				})
+				return
+			}
+
+			for _, n := range ns {
+				list = append(list, map[string]string{
+					"label": n.Name,
+					"value": n.Name,
+				})
+			}
+		}
+
 	}
-	// list = append(list, map[string]string{
-	// 	"label": "全部",
-	// 	"value": "*",
-	// })
-	for _, n := range ns {
-		list = append(list, map[string]string{
-			"label": n.Name,
-			"value": n.Name,
-		})
-	}
+
 	slice.SortBy(list, func(a, b map[string]string) bool {
 		return a["label"] < b["label"]
 	})
