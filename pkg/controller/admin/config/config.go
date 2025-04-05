cb72730514ac08b63884294f0c227a83a511b3ab
weibaohui <weibaohui@yeah.net>
2025-04-05 12:30:18 +0800

    feat(admin/config): 添加配置管理功能
    添加新的配置文件 config.go，实现获取和更新配置的功能。通过该功能，管理员可以通过 API 接口管理和更新系统配置。

---
+++ pkg/controller/admin/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
+	"github.com/weibaohui/k8m/pkg/service"
+)
+
+func GetConfig(c *gin.Context) {
+	config, err := service.ConfigService().GetConfig()
+
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonData(c, config)
+}
+
+func UpdateConfig(c *gin.Context) {
+	var config models.Config
+	if err := c.ShouldBindJSON(&config); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	if err := service.ConfigService().UpdateConfig(&config); err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
