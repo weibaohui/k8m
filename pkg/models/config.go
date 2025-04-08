8cc6594db34c879f8009a6b28893c554b209d885
weibaohui <weibaohui@yeah.net>
2025-04-08 22:07:54 +0800

    refactor(models): 更新Config结构体和初始化配置表
    调整Config结构体的字段默认值，并在初始化配置表时添加新字段EnableAI和LoginType，以增强配置的灵活性和功能性

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -12,18 +12,18 @@ type Config struct {
 	ApiKey            string    `json:"api_key,omitempty"`
 	ApiURL            string    `json:"api_url,omitempty"`
 	ApiModel          string    `json:"api_model,omitempty"`
-	Debug             bool      `json:"debug,omitempty"`
+	Debug             bool      `gorm:"default:false" json:"debug"`
 	LogV              int       `json:"log_v,omitempty"`
-	InCluster         bool      `json:"in_cluster,omitempty"`
+	InCluster         bool      `gorm:"default:true" json:"in_cluster"`
 	LoginType         string    `json:"login_type,omitempty"`
 	JwtTokenSecret    string    `json:"jwt_token_secret,omitempty"`
 	NodeShellImage    string    `json:"node_shell_image,omitempty"`
 	KubectlShellImage string    `json:"kubectl_shell_image,omitempty"`
 	SqlitePath        string    `json:"sqlite_path,omitempty"`
-	AnySelect         bool      `json:"any_select,omitempty"`
-	PrintConfig       bool      `json:"print_config,omitempty"`
-	EnableAI          bool      `json:"enable_ai,omitempty"`       // 是否启用AI功能，默认开启
-	ConnectCluster    bool      `json:"connect_cluster,omitempty"` // 启动集群是是否自动连接现有集群，默认关闭
-	CreatedAt         time.Time `json:"created_at,omitempty"`      // Automatically managed by GORM for creation time
-	UpdatedAt         time.Time `json:"updated_at,omitempty"`      // Automatically managed by GORM for update time
+	AnySelect         bool      `gorm:"default:true" json:"any_select"`
+	PrintConfig       bool      `json:"print_config"`
+	EnableAI          bool      `gorm:"default:true" json:"enable_ai"` // 是否启用AI功能，默认开启
+	ConnectCluster    bool      `json:"connect_cluster"`               // 启动集群是是否自动连接现有集群，默认关闭
+	CreatedAt         time.Time `json:"created_at,omitempty"`          // Automatically managed by GORM for creation time
+	UpdatedAt         time.Time `json:"updated_at,omitempty"`          // Automatically managed by GORM for update time
 }
