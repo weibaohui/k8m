8685283a3deeec4a68577488717ae7b0e9436984
weibaohui <weibaohui@yeah.net>
2025-04-08 23:48:31 +0800

    feat(operation_log): 添加操作日志的缓冲和定时刷新功能
    引入缓冲区和定时刷新机制，以提高操作日志的写入性能。通过使用sync.Mutex确保线程安全，并利用time.Ticker定期刷新缓冲区到数据库。当缓冲区达到100条记录或定时器触发时，将日志批量写入数据库。关闭服务时，确保所有日志被刷新。

--- pkg/service/operation_log.go
+++ pkg/service/operation_log.go
@@ -1,12 +1,70 @@
 package service
 
 import (
+	"sync"
+	"time"
+
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
 	"github.com/weibaohui/k8m/pkg/models"
 )
 
 type operationLogService struct {
+	buffer    []*models.OperationLog
+	bufferMux sync.Mutex
+	ticker    *time.Ticker
+	stopChan  chan bool
 }
 
-func (s *operationLogService) Add(m *models.OperationLog) {
-	_ = m.Save(nil)
+func NewOperationLogService() *operationLogService {
+	s := &operationLogService{
+		buffer:   make([]*models.OperationLog, 0, 100),
+		ticker:   time.NewTicker(2 * time.Second),
+		stopChan: make(chan bool),
+	}
+	go s.startFlushLoop()
+	return s
+}
+
+func (s *operationLogService) Add(m *models.OperationLog, params ...any) {
+	if len(params) > 0 {
+		for _, param := range params {
+			m.Params += utils.ToJSON(param)
+		}
+	}
+	s.bufferMux.Lock()
+	s.buffer = append(s.buffer, m)
+	if len(s.buffer) >= 100 {
+		s.flushBuffer()
+	}
+	s.bufferMux.Unlock()
+}
+
+func (s *operationLogService) startFlushLoop() {
+	for {
+		select {
+		case <-s.ticker.C:
+			s.bufferMux.Lock()
+			s.flushBuffer()
+			s.bufferMux.Unlock()
+		case <-s.stopChan:
+			s.ticker.Stop()
+			s.bufferMux.Lock()
+			s.flushBuffer()
+			s.bufferMux.Unlock()
+			return
+		}
+	}
+}
+
+func (s *operationLogService) flushBuffer() {
+	if len(s.buffer) == 0 {
+		return
+	}
+	dao.DB().CreateInBatches(s.buffer, 100)
+	s.buffer = s.buffer[:0]
+}
+
+func (s *operationLogService) Close() {
+	s.stopChan <- true
 }
