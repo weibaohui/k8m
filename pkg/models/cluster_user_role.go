f6d36e26db897519bece28968f754bfd8105d6bc
weibaohui <weibaohui@yeah.net>
2025-04-13 21:49:03 +0800

    refactor: 将ClusterUserRole中的RoleType重命名为AuthorizationType
    将ClusterUserRole结构体中的RoleType字段重命名为AuthorizationType，以更准确地反映其用途和含义。同时更新了相关注释以保持一致。

--- pkg/models/cluster_user_role.go
+++ pkg/models/cluster_user_role.go
@@ -10,18 +10,18 @@ import (
 )
 
 // ClusterUserRole 集群用户权限表
-// RoleType有两种类型（user、user_group），如果是用户，那么代表这个人有哪些权限
+// AuthorizationType有两种类型（user、user_group），如果是用户，那么代表这个人有哪些权限
 // 如果是Group，那么代表这个组有哪些权限，这个组可能会有多个用户，那么这多个用户都有相关的权限
 type ClusterUserRole struct {
-	ID         uint               `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
-	Cluster    string             `gorm:"index" json:"cluster,omitempty"`    // 集群名称
-	Username   string             `gorm:"index" json:"username,omitempty"`   // 用户名
-	Role       string             `gorm:"index" json:"role,omitempty"`       // 角色类型：只读、读写、Exec
-	Namespaces string             `json:"namespaces,omitempty"`              // Namespaces列表，逗号分割 ，该用户可以访问的Ns
-	RoleType   constants.RoleType `json:"role_type,omitempty"`               // 用户类型。User\Group两种，默认为User，空为User。Group指用户组
-	CreatedBy  string             `gorm:"index" json:"created_by,omitempty"` // 创建者
-	CreatedAt  time.Time          `json:"created_at,omitempty"`
-	UpdatedAt  time.Time          `json:"updated_at,omitempty"`
+	ID                uint                               `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
+	Cluster           string                             `gorm:"index" json:"cluster,omitempty"`    // 集群名称
+	Username          string                             `gorm:"index" json:"username,omitempty"`   // 用户名
+	Role              string                             `gorm:"index" json:"role,omitempty"`       // 角色类型：只读、读写、Exec
+	Namespaces        string                             `json:"namespaces,omitempty"`              // Namespaces列表，逗号分割 ，该用户可以访问的Ns
+	AuthorizationType constants.ClusterAuthorizationType `json:"authorization_type,omitempty"`      // 用户类型。User\Group两种，默认为User，空为User。Group指用户组
+	CreatedBy         string                             `gorm:"index" json:"created_by,omitempty"` // 创建者
+	CreatedAt         time.Time                          `json:"created_at,omitempty"`
+	UpdatedAt         time.Time                          `json:"updated_at,omitempty"`
 }
 
 func (c *ClusterUserRole) List(params *dao.Params, queryFuncs ...func(*gorm.DB) *gorm.DB) ([]*ClusterUserRole, int64, error) {
