5dd59bba86731a9e376989cb651e54f81ca88141
weibaohui <weibaohui@yeah.net>
2025-04-16 23:02:54 +0800

    feat(mcp): 添加SSE路径中的channel参数并记录日志
    在SSE路径中添加了channel参数，并在处理MCP请求时记录用户名、角色和channel信息到日志中，以便更好地跟踪请求上下文

--- pkg/mcp/mcp_start.go
+++ pkg/mcp/mcp_start.go
@@ -8,15 +8,19 @@ import (
 	"github.com/weibaohui/k8m/pkg/constants"
 	"github.com/weibaohui/kom/mcp"
 	"github.com/weibaohui/kom/mcp/metadata"
+	"k8s.io/klog/v2"
 )
 
 func Start(version string, port int) {
 	var ctxFn = func(ctx context.Context, r *http.Request) context.Context {
 		username := r.Header.Get(constants.JwtUserName)
 		role := r.Header.Get(constants.JwtUserRole)
+		channel := server.GetRouteParam(ctx, "channel")
+		klog.V(6).Infof("mcp request, username: %s, role: %s, channel: %s", username, role, channel)
 		// 发起mcp调用请求时注入用户名、角色信息
 		ctx = context.WithValue(ctx, constants.JwtUserName, username)
 		ctx = context.WithValue(ctx, constants.JwtUserRole, role)
+
 		return ctx
 	}
 	cfg := metadata.ServerConfig{
@@ -29,6 +33,7 @@ func Start(version string, port int) {
 			server.WithLogging(),
 		},
 		SSEOption: []server.SSEOption{
+			server.WithSSEPattern("/:channel/sse"),
 			server.WithSSEContextFunc(ctxFn),
 		},
 		AuthKey:     constants.JwtUserName,
