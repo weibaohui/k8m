11744672721435de366bbbdedb83ca3f586ed799
weibaohui <weibaohui@yeah.net>
2025-04-26 23:01:09 +0800

    feat(controller): 添加GatewayClassOptionList函数以获取GatewayClass列表
    该函数用于从指定集群中获取GatewayClass资源列表，并将其按名称排序后返回。通过调用Kubernetes API，实现了GatewayClass的查询和格式化输出。

---
+++ pkg/controller/gatewayapi/gateway_class.go
@@ -0,0 +1,42 @@
+package gatewayapi
+
+import (
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/kom/kom"
+	gatewayapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+)
+
+func GatewayClassOptionList(c *gin.Context) {
+	ctx := amis.GetContextWithUser(c)
+	selectedCluster, err := amis.GetSelectedCluster(c)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	var list []gatewayapiv1.GatewayClass
+	err = kom.Cluster(selectedCluster).WithContext(ctx).
+		CRD("gateway.networking.k8s.io", "v1", "GatewayClass").
+		Resource(&gatewayapiv1.GatewayClass{}).List(&list).Error
+	if err != nil {
+		amis.WriteJsonData(c, gin.H{
+			"options": make([]map[string]string, 0),
+		})
+		return
+	}
+	var names []map[string]string
+	for _, n := range list {
+		names = append(names, map[string]string{
+			"label": n.Name,
+			"value": n.Name,
+		})
+	}
+	slice.SortBy(names, func(a, b map[string]string) bool {
+		return a["label"] < b["label"]
+	})
+	amis.WriteJsonData(c, gin.H{
+		"options": names,
+	})
+}
