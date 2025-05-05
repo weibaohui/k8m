417ab455963e8a9059a0bf15455d32fea0000b74
weibaohui <weibaohui@yeah.net>
2025-05-05 23:02:49 +0800

    feat(jwt): 新增从token中解析username的功能
    新增了GetUsernameFromToken和GetJwtMapClaimsFromToken两个函数，用于从JWT token中解析出username和claims。这些功能增强了JWT处理能力，便于在认证流程中获取用户信息。

--- pkg/comm/utils/jwt_claims.go
+++ pkg/comm/utils/jwt_claims.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/weibaohui/k8m/pkg/constants"
 )
 
 // GetJWTClaims 从请求中获取JWT claims
@@ -27,6 +28,49 @@ func GetJWTClaims(c *gin.Context, jwtTokenSecret string) (jwt.MapClaims, error) 
 	var jwtSecret = []byte(jwtTokenSecret)
 
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("Token 无效")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid JWT claims")
+	}
+
+	return claims, nil
+}
+
+// GetUsernameFromToken 从Authorization中解析出username
+func GetUsernameFromToken(authToken string, jwtTokenSecret string) (string, error) {
+	claims, err := GetJwtMapClaimsFromToken(authToken, jwtTokenSecret)
+	if err != nil {
+		return "", err
+	}
+
+	username, ok := claims[constants.JwtUserName].(string)
+	if !ok {
+		return "", fmt.Errorf("Token中未包含username")
+	}
+
+	return username, nil
+}
+
+// GetJwtMapClaimsFromToken 从token中解析出claims
+func GetJwtMapClaimsFromToken(authToken string, jwtTokenSecret string) (jwt.MapClaims, error) {
+	if authToken == "" {
+		return nil, fmt.Errorf("未提供 Token")
+	}
+
+	if strings.HasPrefix(authToken, "Bearer ") {
+		// token Bear xxxx
+		authToken = authToken[7:]
+	}
+
+	var jwtSecret = []byte(jwtTokenSecret)
+
+	token, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
 		return jwtSecret, nil
 	})
 	if err != nil || !token.Valid {
