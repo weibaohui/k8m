6441472e7a95da9226f2d4a21548b68ca7536aff
weibaohui <weibaohui@yeah.net>
2025-05-26 14:59:47 +0800

    feat(dao): 添加MySQL连接支持并优化SQLite数据库初始化逻辑

--- internal/dao/conn.go
+++ internal/dao/conn.go
@@ -1,15 +1,16 @@
 package dao
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"sync"
 	"time"
 
-	"github.com/weibaohui/k8m/internal/sqlite"
+	"github.com/glebarez/sqlite"
 	"github.com/weibaohui/k8m/pkg/flag"
-
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"k8s.io/klog/v2"
@@ -21,6 +22,46 @@ var (
 	dbInstance *gorm.DB
 	dbErr      error
 )
+
+// ConnMysql 通过配置连接MySQL数据库，主要用于数据迁移
+func ConnMysql() *gorm.DB {
+	cfg := flag.Init()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&collation=%s&%s",
+		cfg.MysqlUser,
+		cfg.MysqlPassword,
+		cfg.MysqlHost,
+		cfg.MysqlPort,
+		cfg.MysqlDatabase,
+		cfg.MysqlCharset,
+		cfg.MysqlCollation,
+		cfg.MysqlQuery,
+	)
+
+	// 隐藏密码
+	showDsn := fmt.Sprintf("%s:******@tcp(%s:%d)/%s?charset=%s&collation=%s&%s",
+		cfg.MysqlUser,
+		cfg.MysqlHost,
+		cfg.MysqlPort,
+		cfg.MysqlDatabase,
+		cfg.MysqlCharset,
+		cfg.MysqlCollation,
+		cfg.MysqlQuery,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// 禁用外键(指定外键时不会在mysql创建真实的外键约束)
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		klog.Errorf("初始化mysql数据库异常: %v", err)
+	}
+	// 开启mysql日志
+	if cfg.MysqlLogMode {
+		db.Debug()
+	}
+
+	klog.V(2).Infof("初始化mysql数据库完成! dsn: %s", showDsn)
+	return db
+}
 
 // connDB 初始化并返回全局唯一的 GORM 数据库连接实例。
 // 若数据库文件不存在，则自动创建所需目录和文件，并设置最大连接数为 1。
@@ -38,44 +79,79 @@ func connDB() (*gorm.DB, error) {
 		)
 
 		cfg := flag.Init()
-
-		// 检测数据库文件是否存在
-		if _, err := os.Stat(cfg.SqlitePath); os.IsNotExist(err) {
-			// 创建文件夹、数据库文件
-			dir := filepath.Dir(cfg.SqlitePath)
-			err := os.MkdirAll(dir, os.ModePerm)
+		if cfg.DBDriver == "sqlite" {
+			if _, err := os.Stat(cfg.SqlitePath); os.IsNotExist(err) {
+				dir := filepath.Dir(cfg.SqlitePath)
+				err := os.MkdirAll(dir, os.ModePerm)
+				if err != nil {
+					klog.Errorf("创建数据库文件[%s]失败: %v", dir, err.Error())
+					return
+				}
+				file, err := os.Create(cfg.SqlitePath)
+				defer file.Close()
+				if err != nil {
+					klog.Errorf("创建数据库文件[%s]失败: %v", cfg.SqlitePath, err.Error())
+					return
+				}
+			}
+			db, err := gorm.Open(sqlite.Open(cfg.SqlitePath), &gorm.Config{
+				Logger: customLogger,
+			})
 			if err != nil {
-				klog.Errorf("创建数据库文件[%s]失败: %v", dir, err.Error())
+				dbErr = err
 				return
 			}
-			// 创建数据库文件
-			file, err := os.Create(cfg.SqlitePath)
-			defer file.Close()
-
+			klog.V(4).Infof("已连接数据库[%s].", cfg.SqlitePath)
+			s, err := db.DB()
 			if err != nil {
-				klog.Errorf("创建数据库文件[%s]失败: %v", cfg.SqlitePath, err.Error())
+				dbErr = err
 				return
 			}
-		}
-		db, err := gorm.Open(sqlite.Open(cfg.SqlitePath), &gorm.Config{
-			Logger: customLogger,
-		})
-		if err != nil {
-			dbErr = err
+			s.SetMaxOpenConns(1)
+			dbInstance = db
 			return
+		} else if cfg.DBDriver == "mysql" {
+			// 若未配置sqlite，尝试mysql
+			dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&collation=%s&%s",
+				cfg.MysqlUser,
+				cfg.MysqlPassword,
+				cfg.MysqlHost,
+				cfg.MysqlPort,
+				cfg.MysqlDatabase,
+				cfg.MysqlCharset,
+				cfg.MysqlCollation,
+				cfg.MysqlQuery,
+			)
+			showDsn := fmt.Sprintf("%s:******@tcp(%s:%d)/%s?charset=%s&collation=%s&%s",
+				cfg.MysqlUser,
+				cfg.MysqlHost,
+				cfg.MysqlPort,
+				cfg.MysqlDatabase,
+				cfg.MysqlCharset,
+				cfg.MysqlCollation,
+				cfg.MysqlQuery,
+			)
+			db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+				Logger:                                   customLogger,
+				DisableForeignKeyConstraintWhenMigrating: true,
+			})
+			if err != nil {
+				dbErr = err
+				klog.Errorf("初始化mysql数据库异常: %v", err)
+				return
+			}
+			if cfg.MysqlLogMode {
+				db = db.Debug()
+			}
+			klog.V(2).Infof("初始化mysql数据库完成! dsn: %s", showDsn)
+			dbInstance = db
 		}
-		klog.V(4).Infof("已连接数据库[%s].", cfg.SqlitePath)
-		s, err := db.DB()
-		if err != nil {
-			dbErr = err
-			return
-		}
-		s.SetMaxOpenConns(1)
-		dbInstance = db
+
 	})
-
 	return dbInstance, dbErr
 }
+
+// DB 获取数据库连接实例
 func DB() *gorm.DB {
 	_, err := connDB()
 	if err != nil {
