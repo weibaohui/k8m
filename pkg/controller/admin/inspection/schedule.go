7469cd25bc7b481b884648b37f4ed7bc6eacb970
weibaohui <weibaohui@yeah.net>
2025-06-12 22:42:58 +0800

    feat(admin/inspection): 添加巡检计划管理接口
    实现巡检计划的列表查询、保存、删除和快速启用/禁用功能

---
+++ pkg/controller/admin/inspection/schedule.go
@@ -0,0 +1,71 @@
+package inspection
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
+)
+
+func List(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.InspectionSchedule{}
+
+	items, total, err := m.List(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonListWithTotal(c, total, items)
+}
+func Save(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.InspectionSchedule{}
+	err := c.ShouldBindJSON(&m)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	err = m.Save(params)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
+func Delete(c *gin.Context) {
+	ids := c.Param("ids")
+	params := dao.BuildParams(c)
+	m := &models.InspectionSchedule{}
+
+	err := m.Delete(params, ids)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+
+	amis.WriteJsonOK(c)
+}
+
+func QuickSave(c *gin.Context) {
+	id := c.Param("id")
+	enabled := c.Param("enabled")
+
+	var entity models.InspectionSchedule
+	entity.ID = utils.ToUInt(id)
+
+	if enabled == "true" {
+		entity.Enabled = true
+	} else {
+		entity.Enabled = false
+	}
+	err := dao.DB().Model(&entity).Select("enabled").Updates(entity).Error
+
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonErrorOrOK(c, err)
+}
