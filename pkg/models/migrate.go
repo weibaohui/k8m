b77c9b836ad3ad86b1b59df897f8c164c1bfe847
weibaohui <weibaohui@yeah.net>
2025-06-29 10:33:03 +0800

    fix: 修正SQL条件中的比较运算符错误

--- pkg/models/migrate.go
+++ pkg/models/migrate.go
@@ -163,7 +163,7 @@ func FixRoleName() error {
 }
 func FixClusterAuthorizationTypeName() error {
 	// 将用户组表中角色进行统一，除了平台管理员以外，都更新为普通用户guest
-	err := dao.DB().Model(&ClusterUserRole{}).Where("authorization_type == '' or authorization_type is null").Update("authorization_type", "user").Error
+	err := dao.DB().Model(&ClusterUserRole{}).Where("authorization_type = '' or authorization_type is null").Update("authorization_type", "user").Error
 	if err != nil {
 		klog.Errorf("更新用户组表中角色失败: %v", err)
 		return err
