fe1c83ea4249654e3a0e4f22338389462d1c9f1a
weibaohui <weibaohui@yeah.net>
2025-07-21 08:15:23 +0800

    docs(helm): 为Helm仓库控制器添加Swagger注释
    为Helm仓库相关的路由方法添加Swagger注释，包括列表查询、保存、删除和更新索引等接口

--- pkg/controller/helm/helm_repo.go
+++ pkg/controller/helm/helm_repo.go
@@ -23,6 +23,11 @@ func RegisterHelmRepoRoutes(admin *gin.RouterGroup) {
 	admin.POST("/helm/repo/save", ctrl.Save)
 }
 
+// @Summary Helm仓库列表
+// @Description 获取所有Helm仓库信息
+// @Security BearerAuth
+// @Success 200 {object} string
+// @Router /admin/helm/repo/list [get]
 func (r *RepoController) List(c *gin.Context) {
 	// 从数据库查询列表
 	params := dao.BuildParams(c)
@@ -36,6 +41,12 @@ func (r *RepoController) List(c *gin.Context) {
 }
 
 // Save 添加或更新Helm仓库
+// @Summary 添加或更新Helm仓库
+// @Description 添加或更新一个Helm仓库信息
+// @Security BearerAuth
+// @Param repo body models.HelmRepository true "Helm仓库信息"
+// @Success 200 {object} string "操作成功"
+// @Router /admin/helm/repo/save [post]
 func (r *RepoController) Save(c *gin.Context) {
 	var repo models.HelmRepository
 	if err := c.ShouldBindJSON(&repo); err != nil {
@@ -56,6 +67,11 @@ func (r *RepoController) Save(c *gin.Context) {
 	amis.WriteJsonOK(c)
 }
 
+// @Summary Helm仓库选项列表
+// @Description 获取所有Helm仓库名称，用于下拉选项
+// @Security BearerAuth
+// @Success 200 {object} string
+// @Router /admin/helm/repo/option_list [get]
 func RepoOptionList(c *gin.Context) {
 	params := dao.BuildParams(c)
 	m := &models.HelmRepository{}
@@ -83,6 +99,12 @@ func RepoOptionList(c *gin.Context) {
 	})
 }
 
+// @Summary 删除Helm仓库
+// @Description 删除一个或多个Helm仓库
+// @Security BearerAuth
+// @Param ids path string true "要删除的仓库ID，多个用逗号分隔"
+// @Success 200 {object} string "操作成功"
+// @Router /admin/helm/repo/delete/{ids} [post]
 func (r *RepoController) Delete(c *gin.Context) {
 	ids := c.Param("ids")
 
@@ -111,6 +133,13 @@ func (r *RepoController) Delete(c *gin.Context) {
 
 	amis.WriteJsonOK(c)
 }
+
+// @Summary 更新Helm仓库索引
+// @Description 更新指定Helm仓库的索引信息
+// @Security BearerAuth
+// @Param ids body string true "要更新索引的仓库ID，多个用逗号分隔"
+// @Success 200 {object} string "操作成功"
+// @Router /admin/helm/repo/update_index [post]
 func (r *RepoController) UpdateReposIndex(c *gin.Context) {
 	var req struct {
 		IDs string `json:"ids"`
