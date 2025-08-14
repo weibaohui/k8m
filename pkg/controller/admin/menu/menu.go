dda213a554ef4d40677abc42f894b58c46d6ff06
weibaohui <weibaohui@yeah.net>
2025-08-14 22:12:26 +0800

    feat(admin): 添加菜单管理相关接口实现
    实现菜单的列表查询、保存和删除功能

---
+++ pkg/controller/admin/menu/menu.go
@@ -0,0 +1,60 @@
+package menu
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weibaohui/k8m/internal/dao"
+	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
+	"github.com/weibaohui/k8m/pkg/models"
+	"gorm.io/gorm"
+)
+
+type AdminMenuController struct {
+}
+
+// AdminMenu 用于菜单相关接口
+// 路由注册函数
+func RegisterAdminMenuRoutes(admin *gin.RouterGroup) {
+
+	ctrl := AdminMenuController{}
+	// menu 平台管理员可操作，管理菜单
+	admin.GET("/menu/list", ctrl.List)
+	admin.POST("/menu/save", ctrl.Save)
+	admin.POST("/menu/delete/:ids", ctrl.Delete)
+
+}
+func (a *AdminMenuController) List(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.Menu{}
+	items, _, err := m.List(params, func(db *gorm.DB) *gorm.DB {
+		return db
+	})
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonData(c, items)
+}
+func (a *AdminMenuController) Save(c *gin.Context) {
+	params := dao.BuildParams(c)
+	m := &models.Menu{}
+	err := m.Save(params, func(db *gorm.DB) *gorm.DB {
+		return db
+	})
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
+func (a *AdminMenuController) Delete(c *gin.Context) {
+	ids := c.Param("ids")
+	params := dao.BuildParams(c)
+	m := &models.Menu{}
+
+	err := m.Delete(params, ids)
+	if err != nil {
+		amis.WriteJsonError(c, err)
+		return
+	}
+	amis.WriteJsonOK(c)
+}
