ad3ec0bdb41ecff7035789de760d7b8ad696d2fc
weibaohui <weibaohui@yeah.net>
2025-09-01 21:27:45 +0800

    fix(pod): 支持从多个Pod流式获取日志并统一输出
    - 移除限制只允许1个Pod，支持根据Selector流式获取多个Pod日志
    - 使用io.Pipe和bufio.Scanner逐行读取每个Pod的日志流
    - 给日志行添加命名前缀，包含命名空间、Pod名和容器名
    - 在客户端断开连接时关闭

--- pkg/controller/pod/pod.go
+++ pkg/controller/pod/pod.go
@@ -1,8 +1,10 @@
 package pod
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 
 	"github.com/gin-gonic/gin"
 	"github.com/weibaohui/k8m/pkg/comm/utils/amis"
@@ -66,25 +68,51 @@ func (lc *LogController) streamPodLogsBySelector(c *gin.Context, ns string, cont
 		amis.WriteJsonError(c, err)
 		return
 	}
-	if len(pods) != 1 {
-		amis.WriteJsonError(c, errors.New("pod 数量过多"))
-		return
+
+	pr, pw := io.Pipe()
+
+	for _, pd := range pods {
+
+		var podName = pd.GetName()
+		logOpt, err := BindPodLogOptions(c, containerName)
+		if err != nil {
+			amis.WriteJsonError(c, err)
+			return
+		}
+		podService := service.PodService()
+		stream, err := podService.StreamPodLogs(ctx, selectedCluster, ns, podName, logOpt)
+		if err != nil {
+			amis.WriteJsonError(c, err)
+			return
+		}
+		go func(pd *v1.Pod, r io.ReadCloser) {
+			defer r.Close()
+			var prefix string
+			if pd.GetNamespace() != "" {
+				prefix = fmt.Sprintf("%s/%s: ", pd.GetNamespace(), pd.GetName())
+			}
+			if containerName != "" {
+				prefix = fmt.Sprintf("%s/%s/%s: ", pd.GetNamespace(), pd.GetName(), containerName)
+			}
+
+			scanner := bufio.NewScanner(r)
+			for scanner.Scan() {
+				line := fmt.Sprintf("[%s] %s", prefix, scanner.Text())
+				if _, err := pw.Write([]byte(line)); err != nil {
+					return // 管道已关闭
+				}
+			}
+		}(&pd, stream)
 	}
 
-	var podName = pods[0].GetName()
-	logOpt, err := BindPodLogOptions(c, containerName)
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	podService := service.PodService()
-	stream, err := podService.StreamPodLogs(ctx, selectedCluster, ns, podName, logOpt)
+	// 监听 ctx，用户断开 SSE 时关闭 PipeWriter
+	go func() {
+		<-ctx.Done()
+		klog.V(2).Infof("SSE connection closed.")
+		pw.Close()
+	}()
 
-	if err != nil {
-		amis.WriteJsonError(c, err)
-		return
-	}
-	sse.WriteSSE(c, stream)
+	sse.WriteSSE(c, pr)
 }
 
 // DownloadLogs 下载Pod日志
