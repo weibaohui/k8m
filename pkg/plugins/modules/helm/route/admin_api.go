5aa36a449a32fd6766bbb5ac7ae6f80dd944cb61
weibaohui <weibaohui@yeah.net>
2026-01-04 22:31:27 +0800

    refactor(helm): 将路由注册从gin迁移到chi并统一方法命名
    将路由注册从gin框架迁移到chi框架，使用直接路由方法替代gin.Group
    统一路由处理方法名为小写格式
    为所有路由处理方法添加response.Adapter包装

--- pkg/plugins/modules/helm/route/admin_api.go
+++ pkg/plugins/modules/helm/route/admin_api.go
@@ -10,47 +10,50 @@ import (
 
 // RegisterPluginAdminRoutes 注册 Helm 插件的管理员路由（平台管理员）
 // 对应原来的 /admin 路径下的路由
+// 从 gin 切换到 chi，使用直接路由方法替代 gin.Group，使用小写方法名
 func RegisterPluginAdminRoutes(arg chi.Router) {
-	g := arg.Group("/plugins/" + modules.PluginNameHelm)
+	prefix := "/plugins/" + modules.PluginNameHelm
 	ctrl := &admin.RepoController{}
-	g.Get("/repo/list", response.Adapter(ctrl.List))
-	g.Post("/repo/delete/:ids", response.Adapter(ctrl.Delete))
-	g.Post("/repo/update_index", response.Adapter(ctrl.UpdateReposIndex))
-	g.Post("/repo/save", response.Adapter(ctrl.Save))
+	arg.Get(prefix+"/repo/list", response.Adapter(ctrl.List))
+	arg.Post(prefix+"/repo/delete/:ids", response.Adapter(ctrl.Delete))
+	arg.Post(prefix+"/repo/update_index", response.Adapter(ctrl.UpdateReposIndex))
+	arg.Post(prefix+"/repo/save", response.Adapter(ctrl.Save))
 
 	settingCtrl := &admin.SettingController{}
-	g.GET("/setting/get", settingCtrl.GetSetting)
-	g.POST("/setting/update", settingCtrl.UpdateSetting)
+	arg.Get(prefix+"/setting/get", response.Adapter(settingCtrl.GetSetting))
+	arg.Post(prefix+"/setting/update", response.Adapter(settingCtrl.UpdateSetting))
 
 	klog.V(6).Infof("注册 Helm 插件管理路由(admin)")
 }
 
 // RegisterPluginAPIRoutes 注册 Helm 插件的 API 路由（K8s API）
 // 对应原来的 /k8s/cluster/{cluster} 路径下的路由
+// 从 gin 切换到 chi，使用直接路由方法替代 gin.Group，使用小写方法名
 func RegisterPluginAPIRoutes(arg chi.Router) {
-	api := arg.Group("/plugins/" + modules.PluginNameHelm)
+	prefix := "/plugins/" + modules.PluginNameHelm
 	ctrl := &admin.ReleaseController{}
 
-	api.GET("/release/list", ctrl.ListRelease)
-	api.GET("/release/ns/:ns/name/:name/history/list", ctrl.ListReleaseHistory)
-	api.POST("/release/:release/repo/:repo/chart/:chart/version/:version/install", ctrl.InstallRelease)
-	api.POST("/release/ns/:ns/name/:name/uninstall", ctrl.UninstallRelease)
-	api.GET("/release/ns/:ns/name/:name/revision/:revision/values", ctrl.GetReleaseValues)
-	api.GET("/release/ns/:ns/name/:name/revision/:revision/notes", ctrl.GetReleaseNote)
-	api.GET("/release/ns/:ns/name/:name/revision/:revision/install_log", ctrl.GetReleaseInstallLog)
-	api.POST("/release/batch/uninstall", ctrl.BatchUninstallRelease)
-	api.POST("/release/upgrade", ctrl.UpgradeRelease)
+	arg.Get(prefix+"/release/list", response.Adapter(ctrl.ListRelease))
+	arg.Get(prefix+"/release/ns/:ns/name/:name/history/list", response.Adapter(ctrl.ListReleaseHistory))
+	arg.Post(prefix+"/release/:release/repo/:repo/chart/:chart/version/:version/install", response.Adapter(ctrl.InstallRelease))
+	arg.Post(prefix+"/release/ns/:ns/name/:name/uninstall", response.Adapter(ctrl.UninstallRelease))
+	arg.Get(prefix+"/release/ns/:ns/name/:name/revision/:revision/values", response.Adapter(ctrl.GetReleaseValues))
+	arg.Get(prefix+"/release/ns/:ns/name/:name/revision/:revision/notes", response.Adapter(ctrl.GetReleaseNote))
+	arg.Get(prefix+"/release/ns/:ns/name/:name/revision/:revision/install_log", response.Adapter(ctrl.GetReleaseInstallLog))
+	arg.Post(prefix+"/release/batch/uninstall", response.Adapter(ctrl.BatchUninstallRelease))
+	arg.Post(prefix+"/release/upgrade", response.Adapter(ctrl.UpgradeRelease))
 
 	klog.V(6).Infof("注册 Helm 插件 API 路由(api)")
 }
 
 // RegisterPluginMgmRoutes 注册 Helm 插件的管理路由（Mgm）
 // 对应原来的 /mgm 路径下的路由
+// 从 gin 切换到 chi，使用直接路由方法替代 gin.Group，使用小写方法名
 func RegisterPluginMgmRoutes(arg chi.Router) {
-	mgm := arg.Group("/plugins/" + modules.PluginNameHelm)
+	prefix := "/plugins/" + modules.PluginNameHelm
 	ctrl := &admin.ChartController{}
-	mgm.GET("/repo/:repo/chart/:chart/versions", ctrl.ChartVersionOptionList)
-	mgm.GET("/repo/:repo/chart/:chart/version/:version/values", ctrl.GetChartValue)
-	mgm.GET("/chart/list", ctrl.ListChart)
+	arg.Get(prefix+"/repo/:repo/chart/:chart/versions", response.Adapter(ctrl.ChartVersionOptionList))
+	arg.Get(prefix+"/repo/:repo/chart/:chart/version/:version/values", response.Adapter(ctrl.GetChartValue))
+	arg.Get(prefix+"/chart/list", response.Adapter(ctrl.ListChart))
 	klog.V(6).Infof("注册 Helm 插件管理路由(mgm)")
 }
