a756699c3ec4d78b4c10a1cf7df1f8354f7e6235
weibaohui <weibaohui@yeah.net>
2026-01-08 11:22:22 +0800

    feat(k8swatch): 添加基于Leader选举的资源监听控制
    实现根据Leader选举状态动态启停Kubernetes资源监听功能
    当实例成为Leader时启动资源监听，失去Leader身份时停止监听
    添加leaderWatchCancel用于优雅停止监听goroutine

--- pkg/plugins/modules/k8swatch/lifecycle.go
+++ pkg/plugins/modules/k8swatch/lifecycle.go
@@ -1,12 +1,17 @@
 package k8swatch
 
 import (
+	"context"
+
 	"github.com/weibaohui/k8m/pkg/plugins"
+	"github.com/weibaohui/k8m/pkg/plugins/eventbus"
+	"github.com/weibaohui/k8m/pkg/plugins/modules"
 	"github.com/weibaohui/k8m/pkg/service"
 	"k8s.io/klog/v2"
 )
 
 type K8sWatchLifecycle struct {
+	leaderWatchCancel context.CancelFunc
 }
 
 func (k *K8sWatchLifecycle) Install(ctx plugins.InstallContext) error {
@@ -35,16 +40,34 @@ func (k *K8sWatchLifecycle) Uninstall(ctx plugins.UninstallContext) error {
 }
 
 func (k *K8sWatchLifecycle) Start(ctx plugins.BaseContext) error {
-	klog.V(6).Infof("启动K8sWatch插件后台任务")
+	if plugins.ManagerInstance().IsRunning(modules.PluginNameLeader) {
+		elect := ctx.Bus().Subscribe(eventbus.EventLeaderElected)
+		lost := ctx.Bus().Subscribe(eventbus.EventLeaderLost)
 
-	service.ClusterService().DelayStartFunc(func() {
-		service.PodService().Watch()
-		service.NodeService().Watch()
-		service.PVCService().Watch()
-		service.PVService().Watch()
-		service.IngressService().Watch()
-	})
+		leaderWatchCtx, cancel := context.WithCancel(context.Background())
+		k.leaderWatchCancel = cancel
 
+		go func() {
+			for {
+				select {
+				case <-elect:
+					klog.V(6).Infof("成为Leader，启动K8s资源监听")
+					k.startWatch()
+				case <-lost:
+					klog.V(6).Infof("不再是Leader，停止K8s资源监听")
+					k.stopWatch()
+				case <-leaderWatchCtx.Done():
+					klog.V(6).Infof("K8sWatch插件 Leader 监听 goroutine 退出")
+					return
+				}
+			}
+		}()
+
+		klog.V(6).Infof("根据实例Leader状态启动K8sWatch插件后台任务")
+	} else {
+		k.startWatch()
+		klog.V(6).Infof("启动K8sWatch插件后台任务")
+	}
 	return nil
 }
 
@@ -55,5 +78,26 @@ func (k *K8sWatchLifecycle) StartCron(ctx plugins.BaseContext, spec string) erro
 
 func (k *K8sWatchLifecycle) Stop(ctx plugins.BaseContext) error {
 	klog.V(6).Infof("停止K8sWatch插件后台任务")
+
+	if k.leaderWatchCancel != nil {
+		k.leaderWatchCancel()
+		k.leaderWatchCancel = nil
+	}
+
+	k.stopWatch()
 	return nil
+}
+
+func (k *K8sWatchLifecycle) startWatch() {
+	service.ClusterService().DelayStartFunc(func() {
+		service.PodService().Watch()
+		service.NodeService().Watch()
+		service.PVCService().Watch()
+		service.PVService().Watch()
+		service.IngressService().Watch()
+	})
+}
+
+func (k *K8sWatchLifecycle) stopWatch() {
+	klog.V(6).Infof("停止K8s资源监听")
 }
