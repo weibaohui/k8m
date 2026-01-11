fab396c8a77492db27d1bf41cb20976df5905857
weibaohui <weibaohui@yeah.net>
2026-01-11 11:24:24 +0800

    docs(leader): 更新主备选举插件的描述信息
    修正描述文本使其更清晰准确，强调使用注意事项

--- pkg/plugins/modules/leader/metadata.go
+++ pkg/plugins/modules/leader/metadata.go
@@ -11,7 +11,7 @@ var Metadata = plugins.Module{
 		Name:        modules.PluginNameLeader,
 		Title:       "多实例主备选举插件",
 		Version:     "1.0.0",
-		Description: "提供多实例自动选举能力：通过 Kubernetes 原生机制完成选主。通过LabelSelector筛选具有k8m.io/role: leader的Pod为访问流量承载Pod。其他Pod不承载访问流量。",
+		Description: "提供多实例自动选举能力,在N个实例中选举出一个主实例。使用k8s原生机制完成选主。访问流量应通过LabelSelector筛选带有k8m.io/role: leader标签的Pod做为承载Pod。其他Pod不承载访问流量。使用时请务必注意此点。",
 	},
 	Tables:    []string{},
 	Lifecycle: &LeaderLifecycle{},
